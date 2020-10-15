a3158e0a9ceb0c49fe5d34b6ed0753c97de5fdfd
Thanaphon Sombunkaeo <ict224bj@gmail.com>
2020-10-15 16:48:41 +0700

    [ADMIN] : [UPDATE] navbar style

--- eduroom_frontend/components/layouts/navbar.js
+++ eduroom_frontend/components/layouts/navbar.js
@@ -1,69 +1,98 @@
-import React,{Fragment} from 'react'
-import {AppBar, Toolbar, Button} from '@material-ui/core'
+import React, { Fragment } from 'react';
+import Link from 'next/link';
+import { AppBar, Toolbar, Button } from '@material-ui/core';
 const Navbar = () => {
     const NavItemStyle = {
         color: '#5B5B5B',
         fontWeight: '500',
-        fontFamily: "'Quicksand', sans-serif"
-    }
-    const HeaderStyle = {color:'#3D467F',fontSize:'2em',fontWeight:'bold',fontFamily:"'Quicksand', sans-serif"}
+        fontFamily: "'Quicksand', sans-serif",
+    };
+    const HeaderStyle = {
+        color: '#3D467F',
+        fontSize: '2em',
+        fontWeight: 'bold',
+        fontFamily: "'Quicksand', sans-serif",
+    };
     return (
         <Fragment>
-            <AppBar position="sticky" style={{background:'#FFFFFFAA'}} elevation={0}>
-                <Toolbar style={{padding:'1% 4% 0% 4%'}}>
-                    <div style={{display:'flex',alignItems:'center',justifyContent:'space-between',width:'100%'}}>
-                    <span style={HeaderStyle}>eduroom</span>
-                    <div className="navItemLayout">
-                    <div className="navItem">Exam</div>
-                    <div className="navItem">Course</div>
-                    <div className="navItem">Forum</div>
-                    <div className="navItem">Home</div>
-                    <div className="navAction">
-                        <button className="navLogin">
-                            <span className="navLoginText">Login</span>
-                        </button>
-                    </div>
-                    </div>
+            <AppBar
+                position="sticky"
+                style={{ background: '#FFFFFFAA' }}
+                elevation={0}
+            >
+                <Toolbar style={{ padding: '1% 4% 0% 4%' }}>
+                    <div
+                        style={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'space-between',
+                            width: '100%',
+                        }}
+                    >
+                        <span style={HeaderStyle}>eduroom</span>
+                        <div className="navItemLayout">
+                            <div className="navItem">
+                                <Link href="/exam">Exam</Link>
+                            </div>
+                            <div className="navItem">
+                                <Link href="/course">Course</Link>
+                            </div>
+                            <div className="navItem">
+                                <Link href="/forum">Forum</Link>
+                            </div>
+                            <div className="navItem">
+                                <Link href="/">Home</Link>
+                            </div>
+                            <div className="navAction">
+                                <button className="navLogin">
+                                    <span className="navLoginText">Login</span>
+                                </button>
+                            </div>
+                        </div>
                     </div>
                 </Toolbar>
             </AppBar>
             <style jsx>
-                {
-                    `
+                {`
                     .navHeader {
-                        color: #3D467F;
+                        color: #3d467f;
                         font-size: 2em;
                         font-weight: bold;
                         font-family: 'Quicksand', sans-serif;
                     }
-                    .navItemLayout{
+                    .navItemLayout {
                         width: 35vw;
                         display: flex;
                         justify-content: space-around;
                         align-items: center;
                     }
-                    .navItem { 
-                        color: #5B5B5B;
+                    .navItem {
+                        color: #5b5b5b;
                         font-weight: 500;
                         font-size: 1.2em;
                         font-family: 'Quicksand', sans-serif;
                     }
                     .navLogin {
-                        background: #FE75B7;
+                        background: #fe75b7;
                         border-radius: 25px;
-                        padding: .5rem 1.5rem;
+                        padding: 0.5rem 1.5rem;
                         border: none;
+                        transition: 0.25s;
                     }
-                    .navLoginText{
+                    .navLogin:hover {
+                        cursor: pointer;
+                        opacity: 0.8;
+                        transition: 0.25s;
+                    }
+                    .navLoginText {
                         color: white;
                         font-weight: 700;
                         font-size: 1.2em;
                         font-family: 'Quicksand', sans-serif;
                     }
-                    `
-                }
+                `}
             </style>
         </Fragment>
-    )
-}
-export default Navbar
+    );
+};
+export default Navbar;
--- eduroom_frontend/pages/index.js
+++ eduroom_frontend/pages/index.js
@@ -1,19 +1,25 @@
-import Head from 'next/head'
+import Head from 'next/head';
 import Navbar from '../components/layouts/navbar';
-import LandingContent from '../components/landing/content'
+import LandingContent from '../components/landing/content';
 export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>Eduroom Project</title>
-        <link rel="icon" href="/favicon.ico" />
-        <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet"></link>
-        <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap" rel="stylesheet"></link>
-      </Head>
-      <main>
-        <Navbar />
-        <LandingContent />
-      </main>
-    </div>
-  )
+    return (
+        <div>
+            <Head>
+                <title>Eduroom Project</title>
+                <link rel="icon" href="/favicon.ico" />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap"
+                    rel="stylesheet"
+                ></link>
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap"
+                    rel="stylesheet"
+                ></link>
+            </Head>
+            <main>
+                <Navbar />
+                <LandingContent />
+            </main>
+        </div>
+    );
 }
