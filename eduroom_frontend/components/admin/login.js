7f51d157eff37ec9f879bdb428609168befdf18e
GGolfz <wisarutgolf11@gmail.com>
2020-10-20 19:15:23 +0700

    [ADMIN] : [CHANGE] Module CSS to be JSX style && [CHANGE] Coinshop route to be index of coinshop folder

--- eduroom_frontend/components/admin/login.js
+++ eduroom_frontend/components/admin/login.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useEffect, useState } from 'react';
 import Link from 'next/link';
+import style from '../../styles/admin/login'
 const Content = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -44,78 +45,7 @@ const Content = () => {
         </div>
       </div>
       <style jsx>
-        {`
-          .login {
-            width: 100vw;
-            height: 100vh;
-            display: flex;
-            justify-content: flex-start;
-            align-items: center;
-            padding: 2% 5% 0% 5%;
-            cursor: default;
-          }
-          .login-textfield {
-            background: #eff0f6;
-            border-radius: 10px;
-            width: 100%;
-            padding: 5%;
-            border: none;
-            font-size: 1.1em;
-            color: #3d467f;
-            margin-bottom: 5%;
-          }
-          .login-textfield ::placeholder {
-            color: #3d467f;
-            opacity: 0.75;
-          }
-          .admin-login-page-img {
-            width: 60%;
-            padding: 4%;
-          }
-          .admin-login-img{
-            width: 100%;
-          }
-          .login-content {
-            width: 40%;
-            padding: 12% 5%;
-            font-family: 'Quicksand', sans-serif;
-          }
-          .login-header {
-            font-size: 2.4em;
-            font-family: 'Quicksand', sans-serif;
-            font-weight: bold;
-            color: #3d467f;
-          }
-          .login-form {
-            text-align: center;
-          }
-          .login-description {
-            padding: 5% 0% 5% 0%;
-            font-size: 1em;
-            font-family: 'Quicksand', sans-serif;
-            font-weight: normal;
-            color: #3d467f;
-          }
-          .login-button {
-            background: #F39AC4;
-            border-radius: 25px;
-            padding: 0.75rem 5.3rem;
-            margin-bottom: 5%;
-            border: none;
-            transition: 0.25s;
-          }
-          .login-button:hover {
-            cursor: pointer;
-            opacity: 0.9;
-            transition: 0.2s;
-          }
-          .login-button-text {
-            color: white;
-            font-weight: 700;
-            font-size: 1.5em;
-            font-family: 'Quicksand', sans-serif;
-          }
-        `}
+        {style}
       </style>
     </Fragment>
   );
--- eduroom_frontend/components/landing/register.js
+++ eduroom_frontend/components/landing/register.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useEffect, useState } from 'react';
+import style from '../../styles/landing/register'
 const Register = () => {
   const [user, setUser] = useState({
     firstname: '',
@@ -91,128 +92,7 @@ const Register = () => {
         </div>
       </div>
       <style jsx>
-        {`
-          .label-text {
-            color: #473f47;
-          }
-          .register {
-            width: 100vw;
-            display: flex;
-            justify-content: flex-start;
-            padding: 2% 0% 0% 0%;
-            cursor: default;
-          }
-          .register-textfield {
-            background: #eff0f6;
-            border-radius: 10px;
-            width: 100%;
-            padding: 4%;
-            border: none;
-            color: #3d467f;
-            margin-bottom: 3%;
-          }
-          .register-textfield ::placeholder {
-            color: #3d467f;
-            opacity: 0.75;
-          }
-          .register-page-img {
-            width: 100%;
-          }
-          .register-content {
-            width: 38vw;
-            position: absolute;
-            right: 8%;
-            top: 15%;
-          }
-          .register-link {
-            font-size: 1em;
-            font-weight: bold;
-            color: #fe75b7;
-            text-decoration-line: underline;
-            cursor: pointer;
-          }
-          .striaght-line {
-            border-bottom: 1px solid rgba(71, 63, 71, 0.6);
-            width: 45%;
-            height: 12px;
-          }
-          .register-header {
-            font-size: 2.4em;
-            font-family: 'Quicksand', sans-serif;
-            font-weight: bold;
-            color: #3d467f;
-            margin-bottom: 10px;
-          }
-          .register-form {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            background: rgba(255, 255, 255, 0.6);
-            box-shadow: 6px 4px 19px rgba(0, 0, 0, 0.25);
-            border-radius: 25px;
-            padding: 30px 20px 30px 20px;
-          }
-          .register-description {
-            padding: 5% 0% 5% 0%;
-            font-size: 1em;
-            font-weight: normal;
-            color: #3d467f;
-          }
-          .register-button {
-            background: #3d467f;
-            border-radius: 25px;
-            padding: 0.75rem 5.3rem;
-            margin-bottom: 5%;
-            margin-top: 2.5%;
-            border: none;
-            outline: none;
-          }
-          .register-button:hover {
-            cursor: pointer;
-            opacity: 0.9;
-            transition: 0.2s;
-          }
-          .register-button-text {
-            color: white;
-            font-weight: 700;
-            font-size: 1.5em;
-            font-family: 'Quicksand', sans-serif;
-          }
-          .register-google-button {
-            background: white;
-            border-radius: 25px;
-            padding: 0.75rem 2rem;
-            border: 2px solid rgba(0, 0, 0, 0.3);
-            cursor: pointer;
-            outline: none;
-          }
-          .register-google-button:hover {
-            cursor: pointer;
-            opacity: 0.7;
-            transition: 0.25s;
-          }
-          .or-text {
-            color: rgba(71, 63, 71, 0.6);
-            margin-bottom: 5%;
-            display: flex;
-            font-weight: bold;
-          }
-          .or-text-text {
-            margin: 0% 2%;
-          }
-          .google-logo {
-            width: 20px;
-            margin-right: 12px;
-          }
-          .register-google-button-text {
-            display: flex;
-            align-items: center;
-            color: rgba(71, 63, 71, 0.6);
-            font-weight: 700;
-            font-size: 1em;
-            font-family: 'Quicksand', sans-serif;
-          }
-        `}
+        {style}
       </style>
     </Fragment>
   );
