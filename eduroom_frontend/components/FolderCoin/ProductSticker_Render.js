ec88ea266c35eb1626cc5c83083b28c8c11c58ca
note <note16651@hotmail.co.th>
2020-10-21 12:51:22 +0700

    [G09]: [ADD] Update Styles

--- eduroom_frontend/components/FolderCoin/ProductSticker_Render.js
+++ eduroom_frontend/components/FolderCoin/ProductSticker_Render.js
@@ -1,9 +1,13 @@
-import Link from 'next/link';
-import Styles from '../../styles/CoinStyles/ProductSticker_Render.module.css';
-const Name = props => {
 
+import Card from '@material-ui/core/Card';
+import Styles from '../../styles/CoinStyles/ProductSticker_Render.module.css';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+const Name = props => {
     return (
-        <div className={Styles.cardContainer}>
+
+        <Card className={Styles.cardContainer}>
+        <CardContent >
             <div style={{ padding: '20px' }}>
                 <div className={Styles.container}>
                     <h1 className={Styles.text}>
@@ -12,12 +16,14 @@ const Name = props => {
                     <h1 className={Styles.text}>
                         Price of Sticker :{props.price} Coin
                     </h1>
-                    <Link href={`/coin-shop/payment/${props.id}`}>
-                        <a className={Styles.btn}>Buy!</a>
-                    </Link>
+                    <Button href={`/coin-shop/payment/${props.id}`} variant='contained' color='primary'>
+                        Buy!
+                    </Button>
                 </div>
             </div>
-        </div>
+        </CardContent>
+        </Card>
+
     );
 };
 export default Name;
