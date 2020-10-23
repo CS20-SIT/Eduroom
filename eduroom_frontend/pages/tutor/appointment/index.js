e5ab068a73d138072db63e7f0dc1931c5567fcee
thanawat.bcr <thanwat.bcr@gmail.com>
2020-10-23 16:33:26 +0700

    [SETUP] Pages for private tutor feature

--- eduroom_frontend/pages/tutor/appointment/index.js
+++ eduroom_frontend/pages/tutor/appointment/index.js
@@ -1,7 +1,7 @@
 import React,{Fragment} from 'react'
 const Temp = () => {
     return <Fragment>
-        <div>Instructor List</div>
+        <div>Student's Appointments</div>
     </Fragment>
 }
 export default Temp
