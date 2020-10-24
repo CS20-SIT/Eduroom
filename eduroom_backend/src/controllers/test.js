6996a33ad04c18af06ff00ac8166b365522eb197
Nutdanai <nutdanai.282@mail.kmutt.ac.th>
2020-10-24 16:50:23 +0700

    [G08] : [TRY] Just playing with API request
     Try Get / Post method

--- eduroom_backend/src/controllers/test.js
+++ eduroom_backend/src/controllers/test.js
@@ -1,9 +1,26 @@
-const ErrorResponse = require('../utils/errorResponse')
-const pool = require('../database/db')
+const ErrorResponse = require("../utils/errorResponse");
+const pool = require("../database/db");
 
-exports.test = async (req,res,next) => {
-    const time = await pool.query('SELECT NOW()')
-    // res.status(200).json({success:true,msg: 'test from backend',time:time.rows[0]})
-    res.send({success: true})
-    return
-}
+const test = async (req, res, next) => {
+  const time = await pool.query("SELECT NOW()");
+
+  // res.status(200).json({success:true,msg: 'test from backend',time:time.rows[0]})
+  res.send({ success: true });
+  return;
+};
+
+const getAnn = async (req, res, next) => {
+  const data = await pool.query("select * from announcements ");
+  const ann = data.rows;
+  res.send(ann);
+};
+const postAnn = async (req, res, next) => {
+  const title = req.body.title;
+
+  await pool.query(
+    "INSERT INTO announcements VALUES (DEFAULT, '" + title + "' , 'Hello' ,0 )"
+  );
+
+  res.send({ success: true, title });
+};
+module.exports = { getAnn, test, postAnn };
