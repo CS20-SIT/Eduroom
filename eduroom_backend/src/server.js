567103d765ddbd7454781ccc0890fd56054a1e27
Nutdanai <nutdanai.282@mail.kmutt.ac.th>
2020-10-24 16:50:23 +0700

    [G08] : [TRY] Just playing with API request
     Try Get / Post method

--- eduroom_backend/src/server.js
+++ eduroom_backend/src/server.js
@@ -1,16 +1,18 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const morgan = require('morgan');
-const errorHandler = require('./middleware/error');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-const ConfigRoute = require('./routes/configRoute')
-const passport = require('passport')
-const passportConfig = require('./config/passport')
-if (process.env.NODE_ENV !== 'production') {
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const errorHandler = require("./middleware/error");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const ConfigRoute = require("./routes/configRoute");
+const passport = require("passport");
+const passportConfig = require("./config/passport");
+
+const { test, getAnn, postAnn } = require("./controllers/test");
+
+if (process.env.NODE_ENV !== "production") {
   dotenv.config({ path: `${__dirname}/config/config.env` });
 }
-
 
 const app = express();
 
@@ -18,8 +20,8 @@ app.use(express.json());
 
 app.use(cookieParser());
 
-let originURL = 'http://localhost:3000';
-if (process.env.NODE_ENV === 'production') {
+let originURL = "http://localhost:3000";
+if (process.env.NODE_ENV === "production") {
   originURL = process.env.CLIENT_URL;
 }
 
@@ -30,13 +32,18 @@ app.use(
   })
 );
 
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
-passportConfig(passport)
+passportConfig(passport);
 app.use(passport.initialize());
 
-app.use('/api',ConfigRoute)
+app.use("/api", ConfigRoute);
 app.use(errorHandler);
 
-module.exports = app
+app.get("/api/08", test);
+
+app.get("/api/08/ann", getAnn);
+app.post("/api/08/cann", postAnn);
+
+module.exports = app;
