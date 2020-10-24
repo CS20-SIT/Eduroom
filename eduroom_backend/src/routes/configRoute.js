a320c926d265fb6679eb43dc7f40c3544413c6ec
Gorgun <yosathorn.pengma@mail.kmutt.ac.th>
2020-10-24 17:20:16 +0700

    [G14] : [ADD] get and create Event back-end

--- eduroom_backend/src/routes/configRoute.js
+++ eduroom_backend/src/routes/configRoute.js
@@ -2,7 +2,9 @@ const express = require('express')
 const router = express.Router()
 const TestRoute = require('./testRoute')
 const AuthRoute = require('./authRoute')
+const EventRoute = require('./eventRoute')
 router.use('/test',TestRoute)
 router.use('/auth',AuthRoute)
+router.use('/event',EventRoute)
 
 module.exports = router
