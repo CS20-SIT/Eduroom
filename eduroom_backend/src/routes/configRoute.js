dc21eceefd2bbb6bc69417d804a1f024cce051c5
fantasyzernontra <fantasyzernontra@hotmail.com>
2020-10-24 11:02:17 +0700

    [ADD] Get Amount of Specific Course API

--- eduroom_backend/src/routes/configRoute.js
+++ eduroom_backend/src/routes/configRoute.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const router = express.Router()
+
 const TestRoute = require('./testRoute')
 const AuthRoute = require('./authRoute')
+const AnalysisRoute = require('./analysisRoute')
+
 router.use('/test',TestRoute)
 router.use('/auth',AuthRoute)
+router.use('/analysis', AnalysisRoute)
 
 module.exports = router
