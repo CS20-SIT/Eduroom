e8c550676dd087a37ec189018cfd7d45f81dac1e
best <sunsasabest@hotmail.com>
2020-10-26 21:55:59 +0700

    [ADD] floder calendar to style, component, page

---
+++ eduroom_frontend/pages/calendar/index.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react'
+import GeneralTemplate from '../components/template/general'
+const Calendar = () => {
+  return (
+    <Fragment>
+      <GeneralTemplate>
+        <h1>Hello Calendar</h1>
+      </GeneralTemplate>
+    </Fragment>
+  )
+}
+export default Calendar
