95c339decac9e00c23f6c9f57077064d47530a1c
best <sunsasabest@hotmail.com>
2020-10-26 22:43:32 +0700

    [TRY] grid to make calendar (not finish)

--- eduroom_frontend/components/calendar/calendar.js
+++ eduroom_frontend/components/calendar/calendar.js
@@ -1,17 +1,36 @@
 import React, { Fragment } from 'react';
-import { useRouter } from 'next/router';
+// import { useRouter } from 'next/router';
 import style from '../../styles/calendar/calendar'
 const Content = () => {
-  const router = useRouter();
-  return (
-    <Fragment>
-      <div>
-        <h1 className="test">Hello Calendar</h1>
-      </div>
-      <style jsx>
-        {style}
-      </style>
-    </Fragment>
-  );
+    //   const router = useRouter();
+
+    return (
+        <Fragment>
+            <div>
+                <h1 className="test">Hello Calendar</h1>
+                <div className="grid">
+                    <div className="gridItem">Mon</div>
+                    <div className="gridItem">Tue</div>
+                    <div className="gridItem">Wed</div>
+                    <div className="gridItem">Thu</div>
+                    <div className="gridItem">Fri</div>
+                    <div className="gridItem">Sat</div>
+                    <div className="gridItem">Sun</div>
+
+                    <div className="gridItem">1</div>
+                    <div className="gridItem">2</div>
+                    <div className="gridItem">3</div>
+                    <div className="gridItem">4</div>
+                    <div className="gridItem">5</div>
+                    <div className="gridItem">6</div>
+                    <div className="gridItem">7</div>
+                    
+                </div>
+            </div>
+            <style jsx>
+                {style}
+            </style>
+        </Fragment>
+    );
 };
 export default Content;
