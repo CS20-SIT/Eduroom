52fc42388f80fcab0c5f73bc098295abd01587eb
coolkidneversleep <iiippbhy@gmail.com>
2020-10-26 18:51:49 +0700

    [G12] : [ADD] forum page and create forum page

--- eduroom_backend/src/routes/configRoute.js
+++ eduroom_backend/src/routes/configRoute.js
@@ -2,7 +2,9 @@ const express = require('express')
 const router = express.Router()
 const TestRoute = require('./testRoute')
 const AuthRoute = require('./authRoute')
+const ForumRoute = require('./forumRoute')
 router.use('/test',TestRoute)
 router.use('/auth',AuthRoute)
+router.use('/forum',ForumRoute)
 
 module.exports = router
