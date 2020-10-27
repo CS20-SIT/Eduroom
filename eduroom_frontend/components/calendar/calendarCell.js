38ecd2444573b58aa6ca2152aa03e7c80515c650
best <sunsasabest@hotmail.com>
2020-10-27 08:52:46 +0700

    [ADD] dynamic calendar heading

---
+++ eduroom_frontend/components/calendar/calendarCell.js
@@ -0,0 +1,17 @@
+import React, { Fragment } from 'react';
+// import { useRouter } from 'next/router';
+import style from '../../styles/calendar/calendar'
+const Content = (props) => {
+    //   const router = useRouter();
+    console.log(props.Content);
+    return (
+        <Fragment>
+            <div className="gridItem">{props.Content}</div>
+            <style jsx>
+                {style}
+            </style>
+        </Fragment>
+        
+    );
+};
+export default Content;
--- eduroom_frontend/styles/calendar/calendar.js
+++ eduroom_frontend/styles/calendar/calendar.js
@@ -10,7 +10,7 @@ export default css`
     padding: 10px;
   }
   .gridItem {
-    background-color: rgba(255, 255, 255, 0.8);
+  background-color: rgba(255, 255, 255, 0.8);
   border: 1px solid rgba(0, 0, 0, 0.8);
   padding: 20px;
   font-size: 30px;
