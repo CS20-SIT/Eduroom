e93e8a7f109237e5bdec25e232463fe8948d9588
note <note16651@hotmail.co.th>
2020-10-27 15:42:13 +0700

    [G09] : [ADD] LandingPage Sticker && [Update] Style

--- eduroom_frontend/components/FolderCoin/ProductSticker_Render.js
+++ eduroom_frontend/components/FolderCoin/ProductSticker_Render.js
@@ -1,4 +1,3 @@
-
 import Card from '@material-ui/core/Card';
 import Styles from '../../styles/CoinStyles/ProductSticker_Render.module.css';
 import CardContent from '@material-ui/core/CardContent';
@@ -6,23 +5,29 @@ import Button from '@material-ui/core/Button';
 const Name = props => {
     return (
 
-        <Card className={Styles.cardContainer}>
-        <CardContent >
-            <div style={{ padding: '20px' }}>
-                <div className={Styles.container}>
-                    <h1 className={Styles.text}>
-                        Name Sticker : {props.title}
-                    </h1>
-                    <h1 className={Styles.text}>
-                        Price of Sticker :{props.price} Coin
-                    </h1>
-                    <Button href={`/coin-shop/payment/${props.id}`} variant='contained' color='primary'>
-                        Buy!
-                    </Button>
-                </div>
-            </div>
-        </CardContent>
-        </Card>
+        <div className={Styles.cardContainer}>
+            <Card >
+                <CardContent>
+                    <div style={{ padding: '20px' }}>
+                        <div className={Styles.container}>
+                            <h1 className={Styles.text}>
+                                Name Sticker : {props.title}
+                            </h1>
+                            <h1 className={Styles.text}>
+                                Price of Sticker :{props.price} Coin
+                            </h1>
+                            <Button
+                                href={`/coin-shop/payment/${props.id}`}
+                                variant='contained'
+                                color='primary'
+                            >
+                                Buy!
+                            </Button>
+                        </div>
+                    </div>
+                </CardContent>
+            </Card>
+        </div>
 
     );
 };
