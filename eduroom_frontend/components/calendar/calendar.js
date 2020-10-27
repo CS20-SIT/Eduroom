205064749a7c7b4eaa289e5ddd6e6db39cb182e4
best <sunsasabest@hotmail.com>
2020-10-27 20:46:28 +0700

    [list the daysInMonth dynamically]

--- eduroom_frontend/components/calendar/calendar.js
+++ eduroom_frontend/components/calendar/calendar.js
@@ -1,11 +1,35 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import Cell from '../../components/calendar/calendarCell'
 // import { useRouter } from 'next/router';
 import style from '../../styles/calendar/calendar'
+import moment from 'moment';
+
 const Content = () => {
     //   const router = useRouter();
-    const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+    const days = moment.weekdaysShort();
+    const [day] = useState({dateObject: moment()})
 
+
+    const firstDayOfMonth = ()=>{
+        let dateObj = day.dateObject
+        let firstDay = moment(dateObj)
+            .startOf("month")
+            .format("d");
+        return firstDay;
+    }
+
+    let blank = [];
+    for(let i=0; i<firstDayOfMonth(); i++){
+        blank.push("");
+    }
+
+
+    let daysInMonth = [];
+    for(let d = 1; d<= moment(day.dateObject).daysInMonth(); d++){
+        daysInMonth.push(d);
+    }
+
+    console.log(firstDayOfMonth());
 
     return (
         <Fragment>
@@ -13,6 +37,12 @@ const Content = () => {
 
                 <div className="grid">
                     {days.map((day) => {
+                        return <Cell Content={day} />
+                    })}
+                    {blank.map((day) => {
+                        return <Cell Content={day} />
+                    })}
+                    {daysInMonth.map((day) => {
                         return <Cell Content={day} />
                     })}
                 </div>
