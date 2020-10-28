110cd03a259b30199032aa5f17d8e5026812a719
pphawikaa <pphawikaa@gmail.com>
2020-10-28 13:23:42 +0700

    [ADD] payment page

--- eduroom_backend/src/routes/configRoute.js
+++ eduroom_backend/src/routes/configRoute.js
@@ -2,7 +2,9 @@ const express = require('express')
 const router = express.Router()
 const TestRoute = require('./testRoute')
 const AuthRoute = require('./authRoute')
+
 router.use('/test',TestRoute)
 router.use('/auth',AuthRoute)
+
 
 module.exports = router
