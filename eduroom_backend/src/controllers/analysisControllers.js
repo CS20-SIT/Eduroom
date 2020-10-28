f75fd52a5e22b6fad4d3368da9ad70484de4e109
fantasyzernontra <fantasyzernontra@hotmail.com>
2020-10-28 14:25:51 +0700

    [FIX] Query Response

--- eduroom_backend/src/controllers/analysisControllers.js
+++ eduroom_backend/src/controllers/analysisControllers.js
@@ -1,17 +1,15 @@
 const pool = require('../database/db')
-
-
 //Course Method
 
 //GET Method
 const getAmountOfCourse = async (req, res) => {
     try {
-        const NumberOfCourse = await pool.auery('SELECT COUNT(*) FROM COURSE ')
+        const { rows } = await pool.auery('SELECT COUNT(*) FROM COURSE ')
 
         if(!NumberOfCourse)
         res.status(404).send({ msg: 'Not Found' })
 
-        res.status(200).send(NumberOfCourse)
+        res.status(200).send(rows)
     } catch(err){
         res.status(500).send(err.massage)
     }
@@ -19,14 +17,14 @@ const getAmountOfCourse = async (req, res) => {
 
 const getAmountOfSpecificCourse = async (req, res) => {
     try {
-        const amountOfSpecificCourse = await pool.query('SELECT COUNT(*) as "Amount Of Course", Categories.cata_name ' +
+        const { rows } = await pool.query('SELECT COUNT(*) as "Amount Of Course", Categories.cata_name ' +
             'FROM Course_categories, Categories ' +
             'GROUP BY Categories.cata_id')
 
         if (!amountOfSpecificCourse)
             res.status(404).send({ msg: 'Not Found' })
 
-        res.status(200).send(amountOfSpecificCourse)
+        res.status(200).send(rows)
     } catch (err) {
         res.status(500).send(err.message)
     }
@@ -35,14 +33,14 @@ const getAmountOfSpecificCourse = async (req, res) => {
 //get SoldOutCourse
 const getSoldOutCourse = async (req, res) => {
     try {
-        const Financial_Transaction = await pool.query(" SELECT COUNT(*) as 'Amount Of SoldOut Course' " +
+        const { rows } = await pool.query(" SELECT COUNT(*) as 'Amount Of SoldOut Course' " +
             'FROM Financial_Transaction ' +
             'WHERE Financition_Transaction.description LIKE "%course%" ')
 
         if (!Financial_Transaction)
             res.status(404).send({ msg: 'Not Found' })
 
-        res.status(200).send(Financial_Transaction)
+        res.status(200).send(rows)
     }catch(err){
         res.status(500).send(err.message)
     }
