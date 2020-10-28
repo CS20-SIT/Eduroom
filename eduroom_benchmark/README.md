f4f86e90747259a18567b78efab3148a834eb73d
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2020-10-28 11:47:32 +0700

    [ADMIN]:[ADD] Output as a markdown file

---
+++ eduroom_benchmark/README.md
@@ -0,0 +1,49 @@
+# Low Performance Route
+**https://eduroom.cscms.me/**:  `84`
+**https://eduroom.cscms.me/login**:  `75`
+**https://eduroom.cscms.me/register**:  `82.25`
+**https://eduroom.cscms.me/edqiz**:  `89.75`
+
+## /
+**URL**: https://eduroom.cscms.me/
+**Performance**: 90
+**Accessibility**: 71
+**Best Practices**: 100
+**SEO**: 75
+**Average**: 84
+## /login
+**URL**: https://eduroom.cscms.me/login
+**Performance**: 65
+**Accessibility**: 60
+**Best Practices**: 100
+**SEO**: 75
+**Average**: 75
+## /register
+**URL**: https://eduroom.cscms.me/register
+**Performance**: 95
+**Accessibility**: 61
+**Best Practices**: 100
+**SEO**: 73
+**Average**: 82.25
+## /edqiz
+**URL**: https://eduroom.cscms.me/edqiz
+**Performance**: 98
+**Accessibility**: 75
+**Best Practices**: 100
+**SEO**: 86
+**Average**: 89.75
+## /edqiz/create
+**URL**: https://eduroom.cscms.me/edqiz/create
+**Performance**: 95
+**Accessibility**: 75
+**Best Practices**: 100
+**SEO**: 91
+**Average**: 90.25
+## /support
+**URL**: https://eduroom.cscms.me/support
+**Performance**: 97
+**Accessibility**: 85
+**Best Practices**: 100
+**SEO**: 83
+**Average**: 91.25
+
--- eduroom_benchmark/index.js
+++ eduroom_benchmark/index.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const lighthouse = require('lighthouse');
 const chromeLauncher = require('chrome-launcher');
 (async () => {
+  let str = ""
   const result = await fs.readFileSync('whitelist.txt','utf-8')
   let routes = result.split('\n')
   let warning = []
@@ -12,25 +13,25 @@ const chromeLauncher = require('chrome-launcher');
     const result = runnerResult.lhr.categories;
     let score = 0;
     let category = Object.keys(result)
-    console.log('URL: https://eduroom.cscms.me'+route)
+    str += '## '+route + '\n'
+    str +=  `**URL**: https://eduroom.cscms.me${route}\n` 
     for(let el of category){
         if(result[el].title != 'Progressive Web App'){
-            console.log(result[el].title + ":" , result[el].score*100)
+            str += `**${result[el].title}**` + ": " + result[el].score*100 + '\n'
             score+=result[el].score*100
         }
     }
-    console.log("Average: ",score/4)
-    if(score/4 < 80){
+    str += "**Average**: " + score/4 + '\n'
+    if(score/4 < 90){
         warning.push({url:"https://eduroom.cscms.me"+route,avg:score/4})
     }
     await chrome.kill();
   }
   if(warning.length !=0){
-    console.error("\x1b[41m"+"Low Performance Route","\x1b[0m")
-    console.error()
+    console.log("# Low Performance Route")
     for(let warn of warning){
-        console.error("\x1b[41m"+warn.url+ ":  \x1b[1m"+ warn.avg+"\x1b[0m")
-        console.error()
+        console.log(`**${warn.url}**`+ ":  "+ '`' +warn.avg + '`')
     }
   }
+  console.log('\n' + str);
 })();
