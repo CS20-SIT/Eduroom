40fe776d8d92a6e04c5f1de9155ccbcfa04fffef
note <note16651@hotmail.co.th>
2020-10-29 14:57:38 +0700

    [G09] : [UPDATE] Styles

--- eduroom_frontend/pages/coin-shop/StoreSticker.js
+++ eduroom_frontend/pages/coin-shop/StoreSticker.js
@@ -3,17 +3,21 @@ import Product_Sticker from '../../components/FolderCoin/Sticker_Store';
 import Styles from '../../styles/CoinStyles/coin-shop.module.css';
 import Pop from '../../components/FolderCoin/Pop_up';
 import Main from '../../components/FolderCoin/Main_Store';
+import General from '../../components/template/generalnonav';
 const Temp = () => {
     return (
         <Fragment>
+            <General/>
             <div className={Styles.img3}>
             <div className={Styles.animation2}>
                 <div>
                     <h1>All sticker</h1>
                     <Product_Sticker></Product_Sticker>
+                    
                 </div>
             </div>
             </div>
+
         </Fragment>
     );
 };
