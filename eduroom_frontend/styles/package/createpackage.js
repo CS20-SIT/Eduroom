2a170b13a13073741d1f27a1c382079733fc5439
nuttavadeepj <nuttavadee.pj@mail.kmutt.ac.th>
2020-10-29 14:03:17 +0700

    [G13] : [FIX] Package Management Page

--- eduroom_frontend/styles/package/createpackage.js
+++ eduroom_frontend/styles/package/createpackage.js
@@ -7,39 +7,49 @@ export default css`
     font-weight: bold;
 }
 .container {
-    background-color: #f2ecfe;
-    max-width:75%;
+    background-color: #ffffff;
+    width: 88%;
     margin: auto auto 5% auto;
     border-radius: 10px;
     box-shadow: 0 4px 8px 0 rgba(0,0,0,0.25);
     display: block;
 }
 
-.typebox {
-    padding: 4% 15%;
-}
 input,select,textarea {
-    border: 1px solid #5B5B5B;
+    border: 1px solid #EFF0F6;
     width: 100%;
     padding: 13px 20px;
     border-radius: 10px;
     font-size: 1.2em;
-    outline: none;
     margin-bottom: 25px;
-    display: block;
-    background-color: white;
+    background-color: #EFF0F6;
     cursor: pointer;
+    color: #3d467f;
 }
+input[type=checkbox] {
+    width: 5%;
+    margin: 5px;
+    height: 20px;
+    width: 20px;
+    background-color: #3d467f;
+}
+input:checked {
+    background-color: #3d467f;
+}
+
+::placeholder {
+    color: #3d467f;
+}
+
 .imageupload {
-    height: 100%;
-    border: 1px dashed #B3ABBC;
+    border: 1px dashed #b3abbc;
     border-radius: 5px;
     display: flex;
     justify-content: center;
     align-items: center;
     cursor: pointer;
     color: grey;
-    padding: 5%;
+    padding: 15%;
     background-color: white;
     margin-bottom: 25px;
 }
@@ -50,53 +60,59 @@ input,select,textarea {
     color: #8E8796;
     font-size: 1.4em;
 }
-.text {
+.subtitle {
     color: #3D467F;
     font-style: normal;
     font-weight: 550;
     font-size: 22px; 
-    margin-bottom: 10px;   
+    margin-bottom: 20px;   
 
 }
-.subtitle {
+.text {
     font-size: 20px; 
     padding: 5% 8% 3%;
 }
-.pdetail {
-    padding: 20px;
-    display: block;
-    width: 100%;
-    overflow: auto;
-    cursor: text;
-    border: 1px solid grey;
-}
+
 .coursebox {
     background-color: white;
     border-radius: 5px;
-    border: 1px solid grey;
+    border: 1px solid white;
     margin-bottom: 7%;
-    
+    text-align: center;
+    width: 101.5%;
+   
 }
+::-webkit-scrollbar {
+    width: 10px;
+  }
+  
+::-webkit-scrollbar-thumb {
+    background: white;
+  }
+  
+::-webkit-scrollbar-thumb:hover {
+    background: #d3d4da;
+  }
 .list {
-    border: 1px solid #000000;
+    border: 1px solid #EFF0F6;
     box-sizing: border-box;
-    border-radius: 12px;
-    padding: 5%;
-    margin: 0 8% 5%;
+    border-radius: 10px;
+    padding: 6%;
+    margin-bottom: 2%;
+    background-color: #EFF0F6;
+    text-align: left;
 }
 .createbutton {
-    background: #FFFFFF;
+    background: #FFAAE7;
     border: 3px solid #FFAAE7;
     border-radius: 30px;
     font-size: 22px; 
-    color: #FFAAE7;
-    padding: 10px 90px ;
+    color: white;
+    padding: 13px 90px ;
     font-weight: 550;
     cursor: pointer;
-    margin-bottom: 2%;
+    margin-bottom: 5%;
     
 }
-.center {
-    text-align: center;
-}
+
 `
