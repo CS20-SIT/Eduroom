6bce95c80c4f48180d6cd0d6600c5a904ae3259b
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2020-10-29 11:38:07 +0700

    [ADMIN]:[ADD] Passport auth route

--- eduroom_backend/src/config/passport.js
+++ eduroom_backend/src/config/passport.js
@@ -1,14 +1,12 @@
 const JwtStrategy = require('passport-jwt').Strategy
+const GoogleStrategy = require('passport-google-oauth20').Strategy
 const ExtractJwt = require('passport-jwt').ExtractJwt
 const pool = require('../database/db')
 
-const fromCookieCustom = () => (req) => {
-  let token
-  if (!req.cookies['jwtToken'] || !req.cookies) {
-    return null
-  }
-  if (req && req.cookies && req.cookies['jwtToken'].startsWith('Bearer')) {
-    token = req.cookies['jwtToken'].split(' ')[1]
+const fromCookieCustom = () => (req) => { // tell passport to read JWT from cookies
+  var token = null;
+  if (req && req.cookies){
+      token = req.cookies['jwt']
   }
   return token
 }
@@ -22,10 +20,20 @@ module.exports = (passport) => {
   passport.use(
     new JwtStrategy(jwtOptions, async (jwt_payload, done) => {
       try {
-        return done(null, false)
+        return done(null, jwt_payload)
       } catch (err) {
         return done(err, false)
       }
     })
   )
+  passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_SECRET_KEY,
+    callbackURL: process.env.BACKEND_API + '/api/auth/google/callback'
+  },
+  (accessToken, refreshToken, profile, done) => {
+    // Update/query user in db
+      console.log(JSON.stringify(profile))
+      return done(null, profile)
+  }))
 }
