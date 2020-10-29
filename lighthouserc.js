758874e573b43b2199f6d3374b2ff5f9a146e89e
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2020-10-29 14:17:13 +0700

    [ADMIN]:[ADD] lighthouse ci config

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,7 +1,16 @@
 module.exports = {
   ci: {
     collect: {
-        url: ['https://eduroom.cscms.me']
+      numberOfRuns: 1,
+      headful: false,
+      settings: {
+        emulatedFormFactor: 'desktop'
+      },
+      url: [
+        'https://eduroom.cscms.me', 
+        'https://eduroom.cscms.me/login', 
+        'http://eduroom.cscms.me/register'
+      ]
     },
     upload: {
       target: 'temporary-public-storage',
