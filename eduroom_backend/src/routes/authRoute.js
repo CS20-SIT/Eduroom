c04aa6ca8b3b6e3f500907ff37d112fc5d8285b0
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2020-10-29 11:38:07 +0700

    [ADMIN]:[ADD] Passport auth route

--- eduroom_backend/src/routes/authRoute.js
+++ eduroom_backend/src/routes/authRoute.js
@@ -1,11 +1,42 @@
 const express = require('express')
+const passport = require('passport')
+const jwt = require('jsonwebtoken')
 const router = express.Router()
+
+const jwtSecret = process.env.JWT_SECRET
+const jwtSignOption = {
+    algorithm: 'HS256',
+    expiresIn: process.env.JWT_EXPIRE
+}
 
 router.get('/login', (req,res,next)=>{
     res.status(200).json({success:ture})
 })
 router.get('/register', (req,res,next)=>{
     res.status(200).json({success:ture})
+})
+
+router.get('/google', passport.authenticate('google', { scope: ['profile','email'], session: false }))
+
+router.get('/google/callback', passport.authenticate('google', {session: false}), (req, res) => {
+    console.log('Google Authen');
+    let user = {
+        displayName: req.user.displayName,
+        name: req.user.name.givenName,
+        email: req.user._json.email,
+        provider: req.user.provider }
+    console.log(user)
+    // Find or add user in db
+    const token = jwt.sign({
+        user: 'userid'
+    }, jwtSecret, jwtSignOption)
+
+    res.cookie('jwt', token)
+    res.redirect(process.env.CLIENT_URL)
+})
+
+router.get('/profile', passport.authenticate('jwt', { session: false }) ,(req,res)=>{
+    res.send(`Wellcome user ${JSON.stringify(req.user)}`)
 })
 
 module.exports = router
