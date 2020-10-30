50f7e0187af883aa9dc16be89fd1dda93d106eb1
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2020-10-30 17:51:35 +0700

    [ADMIN]:[ADD] rrtms and throughput throttling

--- lighthouserc.js
+++ lighthouserc.js
@@ -8,6 +8,8 @@ module.exports = {
         throttlingMethod: 'devtools',
         chromeFlags: '--window-size=1440,900',
         throttling: {
+          rttMs: 40,
+          throughputKbps: 10240,
           cpuSlowdownMultiplier: 1
         }
       },
