26a5daed2c3de33a4c1e56082fc89372070906c2
tonkla41 <57616555+tonkla41@users.noreply.github.com>
2020-10-31 18:56:29 +0700

    [ADMIN] : [Merge] G15 to Dev
    * [ADD] wishlist component
    
    * [EDIT] page/user/wishlist
    
    * [EDIT] page/user/index
    
    * [EDIT] page/user/edit
    
    * [G:15] : [EDIT] Wishlist
    
    * [G15] : [EDIT] wishlist & [ADD] MyCourse
    
    Co-authored-by: bunason sutjaritrungsri <bunason.1@mail.kmutt.ac.th>

---
+++ eduroom_frontend/components/user/myCourseForm.js
@@ -0,0 +1,33 @@
+import React, { Fragment } from 'react'
+import Link from 'next/link';
+import Card from '@material-ui/core/Card';
+import Styles from '../../styles/user/wishlistForm.module.css';
+import CardContent from '@material-ui/core/CardContent';
+const MyCourseForm = (props) => {
+    // console.log(props.test);
+    const checkComplete=(boolean)=>{
+        if(boolean) return 'Completed';
+        else return 'Incompleted';
+    }
+  return (
+    <Fragment>
+      <div className={Styles.cardContainer}>
+      <Card>
+      <CardContent>
+        <div  className={Styles.cardContainer}>
+        <div className={Styles.container}>
+          <Link href={'../../course/'+props.id}>
+              <span>
+                  <div>Title: {props.title}</div>
+                  <div>Status: {checkComplete(props.isCompleted)}</div>
+              </span>
+          </Link>
+        </div>
+        </div>
+        </CardContent>
+        </Card>
+        </div>
+    </Fragment>
+  )
+}
+export default MyCourseForm
