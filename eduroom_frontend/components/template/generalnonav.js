fcc9a9fe558d2cb9d8b86491fb21386ea1fd7795
GGolfz <wisarutgolf11@gmail.com>
2020-10-31 17:33:28 +0700

    [ADMIN] : [ADD] General No Sidebar for some Group

--- eduroom_frontend/components/template/generalnonav.js
+++ eduroom_frontend/components/template/generalnonav.js
@@ -1,10 +1,45 @@
 import React, { Fragment } from 'react'
 import Header from '../layouts/header'
+import SideNav from '../layouts/sidenav/sidenav'
 const GeneralNonav = (props) => {
   return (
     <Fragment>
       <Header />
-      <main>{props.children}</main>
+      <div
+        style={{
+          display: 'flex',
+          flex: '1 1 auto',
+          justifyContent: 'space-between',
+          background: '#F4F5F7'
+        }}
+      >
+        <SideNav />
+        <div id="content">
+          <main>{props.children}</main>
+        </div>
+        {props.img ? (
+          <img
+            alt="background-img"
+            src={props.img}
+            className="background-img"
+          />
+        ) : null}
+      </div>
+      <style jsx>
+        {`
+          #content {
+            width: 95%;
+            left: 5%;
+            z-index: 20;
+          }
+          .background-img {
+            position: absolute;
+            bottom: 0;
+            width: 100vw;
+            z-index: 5;
+          }
+        `}
+      </style>
     </Fragment>
   )
 }
