6f8cfdf304b395dd0cd8f48064056c12403c2985
GGolfz <wisarutgolf11@gmail.com>
2020-10-31 20:04:04 +0700

    [ADMIN] : [ADD] 404 Page

---
+++ eduroom_frontend/components/template/error.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react'
+import Header from '../layouts/header'
+import Head from 'next/head'
+import ErrorContent from '../layouts/error'
+const Error = () => {
+    return (
+        <Fragment>
+            <Header/>
+            <Head><title>404: Not Found</title></Head>
+            <ErrorContent/>
+        </Fragment>
+    )
+}
+export default Error;
