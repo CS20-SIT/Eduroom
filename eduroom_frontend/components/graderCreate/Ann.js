b2d1bce941f4ab82dc2c697e8f62f8eeac4ab918
Nutdanai <nutdanai.282@mail.kmutt.ac.th>
2020-11-01 03:03:15 +0700

    [G08] : [Add] Edit Function For Announcement and  FInish Announcement

--- eduroom_frontend/components/graderCreate/Ann.js
+++ eduroom_frontend/components/graderCreate/Ann.js
@@ -2,6 +2,7 @@ import React from "react";
 import AnnDialog from "./AnnCom/AnnDialog";
 import AnnTable from "./AnnCom/AnnTable";
 import { useState, useEffect } from "react";
+import Grid from '@material-ui/core/Grid';
 //prepare for adding abmin log wheen create / edit ann
 const Test = () => {
   const [refresh, setRefresh] = useState(false);
@@ -11,11 +12,36 @@ const Test = () => {
     setRefresh(true);
     setRefresh(false);
   };
+const sBig = {marginLeft:'7.5%',marginRight:'7.5%',marginTop:'2.5%'}
+const sBigTitle ={'font-family': 'Quicksand , sans-serif' ,  'font-size': '2em' ,  color: '#5b5b5b','font-weight': 'bold'
+, 
+}
 
   return (
-    <div>
-      <AnnDialog onSuccess={handleUpdate} />
-      <AnnTable update={refresh} />
+    <div style={sBig}>
+        <Grid  spacing={3} container
+  direction="row"
+  justify="flex-start"
+  alignItems="baseline"
+>
+        <Grid item xl={2} md={3} >
+       
+         <span style={sBigTitle}> Announcement</span>
+        </Grid>
+        <Grid item xl={2} md={3} >
+       <span style={{backgroundColor:'red',paddingTop:7}}>
+        <AnnDialog  onSuccess={handleUpdate} /></span>
+        </Grid>
+       
+        
+        <Grid item xl={12} md={12}>
+        <AnnTable  onSuccess={handleUpdate}  update={refresh} />
+         
+        </Grid>
+      
+      </Grid>
+   
+      
     </div>
   );
 };
