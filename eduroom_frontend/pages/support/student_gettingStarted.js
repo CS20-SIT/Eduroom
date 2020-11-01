4ad7eea257c632ad8de2dc94e4a4992353c69bc9
NungwinPP <parinthon10031@gmail.com>
2020-11-01 22:27:49 +0700

    [G12] : [ADD] support page

---
+++ eduroom_frontend/pages/support/student_gettingStarted.js
@@ -0,0 +1,11 @@
+import {Fragment} from "@material-ui/core"
+import SideNav from "../../components/layouts/sidenav/sidenav";
+const gettingstarted = () => {
+    return(
+        <Fragment>
+<SideNav/>
+        </Fragment>
+    )
+}
+
+export default gettingstarted
