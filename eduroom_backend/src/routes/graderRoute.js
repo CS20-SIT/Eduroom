e2e03147508ac3179a5c86fb03ed4a2a9cec7e00
Nutdanai <nutdanai.282@mail.kmutt.ac.th>
2020-11-01 19:51:06 +0700

    [G08] : [ADD] add template for api

--- eduroom_backend/src/routes/graderRoute.js
+++ eduroom_backend/src/routes/graderRoute.js
@@ -1,9 +1,29 @@
 const express = require('express')
 const router = express.Router()
 const { test, getAnn, postAnn,editAnn } = require('../controllers/graderCreate/test')
+const {  pAdminLog,pContest,pContestAnn,pContestQuestion } = require('../controllers/graderCreate/fame')
+const {  pQuestion,pQuestionTag,pQuestionSample, pQuestionTestcase } = require('../controllers/graderCreate/earth')
 
 router.get('/', test)
 router.get('/ann', getAnn)
 router.post('/cann', postAnn)
 router.put('/eann',editAnn)
+
+
+//fame
+router.post('/cadminlog',pAdminLog)
+router.post('/ccontest',pContest)
+router.post('/ccontestann',pContestAnn)
+router.post('/ccontestquestion',pContestQuestion)
+
+
+
+//earth
+router.post('/cquestion',pQuestion)
+router.post('/cquestiontag',pQuestionTag)
+router.post('/cquestionsample',pQuestionSample)
+router.post('/cquestiontestcase',pQuestionTestcase)
+
+
+
 module.exports = router
