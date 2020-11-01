370f5aa9531b422a8078a7790bddee1dfc411538
thanawat-bcr <thanawat.bcr@gmail.com>
2020-11-01 13:06:00 +0700

    [G01] : [ADD] availability GET/POST

--- eduroom_frontend/pages/user/instructor/tutor/availability.js
+++ eduroom_frontend/pages/user/instructor/tutor/availability.js
@@ -7,18 +7,12 @@ import {
 } from '../../../../components/tutor/lib/utils';
 
 import GeneralNoNav from '../../../../components/template/generalnonav';
-const Temp = () => {
+const Availability = ({ availabilities, price }) => {
   const timeSlotFormatter = (t) => {
     t = t / 6 + 8;
     return timeFormatter(t);
   };
-  const [timeSlots, setTimeSlots] = useState({
-    1: [],
-    2: [],
-    3: [],
-    4: [],
-    5: [],
-  });
+  const [timeSlots, setTimeSlots] = useState(availabilities);
   const timeSelect = (i) => {
     const day = Math.floor(i % 6);
     const time = Math.floor(i / 6 + 8);
@@ -31,13 +25,11 @@ const Temp = () => {
     tmp[4].sort();
     tmp[5].sort();
     setTimeSlots(tmp);
-    console.log(JSON.stringify(timeSlots));
+    // console.log(JSON.stringify(timeSlots));
   };
   const [hoverSlot, setHoverSlot] = useState(-1);
-  const [cost, setCost] = useState(0);
-  const costTyping = () => {
-    console.log(cost);
-  };
+  const [cost, setCost] = useState(price);
+
   return (
     <Fragment>
       <GeneralNoNav>
@@ -53,6 +45,7 @@ const Temp = () => {
                   className='w-full bg-navy text-white py-2 pointer'
                   style={{ textAlign: 'center', borderRadius: 4 + 'px' }}
                   onClick={() => {
+                    // POST /tutor/instructor/availability
                     console.log(timeSlots);
                     console.log(cost);
                   }}
@@ -84,13 +77,6 @@ const Temp = () => {
                       </div>
                     ) : (
                       <div
-                        // className={`flex px-4 py-2 justify-center items-center pointer ${
-                        //   timeSlots[Math.floor(i % 6)].includes(
-                        //     Math.floor(i / 6 + 8)
-                        //   )
-                        //     ? 'bg-red'
-                        //     : 'bg-white-faded'
-                        // } ${hoverSlot == i ? 'bg-pink' : ''} `}
                         className={`flex px-4 py-2 justify-center items-center pointer ${
                           timeSlots[Math.floor(i % 6)].includes(
                             Math.floor(i / 6 + 8)
@@ -128,6 +114,7 @@ const Temp = () => {
                 <div className='text-lg font-bold'>Price per Hour</div>
                 <form className='flex my-2'>
                   <input
+                    value={cost}
                     type='number'
                     min='0'
                     className='border py-1 px-2'
@@ -185,4 +172,18 @@ const Temp = () => {
     </Fragment>
   );
 };
-export default Temp;
+
+export async function getStaticProps() {
+  // GET /tutor/instructor/availability
+  const availabilities = {
+    1: [15, 16],
+    2: [9, 10, 11, 12],
+    3: [11, 12],
+    4: [13, 14, 15],
+    5: [9, 10],
+  };
+  const price = 2000;
+
+  return { props: { availabilities, price } };
+}
+export default Availability;
