43bc6d1cc1a0148893b4a86c7856c34f00ebc209
HyperactiveX <s.chalanthorn@gmail.com>
2020-11-02 14:31:38 +0700

    [G10] : [UPDATE] update from master

--- eduroom_frontend/components/layouts/sidenav/sidenav.js
+++ eduroom_frontend/components/layouts/sidenav/sidenav.js
@@ -1,93 +1,11 @@
-<<<<<<< HEAD
 import React, { Fragment, useState } from "react"
 import Item from "./item"
 import Link from "next/link"
 import style from "../../../styles/layout/sidebar"
 import Image from "next/image"
+import { items } from "./data/item"
 const SideNav = () => {
-  const items = [
-    {
-      icon: "landing",
-      text: "Landing",
-      link: "/",
-    },
-    {
-      icon: "private",
-      text: "Private Tutor",
-      link: "/tutor",
-    },
-    {
-      icon: "learning",
-      text: "Learning Path",
-      link: "/learningpath",
-    },
-    {
-      icon: "course",
-      text: "Course",
-      link: "/course",
-    },
-    {
-      icon: "forum",
-      text: "Forum",
-      link: "/forum",
-    },
-    {
-      icon: "support",
-      text: "Support",
-      link: "/supprt",
-    },
-    {
-      icon: "message",
-      text: "Message",
-      link: "/chat",
-    },
-    {
-      icon: "coin",
-      text: "Coin & Sticker",
-      link: "/coin-shop",
-    },
-    {
-      icon: "coupon",
-      text: "Coupon",
-      link: "/user/coupon",
-    },
-    {
-      icon: "edqiz",
-      text: "Edqiz",
-      link: "/edqiz",
-    },
-    {
-      icon: "grader",
-      text: "Grader",
-      link: "/graderSystem",
-    },
-    {
-      icon: "calendar",
-      text: "Calendar",
-      link: "/calendar",
-    },
-    {
-      icon: "cert",
-      text: "Certificate",
-      link: "/user/certificates",
-    },
-    {
-      icon: "setting",
-      text: "Setting",
-      link: "/user/edit",
-    },
-  ]
   const [expand, setExpand] = useState(false)
-=======
-import React, { Fragment, useState } from 'react';
-import Item from './item';
-import Link from 'next/link';
-import style from '../../../styles/layout/sidebar';
-import Image from 'next/image'
-import {items} from './data/item'
-const SideNav = () => {
-  const [expand, setExpand] = useState(false);
->>>>>>> 44ca582f53a29cec9a3ea9946ced9eee71ea3b21
   const handleExpand = () => {
     document.getElementById("side-nav").style.width = "16%"
     setExpand(true)
@@ -125,12 +43,6 @@ const SideNav = () => {
       </div>
       <style jsx>{style}</style>
     </Fragment>
-<<<<<<< HEAD
   )
 }
 export default SideNav
-=======
-  );
-};
-export default SideNav;
->>>>>>> 44ca582f53a29cec9a3ea9946ced9eee71ea3b21
