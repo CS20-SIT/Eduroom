15ae946ae945d2f729567d054599ef74e477a3d3
Thanaphon Sombunkaeo <ict224bj@gmail.com>
2020-11-03 16:32:45 +0700

    [ADMIN] : [INIT] authen with context

---
+++ eduroom_frontend/contexts/user/userReducer.js
@@ -0,0 +1,23 @@
+import {
+  REGISTER_USER_SUCCESS,
+  REGISTER_USER_FAIL,
+  GET_USER_SUCCESS,
+  GET_USER_FAIL,
+} from './types';
+
+const userReducer = (state, action) => {
+  switch (action.type) {
+    case REGISTER_USER_SUCCESS:
+      return { ...state, user: action.payload };
+    case REGISTER_USER_FAIL:
+      return state;
+    case GET_USER_SUCCESS:
+      return { ...state, user: action.payload };
+    case GET_USER_FAIL:
+      return state;
+    default:
+      return state;
+  }
+};
+
+export default userReducer;
