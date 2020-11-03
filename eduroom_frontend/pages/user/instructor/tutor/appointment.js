dc8b971a03db0a065c5fa880cd60aab987c1bf6c
thanawat-bcr <thanawat.bcr@gmail.com>
2020-11-03 16:47:18 +0700

    [G01] : [REFACTOR] //instructor/tutor/appointment appointment-info

--- eduroom_frontend/pages/user/instructor/tutor/appointment.js
+++ eduroom_frontend/pages/user/instructor/tutor/appointment.js
@@ -9,6 +9,9 @@ import {
 } from '../../../../components/tutor/lib/utils';
 
 import GeneralNoNav from '../../../../components/template/generalnonav';
+
+import AppointmentInfo from '../../../../components/tutor/instructor-appointment/appointment-info';
+
 const Appointment = ({ appointments }) => {
   //   console.log(appointments[0]);
 
@@ -33,96 +36,7 @@ const Appointment = ({ appointments }) => {
   return (
     <Fragment>
       <GeneralNoNav>
-        <CSSTransition
-          mountOnEnter
-          unmountOnExit
-          in={AID > 0}
-          timeout={{ enter: 300, exit: 300 }}
-          classNames={{ enterActive: 'fade-in', exitActive: 'fade-out' }}
-        >
-          <div
-            className='modal-bg'
-            onClick={() => {
-              setAID(-1);
-            }}
-          ></div>
-        </CSSTransition>
-        <CSSTransition
-          mountOnEnter
-          unmountOnExit
-          in={AID > 0}
-          timeout={{ enter: 1000, exit: 1000 }}
-          classNames={{ enterActive: 'slide-in', exitActive: 'slide-out' }}
-        >
-          <div className='modal-container' onClick={() => {}}>
-            <div className='profile--app  my-2'></div>
-            <div className='text-lg font-bold my-2'>{appointment.name}</div>
-            <div className='flex flex-col my-2 w-full'>
-              <div className='my-2 text-lg font-bold'>Date</div>
-              <div className='mx-2 my-1 text-md font-bold text-secondary'>
-                {appointment.date[0]} {monthConverter(appointment.date[1] - 1)}{' '}
-                {appointment.date[2]}
-              </div>
-            </div>
-            <div className='flex flex-col my-2 w-full'>
-              <div className='my-2 text-lg font-bold'>Time</div>
-              <div className='mx-2 my-1 text-md font-bold text-secondary'>
-                {timeFormatter(appointment.startTime) + ' '}-
-                {' ' + timeFormatter(appointment.endTime)}
-              </div>
-            </div>
-            <div className='flex flex-col my-2 w-full'>
-              <div className='my-2 text-lg font-bold'>Members</div>
-              {appointment.members.map((m) => (
-                <div className='flex'>
-                  <div className='profile--inside my-2'></div>
-                  <div className='mx-4 my-auto text-md font-bold text-secondary'>
-                    {m.name}
-                  </div>
-                </div>
-              ))}
-            </div>
-            <div className='my-auto'></div>
-            {appointment.status == 'Pending' ? (
-              <div>
-                <div
-                  style={{ width: '10rem', backgroundColor: '#EB7DB1' }}
-                  className='pointer text-center font-quicksand text-white py-2 rounded-md my-4'
-                  onClick={() => {
-                    // POST /tutor/instructor/appointment -> true
-                    location.reload();
-                    console.log(true);
-                  }}
-                >
-                  Approve
-                </div>
-                <div
-                  style={{ width: '10rem' }}
-                  className='pointer text-center font-quicksand py-2 border-secondary text-secondary bg-white-faded rounded-md'
-                  onClick={() => {
-                    // POST /tutor/instructor/appointment -> false
-                    location.reload();
-                    console.log(false);
-                  }}
-                >
-                  Reject
-                </div>
-              </div>
-            ) : appointment.status == 'Rejected' ? (
-              <div
-                className={`font-bold text-md my-auto font-quicksand text-error my-4`}
-              >
-                {appointment.status}
-              </div>
-            ) : (
-              <div
-                className={`font-bold text-md my-auto font-quicksand text-navy my-4`}
-              >
-                {appointment.status}
-              </div>
-            )}
-          </div>
-        </CSSTransition>
+        <AppointmentInfo AID={AID} setAID={setAID} appointment={appointment} />
 
         <div className='bg-tutor'>
           <div className='container'>
