6594f16f5589cad7fc1f0d1e109151c5c62aa38c
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2020-11-03 22:05:57 +0700

    [ADMIN]:[ADD] Test on testController

--- eduroom_backend/src/controllers/testController.js
+++ eduroom_backend/src/controllers/testController.js
@@ -1,6 +1,7 @@
 const ErrorResponse = require('../utils/errorResponse')
 const pool = require('../database/db')
 
+
 exports.test = async (req, res, next) => {
   const time = await pool.query('SELECT NOW()')
   // res.status(200).json({success:true,msg: 'test from backend',time:time.rows[0]})
