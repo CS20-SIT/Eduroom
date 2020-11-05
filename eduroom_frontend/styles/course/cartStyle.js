a4a85baaf6421e9891c52029bd45835c90efa0f5
TR-NP7 <nataphon2544@gmail.com>
2020-11-05 14:22:28 +0700

    [G13] : [ADD] Course page & component

---
+++ eduroom_frontend/styles/course/cartStyle.js
@@ -0,0 +1,67 @@
+import css from 'styled-jsx/css'
+export default css`
+
+    .background{
+        background-color: #F4F5F7;
+        padding:4%;
+    }
+    .centerBar{
+        font-size: 32px;
+        color: #3D467F;
+        text-align: center;     
+    }
+    .courseInCart{
+        font-size: 20px;
+        color: #3D467F;
+        margin-left: 12%;
+    }
+    .checkoutBox{
+        box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.3);
+                background: white;
+                width: 40vw;
+                height: 30vh;
+                margin-top: 10%;
+                margin-left: auto;
+                margin-right: auto;
+    }
+    .bigDiv{
+        box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.3);
+                background: white;
+                border-radius: 2vh;
+                width: 70vw;
+                height: 20vh;
+        margin-left: auto;
+        margin-right: auto;
+        display: flex;
+        justify-content: start;
+        padding-top:10px;
+        padding-left:10px;
+        background-color: white;
+    }
+
+    .apply{
+        width: 70px;
+        height: 32px;
+        background: #A880F7;
+        font-size: 16px;
+        border: none;
+        color: white;
+        
+    }
+    .inputCoupon {
+        padding: 4px;
+        font-size: 17px;
+        color: #A880F7;
+       
+      }
+    .checkOut{
+        width: 133px;
+        height: 42px;
+        radius: 10px;
+        background: #A880F7;
+        font-size: 16px;
+        border: none;
+        color: white;
+        border-radius: 10px;
+    }
+`
---
+++ eduroom_frontend/styles/course/review.js
@@ -0,0 +1,17 @@
+import css from 'styled-jsx/css'
+export default css`
+    .box{
+        box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.3);
+                background: white;
+                border-radius: 2vh;
+                width: 80vw;
+                height: 100vh;
+                margin-left: auto;
+                margin-right: auto;
+                margin-top: 2%;
+                display: flex;
+                justify-content: start;
+                background-color: white;
+    }
+    
+`
