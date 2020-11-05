0360aba944e508b11a718d51d85fa834a19b942a
Xta-FamEz <thanyapoj.f@mail.kmutt.ac.th>
2020-11-05 23:54:27 +0700

    [G08] : post backend

--- eduroom_backend/src/controllers/graderCreate/fame.js
+++ eduroom_backend/src/controllers/graderCreate/fame.js
@@ -4,14 +4,13 @@ const pool = require('../../database/db')
 
 
 const pAdminLog = async (req, res, next) => {
-    const title = 'mockup data here'
-    const description =  'mockup data here'
+    const title = 'test01'
+    const detail =  'double bruh'
     const adminid = '12345678-1234-1234-1234-123456789123'
-    const visible =  false
   
     await pool.query(
-      'INSERT INTO announcements(title,description,"adminid",isvisible) VALUES ($1 , $2, $3,$4)',
-      [title, description, adminid,visible]
+      'INSERT INTO adminLog(title,detail,"adminid") VALUES ($1 , $2, $3)',
+      [title, detail, adminid]
     )
   
     res.send({ success: true })
@@ -19,13 +18,47 @@ const pAdminLog = async (req, res, next) => {
 
 
 const pContest = async (req, res, next) => {
- 
+  const title = 'test01'
+  const conRuleType =  'oi'
+  const description =  'double bruh'
+  const startTime = '2020-11-05 15:15:07.902379'
+  const endTime = '2020-11-05 15:15:07.90237'
+  const status = true
+  const adminid = '12345678-1234-1234-1234-123456789123'
+
+  await pool.query(
+    'INSERT INTO contest(title,conRuleType,description,startTime,endTime,status,adminid) VALUES ($1 , $2, $3, $4, $5, $6, $7)',
+    [title, conRuleType, description, startTime, endTime, status, adminid]
+  )
+
+  res.send({ success: true })
 }
 const pContestQuestion = async (req, res, next) => {
- 
+    const conQuestionNo = '1'
+    const conid =  '1'
+    const questionId = '1'
+  
+    await pool.query(
+      'INSERT INTO contest_question(conQuestionNo,conid,questionId) VALUES ($1 , $2, $3)',
+      [conQuestionNo, conid, questionId]
+    )
+  
+    res.send({ success: true })
 }
 const pContestAnn = async (req, res, next) => {
- 
+    const title = 'test01'
+    const description =  'double bruh'
+    const conId = '1'
+    const adminId = '12345678-1234-1234-1234-123456789123'
+    const isVisible = true
+    
+
+    await pool.query(
+      'INSERT INTO contest_announcements(title,description,conId,adminId,isVisible) VALUES ($1 , $2, $3, $4, $5)',
+      [title, description, conId,adminId,isVisible]
+    )
+  
+    res.send({ success: true })
 }
 
 
