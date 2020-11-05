6134b8faba362a00d70ac54b47221d6a2297a309
note <note16651@hotmail.co.th>
2020-11-05 11:30:59 +0700

    [G09] : [ADD]Daily reward && [ADD] Default popUp

---
+++ eduroom_frontend/components/FolderCoin/default_Pop.js
@@ -0,0 +1,11 @@
+import Styles from '../../styles/CoinStyles/default_popup.module.css';
+import White from '../FolderCoin/whiteField';
+const temp = () => {
+    return (
+        <div>
+            <White></White>
+            <div className={Styles.div}></div>
+        </div>
+    );
+};
+export default temp;
