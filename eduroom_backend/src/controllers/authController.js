5062caa8fd7053926bce690c91756b4b874ca0b9
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2020-11-05 10:58:02 +0700

    [ADMIN]:[ADD] sendMail verification

--- eduroom_backend/src/controllers/authController.js
+++ eduroom_backend/src/controllers/authController.js
@@ -1,12 +1,13 @@
 const bcrypt = require('bcryptjs')
 const pool = require('../database/db')
 const { generateCookieJWT, generateVerifyJWT, verifyVerificationJWT } = require('../utils/jwt')
+const sendEmail = require('../utils/sendMail')
 
 exports.getProfile = (req, res) => {
     res.send(req.user)
 }
 
-exports.regisController = (req, res) => {
+exports.regisController = async (req, res) => {
     // Find exist email in db
     const user = req.body
     const password = bcrypt.hashSync(req.body.password)
@@ -15,19 +16,24 @@ exports.regisController = (req, res) => {
     // Create verify token and send verification email
     const verifyToken = Buffer.from(generateVerifyJWT(userID)).toString('base64')
     const verifyUrl = `${process.env.BACKEND_API}/api/auth/verify/${verifyToken}`
-    console.log(verifyUrl);
+    const emailOptions = {
+        email: user.email,
+        subject: 'Eduroom Email Verification',
+        message: `Please Verify your email by click at ${verifyUrl}`
+    }
+    await sendEmail(emailOptions)
 
     const token = generateCookieJWT(userID)
     res.cookie('jwt', token)
     res.status(201).send({ success: true })
 }
 
-exports.verifyEmailController = (req, res) => {
+exports.verifyEmailController = async (req, res) => {
     const b64Token = req.params.token
     const jwtToken = Buffer.from(b64Token, 'base64').toString()
-    // Change user status to verified
     const user = verifyVerificationJWT(jwtToken)
-    res.send({validity})
+    // Change user status to verified
+    res.redirect(`${process.env.CLIENT_URL}/login`)
 }
 
 exports.loginController = (req, res) => {
