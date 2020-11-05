6c3dbc202c0d72f50eb20747cb32b381727a9902
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2020-11-05 13:08:13 +0700

    [ADMIN]:[ADD] ErrorHandler in email verification

--- eduroom_backend/src/controllers/authController.js
+++ eduroom_backend/src/controllers/authController.js
@@ -2,18 +2,19 @@ const bcrypt = require('bcryptjs')
 const pool = require('../database/db')
 const { generateCookieJWT, generateVerifyJWT, verifyVerificationJWT } = require('../utils/jwt')
 const sendEmail = require('../utils/sendMail')
+const errorHandler = require('../middleware/error')
 
 exports.getProfile = (req, res) => {
     res.send(req.user)
 }
 
 exports.regisController = async (req, res) => {
-    // Find exist email in db
+    //TODO: Find exist email in db
     const user = req.body
     const password = bcrypt.hashSync(req.body.password)
     const userID = 'userIDfromRegis'
 
-    // Create verify token and send verification email
+    // Create verification token and send it in email
     const verifyToken = Buffer.from(generateVerifyJWT(userID)).toString('base64')
     const verifyUrl = `${process.env.BACKEND_API}/api/auth/verify/${verifyToken}`
     const emailOptions = {
@@ -23,21 +24,27 @@ exports.regisController = async (req, res) => {
     }
     await sendEmail(emailOptions)
 
+    // Generate JWT for user to login
     const token = generateCookieJWT(userID)
     res.cookie('jwt', token)
     res.status(201).send({ success: true })
 }
 
 exports.verifyEmailController = async (req, res) => {
-    const b64Token = req.params.token
-    const jwtToken = Buffer.from(b64Token, 'base64').toString()
-    const user = verifyVerificationJWT(jwtToken)
-    // Change user status to verified
-    res.redirect(`${process.env.CLIENT_URL}/login`)
+    try {
+        const b64Token = req.params.token
+        const jwtToken = Buffer.from(b64Token, 'base64').toString()
+        const user = verifyVerificationJWT(jwtToken)
+        // TODO: Change user status to verified and Redirect user to verification success page
+        res.redirect(`${process.env.CLIENT_URL}/login`) 
+    } catch (error) {
+        // TODO: Should redirect to verification error page
+        errorHandler(error, req, res)
+    }
 }
 
 exports.loginController = (req, res) => {
-    // Find user and compare password using bcrypt
+    //TODO: Find user and compare password using bcrypt
     const userID = 'userIDfromLogin'
     const token = generateCookieJWT(userID)
     res.cookie('jwt', token)
