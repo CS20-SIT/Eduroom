ad6e99f39804b738388bbae5e6919e6bef32fb4f
TR-NP7 <nataphon2544@gmail.com>
2020-11-06 14:11:24 +0700

    [FIX] : Create Package Page

--- eduroom_frontend/pages/user/instructor/course/createpackage.js
+++ eduroom_frontend/pages/user/instructor/course/createpackage.js
@@ -1,57 +1,29 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 import General from '../../../../components/template/general'
 import Upload from '../../../../components/package/imageupload'
 import Courses from '../../../../components/package/courses'
 import style from '../../../../styles/package/createpackage'
 import Link from 'next/link'
-
-const CreatePackage = () => {
+import CreatePackage from '../../../../components/package/createPackage';
+import ConfirmPackage from '../../../../components/package/confirmPackage';
+const CreatePackagePage = () => {
+    const [page,setPage] = useState(1);
+    const renderPage = () => {
+        if(page === 1){
+            return <CreatePackage changePage={(page)=>setPage(page)}/>;
+        } else if(page === 2){
+            return <ConfirmPackage changePage={(page)=>setPage(page)}/>;
+        }
+    }
     return (
         <Fragment>
             <General>
-                <div style={{ backgroundColor: "#f4f5f7" }}>
-                    <div className="package-header">Create New Package</div>
-                    <div style={{ display: "flex", justifyContent: "center" }}>
-                        <div className="container">
-                            <div style={{ padding: "4% 28%" }}>
-                                <div className="subtitle">Package Information</div>
-                                <div><Upload index={0}/></div>
-                                <div><input type="text" placeholder="Package Name" name="name" id="name"></input></div>
-                                <div><select name="price" defaultValue="default">
-                                    <option disabled value="default" >Price</option>
-                                    <option value="399">399</option>
-                                    <option value="499">499</option>
-                                    <option value="599">599</option>
-                                    <option value="699">699</option>
-                                    <option value="799">799</option>
-                                </select></div>
-                                <div>
-                                    <select name="category" defaultValue="default">
-                                        <option disabled value="default" >Category</option>
-                                        <option value="business">Business</option>
-                                        <option value="development">Development</option>
-                                        <option value="software">IT+Software</option>
-                                        <option value="design">Design</option>
-                                        <option value="computer">Computer</option>
-                                    </select>
-                                </div>
-                                <div><textarea placeholder="Package Detail" name="detail" id="pdetail" rows="6" style={{ resize: 'none' }} className="pdetail"></textarea></div>
-                                <div className="subtitle">Courses</div>
-                                <div className="coursebox" style={{ overflow: 'auto', height: '400px' }}>
-                                    <div><Courses /></div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div style={{ display: "flex", justifyContent: "center" }}>
-                        <Link href="/user/instructor/course/confirmcreate">
-                            <button onClick={() => console.log('Clicked')} className="createbutton">Create</button></Link>
-                    </div>
-                </div>
+                {renderPage()}
                 <style jsx>{style}</style>
             </General>
 
         </Fragment>
     )
 }
-export default CreatePackage
+export default CreatePackagePage
+
