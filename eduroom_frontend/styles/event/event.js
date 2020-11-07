f60c48e796903c149d32ed829a75f73e5d47af49
Thanatat Wongabut <thanatat.wong@mail.kmutt.ac.th>
2020-11-07 23:03:54 +0700

    [G14] : [UPDATE] createEvent Page

--- eduroom_frontend/styles/event/event.js
+++ eduroom_frontend/styles/event/event.js
@@ -5,55 +5,128 @@ export default css`
   background-size : cover;
   height : 100vh;
   background-attachment: fixed;
-  
 }
-
 .space-top{
   height : 10vh;
 }
 .margin-p {
   margin-top:25px;
 }
-
 .topic-font{
   font-size : 36px;
   margin : 0px;
   font-weight: bold;
-  text-shadow:  1px 7px 8px #aba8a8;
-
 }
-
 .head{
   position : absolute;
 }
 .form{
   position : absolute;
   margin-top : 260px;
-  align-items: center;
   background: rgba(255, 255, 255, 0.6);
   box-shadow: 6px 4px 19px rgba(0, 0, 0, 0.10);
   border-radius: 10px;
   padding: 30px 20px 30px 20px;
-  height:50vh;
-  width : 130vh;
+  height:55vh;
+  width : 120vh;
+  justify-items: center;
 }
 .event-title{
   width: 482px;
   height : 50px;
-  margin-left: 68px;
-
-  border-color :#A7ABC5;
+  margin-top: 13px;
+  margin-right: 10px;
+  padding: 4%;
+  border: 1px solid #A7ABC5;
   border-radius: 5px;
   opacity : .9;
 }
 .event-type{
   width: 482px;
   height : 50px;
-  margin-left: 16px;
-  border-color :#A7ABC5;
+  margin-top: 13px;
+  padding-left: 3%;
+  border: 1px solid #A7ABC5;
   border-radius: 5px;
   opacity : .9;
-  
+}
+.event-des{
+  width: 976px;
+  height : 121px;
+  padding-left: 20px;
+  padding-right: 20px;
+  padding-top: 20px;
+  padding-bottom: 10%;
+  border: 1px solid #A7ABC5;
+  border-radius: 5px;
+  opacity : .9;
+  margin-top: 16px;
+  resize: none;
+  overflow: hidden;
+}
+.event-startDate{
+  width: 213px;
+  height : 50px;
+  margin-top: 18px;
+  padding: 4%;
+  border: 1px solid #A7ABC5;
+  border-radius: 5px;
+  opacity : .9;
+  margin-right: 40px;
+  margin-left: 75px;
+}
+.event-startTime{
+  width: 213px;
+  height : 50px;
+  margin-top: 18px;
+  padding: 4%;
+  border: 1px solid #A7ABC5;
+  border-radius: 5px;
+  opacity : .9;
+  margin-right: 40px;
+}
+.event-endDate{
+  width: 213px;
+  height : 50px;
+  margin-top: 18px;
+  padding: 4%;
+  border: 1px solid #A7ABC5;
+  border-radius: 5px;
+  opacity : .9;
+  margin-right: 40px;
+}
+.event-endTime{
+  width: 213px;
+  height : 50px;
+  margin-top: 18px;
+  padding: 4%;
+  border: 1px solid #A7ABC5;
+  border-radius: 5px;
+  opacity : .9;
+  margin-right: 40px;
+}
+.startDate{
+  margin-left: 75px;
+}
+.startTime{
+  margin-left: 174px;
+}
+.endDate{
+  margin-left: 174px;
+}
+.endTime{
+  margin-left: 180px;
+}
+.event-place{
+  width: 976px;
+  height : 50px;
+  margin-top: 18px;
+  margin-right: 10px;
+  margin-left: 13px;
+  padding-left:2%;
+  border: 1px solid #A7ABC5;
+  border-radius: 5px;
+  opacity : .9;
 }
 
 .color-topic{
@@ -75,9 +148,6 @@ export default css`
   margin-left: -195px;
   
 }
-
-
-
 .create-textfield {
   background: #eff0f6;
   border-radius: 5px;
@@ -88,65 +158,61 @@ export default css`
   margin-bottom: 5%;
   maring-left : 15px
 }
-
 .create-topic{
   font-weight : bold;
   font-size   : 22px;
   margin : 0px;
   color :#3d467f;
-
 }
-
 .button{
   text-align : center;
   margin-top : 30px;
 }
-.create-button {
+.event-confirmText {
+  color: white;
+  font-weight: 700;
+  font-size: 1.2em;
+  font-family: 'Quicksand', sans-serif;
+
+}
+.event-confirm {
   background: #fe75b7;
   border-radius: 10px;
   padding: 0.5rem 1.5rem;
   border: none;
   outline: none;
   transition: 0.25s;
+  margin-top:55px;
+  margin-right:30px;
+  width:122px;
+  height:39px;
+  align-item:center;
 }
-.create-button-create {
-  color: white;
-  font-weight: 700;
-  font-size: 1.2em;
-  font-family: 'Quicksand', sans-serif;
-}
-.create-button:hover {
+.event-confirm:hover {
   cursor: pointer;
   opacity: 0.8;
   transition: 0.25s;
 }
-
-
-
-.cancel-button {
-  background: #FFFFFF;
+.event-cancelText {
+  color: #3D467F;
+  font-weight: 700;
+  font-size: 1.2em;
+  font-family: 'Quicksand', sans-serif;
+}
+.event-cancel {
+  background: #ffffff;
   border-radius: 10px;
   padding: 0.5rem 1.5rem;
-  
-  outline: none;
+  border: 1px solid #3D467F;
   transition: 0.25s;
+  margin-top:55px;
+  width:122px;
+  height:39px;
+  align-item:center;
 }
-.cancel-button-cancel {
-  border-color : #3D467F;
-  color: 3D467F;
-  font-weight: 700;
-  font-size: 1.2em;
-  font-family: 'Quicksand', sans-serif;
-}
-.cancel-button:hover {
-  background-color :#3D467F;
+.event-cancel:hover {
   cursor: pointer;
   opacity: 0.8;
   transition: 0.25s;
-  color : #FFFFFF
 }
-
-
-
-
 `;
