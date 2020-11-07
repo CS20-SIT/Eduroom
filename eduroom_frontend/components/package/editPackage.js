558f13ad6af511be264603944bfb0b81abf6329f
nuttavadeepj <nuttavadee.pj@mail.kmutt.ac.th>
2020-11-07 18:50:55 +0700

    [UPDATE] Package Management Page

--- eduroom_frontend/components/package/editPackage.js
+++ eduroom_frontend/components/package/editPackage.js
@@ -3,11 +3,11 @@ import Upload from './imageupload'
 import Courses from './courses'
 import style from '../../styles/package/createpackage'
 
-const CreatePackage = (props) => {
+const EditPackage = (props) => {
     return (
         <Fragment>
             <div style={{ backgroundColor: "#f4f5f7" }}>
-                <div className="package-header">Create New Package</div>
+                <div className="package-header">Edit Package</div>
                 <div style={{ display: "flex", justifyContent: "center" }}>
                     <div className="container">
                         <div style={{ padding: "4% 15%" }}>
@@ -22,7 +22,6 @@ const CreatePackage = (props) => {
                                     <div>
                                         <select name="price" defaultValue="default">
                                             <option disabled value="default" >Discount</option>
-                                            <option value="5">5%</option>
                                             <option value="10">10%</option>
                                             <option value="20">20%</option>
                                             <option value="30">30%</option>
@@ -56,7 +55,7 @@ const CreatePackage = (props) => {
                 </div>
                 <div style={{ display: "flex", justifyContent: "center",marginBottom: '5%' }}>
 
-                    <button onClick={() => props.changePage(2)} className="createbutton">Create</button>
+                    <button onClick={() => props.changePage(2)} className="createbutton">Edit</button>
                 </div>
             </div>
             <style jsx>{style}</style>
@@ -65,4 +64,4 @@ const CreatePackage = (props) => {
         </Fragment>
     )
 }
-export default CreatePackage
+export default EditPackage
--- eduroom_frontend/pages/user/instructor/course/index.js
+++ eduroom_frontend/pages/user/instructor/course/index.js
@@ -5,23 +5,23 @@ import Link from 'next/link'
 import Ownpackage from '../../../../components/package/ownpackage'
 
 const Index = () => {
-
     return (
         <Fragment>
             <General>
                 <div style={{ backgroundColor: "#f4f5f7" }}>
-                    <div className="package-header">Package Management</div>
+                    <div className="package-header">Package Management </div>
                     <div style={{ display: "flex", justifyContent: "center" }}>
                         <div className="container" >
                             {/* <div className="package-content">You Have Not Create Package Yet</div> */}
+                            <Ownpackage />
                             <Ownpackage />
                             <div style={{ display: "flex", justifyContent: "center" }}>
                                 <Link href="/user/instructor/course/createpackage">
                                     <button className="addpackbutton">
                                         <i className="fas fa-plus-circle"></i>
                                         <br></br>
-            Add new package
-          </button>
+                                        Add new package
+                                    </button>
                                 </Link>
                             </div>
                         </div>
@@ -32,4 +32,7 @@ const Index = () => {
         </Fragment>
     )
 }
+
+
 export default Index
+
