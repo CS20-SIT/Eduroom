96371173517f717298986cafa71fa61386c6db68
TR-NP7 <nataphon2544@gmail.com>
2020-11-09 16:12:19 +0700

    [G13] : [UPDATE] cart bg + review bg

--- eduroom_frontend/pages/course/[id]/review.js
+++ eduroom_frontend/pages/course/[id]/review.js
@@ -1,12 +1,16 @@
 import React, { Fragment } from 'react'
 import Review from '../../../components/course/review'
 import GeneralNoNav from '../../../components/template/generalnonav'
+import style from "../../../styles/course/review";
 const review = () => {
     return(
     <Fragment>  
         <GeneralNoNav>
-        <Review/>
+            <div className="bg">
+                <Review/>
+            </div>
         </GeneralNoNav>     
+        <style jsx>{style}</style>
     </Fragment>
     )
 }
