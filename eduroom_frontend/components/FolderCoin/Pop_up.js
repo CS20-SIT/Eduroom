7a5f1e33ad6bf633456500f208276222815962cf
Chingly <kavisara.noya@mail.kmutt.ac.th>
2020-11-11 13:31:08 +0700

    [G09]: [UPDATE] Style Payment

--- eduroom_frontend/components/FolderCoin/Pop_up.js
+++ eduroom_frontend/components/FolderCoin/Pop_up.js
@@ -4,14 +4,30 @@ const temp = (props) => {
     //     this.props.toggle();
     // };
     return (
-        
-        <div>
-        <h1>
-            Name Sticker : {props.title}
-            Price : {props.price}
-        </h1>
+
+        <div className={Styles.setPayment}>
+            <div>
+                <img className={Styles.showPhoto} src='../../images/Coin-image/Icon_Peach.svg' />
+                <div className={Styles.setMoney}>
+                <img className={Styles.coinPhoto} src='../../images/Coin-image/icon_coin_back.svg' />
+                <p className={Styles.priceView}>{props.price}</p>
+                </div>
+            </div>
+            <div className={Styles.title}>
+
+                <p >
+                    {props.title}
+                </p>
+                <p>Neque porro quisquam est qui dolorem ipsum
+                quia dolor sit amet, consectetur, adipisci</p>
+                <p>No expiration Date</p>
+                <p>
+                    Price : {props.price}
+                </p>
+                <p>My coins: 99</p>
+            </div>
         </div>
-    
+
     );
 }
 export default temp;
