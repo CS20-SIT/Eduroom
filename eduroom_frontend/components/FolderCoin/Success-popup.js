8c772babc5b98ad67c87d8e4a9af40116edc0f05
Chingly <kavisara.noya@mail.kmutt.ac.th>
2020-11-14 00:33:46 +0700

    [G09] : [ADD] pop-up ensure and success

--- eduroom_frontend/components/FolderCoin/Success-popup.js
+++ eduroom_frontend/components/FolderCoin/Success-popup.js
@@ -1,4 +1,15 @@
+import Card from '@material-ui/core/Card';
+import Styles from '../../styles/CoinStyles/ensurePayment.module.css';
+const successPopup = () => {
+    return (
 
-const successPopup= () =>{
-
+        <div className={Styles.setMiddle}>
+            <h1>
+                PURCHASED !
+            </h1>
+            <img className={Styles.photoPurchase} src='../../images/Coin-image/paypal 1.svg' />
+            <button className={Styles.btn}>Go to Sticker Store</button>
+        </div>
+    );
 }
+export default successPopup;
