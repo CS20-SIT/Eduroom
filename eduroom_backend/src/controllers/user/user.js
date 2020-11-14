941b02c23d8e1416b3521492d7df2cab20c6e7d4
bunason sutjaritrungsri <bunason.1@mail.kmutt.ac.th>
2020-11-15 03:12:57 +0700

    [G15][EDIT]Synchronize FrontBack:Wishlist&MyCourse

--- eduroom_backend/src/controllers/user/user.js
+++ eduroom_backend/src/controllers/user/user.js
@@ -8,44 +8,45 @@ const test = async (req, res, next) => {
   return
 }
 
-const getAnn = async (req, res, next) => {
-  const data = await pool.query('select * from announcements order by 1 DESC ')
-  const ann = data.rows
-  res.send(ann)
-}
-const postAnn = async (req, res, next) => {
-  const title = req.body.title
-  const description = req.body.description
-  const adminid = req.body.adminid
-
-  await pool.query(
-    'INSERT INTO announcements(title,description,"adminid") VALUES ($1 , $2, $3)',
-    [title, description, adminid]
-  )
-
-  res.send({ success: true, title, description, adminid })
-}
-
+// const user="08e9d239-b3f2-4db8-b29a-da99a314df92";
 const getWishlist = async (req, res, next) => {
+    const user=req.body.userid;
+    const condition=req.body.condition;
+    // const condition='';
+    const orderby=req.body.orderby;
+    // const orderby='addtime desc';
     const data = await pool.query(
-    'select w.userid,w.courseid,addtime,coursename,coursepicture,price,p.firstname,p.lastname'+
-    'from user_wishlist w'+
-    'inner join course c on c.courseid=w.courseid'+
-    'inner join instructor i on i.instructorid=c.ownerid'+
-    'inner join user_profile p on i.userid=p.userid'+
-    'order by addtime desc;')
+    'select w.userid,w.courseid,addtime,coursename,coursepicture,price,p.firstname,p.lastname '+
+    'from user_wishlist w '+
+    'inner join course c on c.courseid=w.courseid '+
+    'inner join instructor i on i.instructorid=c.ownerid '+
+    'inner join user_profile p on i.userid=p.userid '+
+    'where w.userid=$1 '+condition+
+    'order by '+orderby+'; ',
+    [user])
     const ann = data.rows
     res.send(ann)
 }
 
 const getMycourse = async (req, res, next) => {
+    const user=req.body.userid;
+    const finish=req.body.finish;
+    const condition=req.body.condition;
+    const orderby=req.body.orderby;
+
+    // const user='08e9d239-b3f2-4db8-b29a-da99a314df92';
+    // const condition='';
+    // const orderby='addtime desc';
+
     const data = await pool.query(
-    'select m.userid,m.courseid,addtime,lastvisit,isfinished,coursename,coursepicture,p.firstname,p.lastname'+
-    'from user_mycourse m'+
-    'inner join course c on c.courseid=m.courseid'+
-    'inner join instructor i on i.instructorid=c.ownerid'+
-    'inner join user_profile p on i.userid=p.userid'+
-    'order by addtime desc;')
+    'select m.userid,m.courseid,addtime,lastvisit,isfinished,coursename,coursepicture,p.firstname,p.lastname '+
+    'from user_mycourse m '+
+    'inner join course c on c.courseid=m.courseid '+
+    'inner join instructor i on i.instructorid=c.ownerid '+
+    'inner join user_profile p on i.userid=p.userid '+
+    'where m.userid=$1 and isfinished=$2 '+condition+
+    'order by '+orderby+'; ',
+    [user,finish])
     const ann = data.rows
     res.send(ann)
 }
@@ -60,4 +61,4 @@ const postDeleteWishlist = async (req, res, next) => {
     res.send({ success: true})
   }
 
-module.exports = { getAnn, test, postAnn,getWishlist,getMycourse,postDeleteWishlist}
+module.exports = { test,getWishlist,getMycourse,postDeleteWishlist}
--- eduroom_backend/src/routes/userRoute.js
+++ eduroom_backend/src/routes/userRoute.js
@@ -2,8 +2,8 @@ const express = require('express')
 const router = express.Router()
 const {getWishlist,getMycourse,postDeleteWishlist}= require('../controllers/user/user')
 
-router.get('/getWishlist', getWishlist)
-router.get('/getMycourse', getMycourse)
+router.post('/getWishlist', getWishlist)
+router.post('/getMycourse', getMycourse)
 router.post('/postDeleteWishlist', postDeleteWishlist)
 
 module.exports = router
