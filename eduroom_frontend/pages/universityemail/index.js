fa6183c5fa9856e430154e1e1df996d0d1f4c554
Sakkarin <sakkarin.kmutt@mail.kmutt.ac.th>
2020-11-14 17:56:27 +0700

    Add insert university email

--- eduroom_frontend/pages/universityemail/index.js
+++ eduroom_frontend/pages/universityemail/index.js
@@ -1,6 +1,8 @@
-import React,{Fragment} from 'react'
+import React,{Fragment,useEffect} from 'react'
 import Uregister from '../../components/UniversityEmail/Uregister'
+
 const RegisterPage = () => {
+    
     return <Fragment>
         <Uregister />
     </Fragment>
