f2564936588cf4fd59e0b7ac4a21d7468c39583f
Supawit <jimsupawitzz@gmail.com>
2020-11-14 19:54:24 +0700

    [FIXED]: Error Chat Route

--- eduroom_backend/src/routes/chatRoute.js
+++ eduroom_backend/src/routes/chatRoute.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const router = express.Router()
-const {getChatlist, getMessagelist} = require('../controllers/chatController')
+const {getChatlist, getGroupPicture} = require('../controllers/chatController')
 
 let test = (req, res) => {
     res.send('test')
   }
 router.get('/getChatlist', getChatlist);
-router.get('/getMessagelist', getMessagelist);
+router.get('/getGroupPicture', getGroupPicture);
 
 
 module.exports = router
