24d2045a0a4031db89232a8c5abde5a6c0930348
coolkidneversleep <iiippbhy@gmail.com>
2020-11-16 17:55:07 +0700

    [G12]:[ADD] room page

--- eduroom_backend/src/controllers/forumController.js
+++ eduroom_backend/src/controllers/forumController.js
@@ -11,23 +11,38 @@ exports.forumTest = async (req, res, next) => {
 };
 
 exports.showForum = async (req, res, next) => {
-  const data = await pool.query("select * from forum_form");
+  const data = await pool.query("select forumid, titlethread, userid, posttime, subtypename, typename, c.categorytypeid from forum_form f , category_type c , sub_category s where f.subcategoryiid = s.subcategoryiid and s.categorytypeid = c.categorytypeid ;");
   const forum = data.rows;
   res.status(200).json({ success: true, data: forum });
 };
 exports.selectForum = async (req, res, next) => {
-  const id = req.params.id;
+  const id = req.query.id;
   
   const data = await pool.query("select * from forum_form where forumid= $1", [
     id,
   ]);
-  const data2 = await pool.query("select * from forum_answer_from where forumid= $1", [
+  const data2 = await pool.query("select * from forum_answer_form where forumid= $1", [
     id,
   ]);
   const forum = data.rows;
   const comment = data2.rows;
   console.log(forum);
   res.status(200).json({ success: true, data: {forum,comment} });
+};
+exports.room = async (req,res,next) =>{
+  const data = await pool.query("select * from category_type")
+  const room = data.rows;
+  res.status(200).json({ success: true, data: room });
+}
+exports.selectRoom = async (req, res, next) => {
+  const roomname = req.query.roomname;
+  const data = await pool.query("select titlethread, userid, posttime, subtypename from forum_form f , category_type c , sub_category s where f.subcategoryiid = s.subcategoryiid and c.categorytypeid=s.categorytypeid and c.typename = $1",
+   [
+    roomname,
+  ]);
+  const forum = data.rows;
+  res.status(200).json({ success: true, data: forum });
+  console.log(forum);
 };
 
 exports.createForum = async (req, res, next) => {
@@ -38,11 +53,11 @@ exports.createForum = async (req, res, next) => {
 exports.createComment = async (req, res, next) => {
   const temp = req.body;
   // const ansno = { no: "3" };
-  const result = await pool.query('SELECT MAX(answerno) as answerno from forum_answer_from');
+  const result = await pool.query('SELECT MAX(answerno) as answerno from forum_answer_form');
   const ansno = result.rows[0].answerno+1;
   const user = { uid: "123e4567-e89b-12d3-a456-426614174000" };
   const data = await pool.query(
-    "insert into forum_answer_from (forumid, answerno, userid, anstime, answer ) values($1,$2,$3,current_timestamp,$4)",
+    "insert into forum_answer_form (forumid, answerno, userid, anstime, answer ) values($1,$2,$3,current_timestamp,$4)",
     [temp.id, ansno, user.uid, temp.comment]
   );
   const forum = data.rows;
@@ -52,7 +67,7 @@ exports.createComment = async (req, res, next) => {
 exports.setForum = async (req, res, next) => {
   const temp = req.body;
   // const user = req.user ;
-  const forumid = { id: "6" };
+  // const forumid = { id: "8" };
   const user = { uid: "123e4567-e89b-12d3-a456-426614174000" };
   const subcat = await pool.query(
     "SELECT subcategoryiid FROM sub_category WHERE subtypename = $1",
@@ -61,8 +76,8 @@ exports.setForum = async (req, res, next) => {
   if (subcat.rowCount > 0) {
     const subcatID = subcat.rows[0].subcategoryiid;
     const data = await pool.query(
-      "insert into forum_form (forumid, userid, posttime, titlethread, subcategoryiid, content) values($1,$2,current_timestamp,$3,$4,$5)",
-      [forumid.id, user.uid, temp.title, subcatID, temp.content]
+      "insert into forum_form ( userid, posttime, titlethread, subcategoryiid, content) values($1,current_timestamp,$2,$3,$4)",
+      [user.uid, temp.title, subcatID, temp.content]
     );
     const forum = data.rows;
     res.status(200).json({ success: true, data: forum });
