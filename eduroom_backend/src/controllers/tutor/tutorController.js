82e34a74d9cb0df5c3e176dd29a44f7febdcdf26
thanawat-bcr <thanawat.bcr@gmail.com>
2020-11-17 23:27:21 +0700

    [G01] : [UPDATE] id hardcode -> query

--- eduroom_backend/src/controllers/tutor/tutorController.js
+++ eduroom_backend/src/controllers/tutor/tutorController.js
@@ -104,11 +104,11 @@ const getInstructorInfo = async (req, res) => {
                  */
 
 		// ID : instructorID
-		// const { id } = req.query
-		// console.log(id)
+		const { id } = req.query
+		console.log('id', id)
 
 		// ID : hardcode
-		const id = '9e6cfde7-af2c-4f56-b76e-2c68d97e847f'
+		// const id = '9e6cfde7-af2c-4f56-b76e-2c68d97e847f'
 
 		let result = await pool.query(`select i.instructorid,u.firstname,u.lastname,e.subjectname, i.biography,p.price from instructor_availabilities_price p, instructor i, user_profile u, instructor_expert e
         where i.instructorid = '${id}' and p.instructorid = i.instructorid and i.userid = u.userid and e.instructorid = p.instructorid;
@@ -151,43 +151,80 @@ const getInstructorInfo = async (req, res) => {
 		res.status(404).send(e)
 	}
 }
-const getStudentAppointments = async (req, res) => {
+const getInstructorReview = async (req, res) => {
 	try {
-		// ID from cookies
-		const id = '44f8e863-226c-4bed-9556-aa6e1600d3bc'
+		// ID : instructorID
+		const { id } = req.query
+		console.log(id)
+
+		// ID : hardcode
+		// const id = '9e6cfde7-af2c-4f56-b76e-2c68d97e847f'
 
 		let result = await pool.query(`
-        select a.appointmentid,u.firstname , u.lastname ,e.subjectname,date_part('hour', a.starttime) as starttime,date_part('hour', a.endtime) as endtime, to_char( starttime, 'DD MON YYYY') as date, a.status
-            from  instructor i, user_profile u, instructor_expert e, instructor_appointments a,instructor_appointment_members m
-            where i.instructorid = a.instructorid
-            and i.userid = u.userid
-            and a.appointmentid = m.appointmentid
-            and m.userid = '${id}'
-            and e.instructorid = i.instructorid
-            order by a.appointmentid;
-        `)
-		appointment = result.rows
+        select m.score, m.description, u.firstname, u.lastname,to_char( starttime, 'YYYYMMDD') as date
+        from instructor_appointments a, instructor_appointment_members m, user_profile u
+        where a.appointmentid = m.appointmentid
+          and a.instructorid = '${id}'
+          and m.score is not null
+          and u.userid = m.userid`)
 
-		const appointments = []
-		appointment.forEach((a) => {
-			const starttime = a.starttime > 9 ? '' + a.starttime + ':00' : '0' + a.starttime + ':00'
-			const endtime = a.endtime > 9 ? '' + a.endtime + ':00' : '0' + a.endtime + ':00'
-			let isAgree = 'Pending'
-			if (a.status === true) isAgree = 'Approved'
-			if (a.status === false) isAgree = 'Rejected'
+		rates = result.rows
+		const rating = []
+		rates.forEach((r) => {
 			let tmp = {
-				id: a.appointmentid,
-				name: a.firstname + ' ' + a.lastname,
-				info: a.subjectname,
-				date: a.date,
-				starttime,
-				endtime,
-				isAgree,
+				score: r.score,
+				desc: r.description,
+				name: r.firstname + ' ' + r.lastname,
+				date: r.date,
 			}
-			appointments.push(tmp)
+			rating.push(tmp)
+		})
+		res.status(200).send({ rating })
+	} catch (e) {
+		res.status(404).send(e)
+	}
+}
+const getInstructorAvailability = async (req, res) => {
+	try {
+		// id, dates : instructorID + dd-mm-yyyy
+		const { id, dates } = req.query
+		// console.log('INSTRUCTOR AVAILABILITTY')
+		// console.log(id, dates)
+
+		// name : hardcode
+		// const id = '9e6cfde7-af2c-4f56-b76e-2c68d97e847f'
+		// const dates = '2020-11-17'
+
+		const date = dates.split('-')
+		const start = `${date[0]}-${date[1]}-${date[2]}`
+		const end = `${date[0]}-${date[1]}-${parseInt(date[2]) + 1}`
+		const d = new Date(date[0], date[1] - 1, date[2])
+		const day = d.getDay() - 1
+
+		let result = await pool.query(`
+        select date_part('hour', starttime) as starttime, date_part('hour', endtime) as endtime
+        from instructor_appointments
+        where starttime >= timestamp '${start} 00:00:00'
+          and starttime < timestamp '${end} 00:00:00'
+          and instructorid = '${id}';`)
+		sameDay = result.rows
+
+		result = await pool.query(`
+        select time from instructor_availabilities
+        where day = '${day}'
+          and instructorid = '${id}';`)
+		const times = []
+		result.rows.forEach((r) => {
+			times.push(r.time)
 		})
 
-		res.status(200).send({ appointments })
+		sameDay.forEach((d) => {
+			const removal = parseInt(d.endtime) - parseInt(d.starttime)
+			const removeIndex = times.indexOf(d.starttime)
+			times.splice(removeIndex, removal)
+		})
+
+		res.status(200).send({ times })
 	} catch (e) {
 		res.status(404).send(e)
 	}
@@ -257,78 +294,43 @@ const getInstructorAppointments = async (req, res) => {
 		res.status(404).send(e)
 	}
 }
-const getInstructorReview = async (req, res) => {
+const getStudentAppointments = async (req, res) => {
 	try {
-		// ID : instructorID
-		// const { id } = req.query
-		// console.log(id)
+		// ID from cookies
+		const id = '44f8e863-226c-4bed-9556-aa6e1600d3bc'
 
-		// ID : hardcode
-		const id = '9e6cfde7-af2c-4f56-b76e-2c68d97e847f'
 		let result = await pool.query(`
-        select m.score, m.description, u.firstname, u.lastname,to_char( starttime, 'YYYYMMDD') as date
-        from instructor_appointments a, instructor_appointment_members m, user_profile u
-        where a.appointmentid = m.appointmentid
-          and a.instructorid = '${id}'
-          and m.score is not null
-          and u.userid = m.userid`)
+        select a.appointmentid,u.firstname , u.lastname ,e.subjectname,date_part('hour', a.starttime) as starttime,date_part('hour', a.endtime) as endtime, to_char( starttime, 'DD MON YYYY') as date, a.status
+            from  instructor i, user_profile u, instructor_expert e, instructor_appointments a,instructor_appointment_members m
+            where i.instructorid = a.instructorid
+            and i.userid = u.userid
+            and a.appointmentid = m.appointmentid
+            and m.userid = '${id}'
+            and e.instructorid = i.instructorid
+            order by a.appointmentid;
+        `)
+		appointment = result.rows
 
-		rates = result.rows
-		const rating = []
-		rates.forEach((r) => {
+		const appointments = []
+		appointment.forEach((a) => {
+			const starttime = a.starttime > 9 ? '' + a.starttime + ':00' : '0' + a.starttime + ':00'
+			const endtime = a.endtime > 9 ? '' + a.endtime + ':00' : '0' + a.endtime + ':00'
+			let isAgree = 'Pending'
+			if (a.status === true) isAgree = 'Approved'
+			if (a.status === false) isAgree = 'Rejected'
 			let tmp = {
-				score: r.score,
-				desc: r.description,
-				name: r.firstname + ' ' + r.lastname,
-				date: r.date,
+				id: a.appointmentid,
+				name: a.firstname + ' ' + a.lastname,
+				info: a.subjectname,
+				date: a.date,
+				starttime,
+				endtime,
+				isAgree,
 			}
-			rating.push(tmp)
-		})
-		res.status(200).send({ rating })
-	} catch (e) {
-		res.status(404).send(e)
-	}
-}
-const getInstructorAvailability = async (req, res) => {
-	try {
-		// id, dates : instructorID + dd-mm-yyyy
-		// const { id, dates } = req.query
-		// console.log(id, dates)
-
-		// name : hardcode
-		const id = '9e6cfde7-af2c-4f56-b76e-2c68d97e847f'
-		const dates = '2020-12-16'
-
-		const date = dates.split('-')
-		const start = `${date[0]}-${date[1]}-${date[2]}`
-		const end = `${date[0]}-${date[1]}-${parseInt(date[2]) + 1}`
-		const d = new Date(date[0], date[1] - 1, date[2])
-		const day = d.getDay() - 1
-
-		let result = await pool.query(`
-        select date_part('hour', starttime) as starttime, date_part('hour', endtime) as endtime
-        from instructor_appointments
-        where starttime >= timestamp '${start} 00:00:00'
-          and starttime < timestamp '${end} 00:00:00'
-          and instructorid = '${id}';`)
-		sameDay = result.rows
-
-		result = await pool.query(`
-        select time from instructor_availabilities
-        where day = '${day}'
-          and instructorid = '${id}';`)
-		const times = []
-		result.rows.forEach((r) => {
-			times.push(r.time)
+			appointments.push(tmp)
 		})
 
-		sameDay.forEach((d) => {
-			const removal = parseInt(d.endtime) - parseInt(d.starttime)
-			const removeIndex = times.indexOf(d.starttime)
-			times.splice(removeIndex, removal)
-		})
-
-		res.status(200).send({ times })
+		res.status(200).send({ appointments })
 	} catch (e) {
 		res.status(404).send(e)
 	}
