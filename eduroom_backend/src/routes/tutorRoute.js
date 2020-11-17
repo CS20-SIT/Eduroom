4f862fab79eb2466c80935ebb178d8ddf4a94cd2
thanawat-bcr <thanawat.bcr@gmail.com>
2020-11-17 23:27:21 +0700

    [G01] : [UPDATE] id hardcode -> query

--- eduroom_backend/src/routes/tutorRoute.js
+++ eduroom_backend/src/routes/tutorRoute.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const router = express.Router()
 
+// middleware
+// const { jwtAuthenicate } = require('../middleware/jwtAuthenticate')
+
 const {
 	getInstructorAvailabilities,
 	getInstructorList,
