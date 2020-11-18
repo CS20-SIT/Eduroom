c52a070f624e3fd9ce27c50b8fb2199c06a6111d
GGolfz <wisarutgolf11@gmail.com>
2020-11-18 23:22:52 +0700

    [ADMIN] : [ADD] Forum Categories

---
+++ eduroom_frontend/components/forum/layout/categoriesSet.js
@@ -0,0 +1,38 @@
+import React,{Fragment} from 'react'
+import CategoriesBox from './categoriesBox'
+
+const CategoriesSet = () => {
+    const categories = ["Mathematic","Science","Language","Room4","Room5","Room6","Room7"]
+    return (
+        <Fragment>
+            <div className="categoriesSet">
+            {
+                categories.map(el=>{
+                    return (
+                        <div className="categoriesItems">
+                            <CategoriesBox content={el} />
+                        </div>
+                    )
+                })
+            }</div>
+            <style jsx>
+                {
+                    `
+                    .categoriesSet {
+                        display:flex;
+                        justify-content:center;
+                        padding: 2rem;
+                    }
+                    .categoriesItems {
+                        display:flex;
+                        justify-content:center;
+                        padding: 0 .5rem;
+                    }
+                    
+                    `
+                }
+            </style>
+        </Fragment>
+    )
+}
+export default CategoriesSet
