1d5958b870259e12e51518bbb4fec786744edced
note <note16651@hotmail.co.th>
2020-11-18 16:24:46 +0700

    [G:09] : [UPDATE] Styles stickerstore && [G:02] : [Update] POP UP

--- eduroom_frontend/pages/coin-shop/test.js
+++ eduroom_frontend/pages/coin-shop/test.js
@@ -2,20 +2,39 @@ import { useEffect } from 'react'
 import Styles from '../../styles/CoinStyles/test.module.css'
 
 const temp = (props) => {
-	// useEffect(() => {
-	// 	createBalloons(100)
-	// }, [])
-	// function createBalloons(num) {
-	// 	var balloonContainer = document.getElementById('balloon-container')
-	// 	console.log(balloonContainer)
-	// 	for (var i = num; i > 0; i--) {
-	// 		var balloon = document.createElement('div')
-	// 		balloon.className = Styles.balloon
-	// 		balloon.style.cssText = getRandomStyles()
-	// 		balloonContainer.append(balloon)
-	// 	}
-	// 	console.log(balloonContainer)
-	// }
+	useEffect(() => {
+		createBalloons(100)
+	}, [])
+	function random(num) {
+		return Math.floor(Math.random() * num)
+	}
+	function createBalloons(num) {
+		var balloonContainer = document.getElementById('balloon-container')
+		console.log(balloonContainer)
+		for (var i = num; i > 0; i--) {
+			var balloon = document.createElement('div')
+			balloon.className = Styles.balloon
+			balloon.style.cssText = getRandomStyles()
+			balloonContainer.append(balloon)
+		}
+		console.log(balloonContainer)
+	}
+	function getRandomStyles() {
+		var r = random(255)
+		var g = random(255)
+		var b = random(255)
+		var mt = random(200)
+		var ml = random(50)
+		var dur = random(5) + 5
+		console.log(dur)
+		return `
+                background-color: rgba(${r},${g},${b},0.7);
+                color: rgba(${r},${g},${b},0.7); 
+                box-shadow: inset -7px -3px 10px rgba(${r - 10},${g - 10},${b - 10},0.7);
+                margin: ${mt}px 0 0 ${ml}px;
+                animation: float ${dur}s ease-in infinite;
+                `
+	}
 
 	return (
 		<div>
