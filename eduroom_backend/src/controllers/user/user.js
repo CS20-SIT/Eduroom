0b8217d74011ff583b98eddf3307e52f49fde2db
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2020-11-19 21:09:48 +0700

    [ADMIN]:[FIX] User routes and controller

--- eduroom_backend/src/controllers/user/user.js
+++ eduroom_backend/src/controllers/user/user.js
@@ -2,17 +2,16 @@ const bcrypt = require('bcryptjs')
 const errorHandler = require('../../middleware/error')
 const pool = require('../../database/db')
 
-const test = async (req, res, next) => {
+const test = async (req, res) => {
 	const time = await pool.query('SELECT NOW()')
 
-	res.send({ success: true })
-	return
+	res.send({ time })
 }
 
 // const user="08e9d239-b3f2-4db8-b29a-da99a314df92";
-const getWishlist = async (req, res, next) => {
+const getWishlist = async (req, res) => {
 	try {
-		const user = '08e9d239-b3f2-4db8-b29a-da99a314df92'
+		const user = req.user.id
 		// const user = req.user.id
 		const condition = req.query.condition
 		// const condition='';
@@ -20,20 +19,20 @@ const getWishlist = async (req, res, next) => {
 		// const orderby='addtime desc';
 		const data = await pool.query(
 			'select w.userid,w.courseid,addtime,coursename,coursepicture,price,p.firstname,p.lastname ' +
-			'from user_wishlist w ' +
-			'inner join course c on c.courseid=w.courseid ' +
-			'inner join instructor i on i.instructorid=c.ownerid ' +
-			'inner join user_profile p on i.userid=p.userid ' +
-			'where w.userid=$1 ' +
-			condition +
-			'order by ' +
-			orderby +
-			'; ',
+				'from user_wishlist w ' +
+				'inner join course c on c.courseid=w.courseid ' +
+				'inner join instructor i on i.instructorid=c.ownerid ' +
+				'inner join user_profile p on i.userid=p.userid ' +
+				'where w.userid=$1 ' +
+				condition +
+				'order by ' +
+				orderby +
+				'; ',
 			[user]
 		)
 		const ann = data.rows
 		res.send(ann)
-	} catch (err) {
+	} catch (error) {
 		const err = {
 			statusCode: 500,
 			message: error,
@@ -42,28 +41,28 @@ const getWishlist = async (req, res, next) => {
 	}
 }
 
-const getMycourse = async (req, res, next) => {
+const getMycourse = async (req, res) => {
 	try {
-		// const user = req.body.userid
+		const user = req.user.id
 		const finish = req.query.finish
 		const condition = req.query.condition
 		const orderby = req.query.orderby
 
-		const user = '08e9d239-b3f2-4db8-b29a-da99a314df92';
-		// const condition='';
-		// const orderby='addtime desc';
+		// const user = '08e9d239-b3f2-4db8-b29a-da99a314df92'
+		// const condition = ''
+		// const orderby = 'addtime desc'
 
 		const data = await pool.query(
 			'select m.userid,m.courseid,addtime,lastvisit,isfinished,coursename,coursepicture,p.firstname,p.lastname ' +
-			'from user_mycourse m ' +
-			'inner join course c on c.courseid=m.courseid ' +
-			'inner join instructor i on i.instructorid=c.ownerid ' +
-			'inner join user_profile p on i.userid=p.userid ' +
-			'where m.userid=$1 and isfinished=$2 ' +
-			condition +
-			'order by ' +
-			orderby +
-			'; ',
+				'from user_mycourse m ' +
+				'inner join course c on c.courseid=m.courseid ' +
+				'inner join instructor i on i.instructorid=c.ownerid ' +
+				'inner join user_profile p on i.userid=p.userid ' +
+				'where m.userid=$1 and isfinished=$2 ' +
+				condition +
+				'order by ' +
+				orderby +
+				'; ',
 			[user, finish]
 		)
 		const ann = data.rows
@@ -77,10 +76,10 @@ const getMycourse = async (req, res, next) => {
 	}
 }
 
-const deleteWishlist = async (req, res, next) => {
+const deleteWishlist = async (req, res) => {
 	try {
 		const course = req.body.courseid
-		const user = req.body.userid
+		const user = req.user.id
 		await pool.query('delete from user_wishlist where (userid,courseid)=($1,$2);', [user, course])
 		res.send({ success: true })
 	} catch (error) {
@@ -91,15 +90,15 @@ const deleteWishlist = async (req, res, next) => {
 		return errorHandler(err, req, res)
 	}
 }
-///////////
-const getProfile = async (req, res, next) => {
+
+const getProfile = async (req, res) => {
 	try {
 		const user = req.user.id
 		const userProfile = await pool.query(
-			'select avatar,firstname,lastname,isstudent,email,birthdate,createat,bio,phoneno ' +
-			'from user_profile u ' +
-			'inner join local_auth l on u.userid = l.userid ' +
-			'where u.userid=$1; ',
+			'select avatar,firstname,lastname,email,birthdate,createat,bio,phoneno ' +
+				'from user_profile u ' +
+				'inner join local_auth l on u.userid = l.userid ' +
+				'where u.userid=$1; ',
 			[user]
 		)
 		res.send(userProfile.rows[0])
@@ -112,26 +111,20 @@ const getProfile = async (req, res, next) => {
 	}
 }
 
-const editProfile = async (req, res, next) => {
+const editProfile = async (req, res) => {
 	try {
-		const user = req.body.userid
+		const user = req.user.id
 		const avatar = req.body.avatar
 		const firstname = req.body.firstname
 		const lastname = req.body.lastname
 		const birthdate = req.body.birthdate
 		const bio = req.body.bio
 		const phoneno = req.body.phoneno
-		await pool.query(
-			'update user_profile set ' +
-			'avatar=$2, ' +
-			'firstname=$3, ' +
-			'lastname=$4, ' +
-			'birthdate=$5, ' +
-			'bio=$6, ' +
-			'phoneno=$7, ' +
-			'where userid=$1; ',
-			[user, avatar, firstname, lastname, birthdate, bio, phoneno]
-		)
+		const updateUserQuery = `update user_profile set avatar='${avatar}', updateat=CURRENT_TIMESTAMP,
+		firstname='${firstname}', lastname='${lastname}', birthdate='${birthdate}', bio='${bio}', phoneno='${phoneno}'
+		where userid = '${user}'`
+		console.log(updateUserQuery)
+		await pool.query(updateUserQuery)
 		res.send({ success: true })
 	} catch (error) {
 		const err = {
@@ -142,18 +135,37 @@ const editProfile = async (req, res, next) => {
 	}
 }
 
-const checkPassword = async (req, res, next) => {
-	const password = req.body.password
-	const user = req.body.userid
-	await pool.query('select password from local_auth where userid = $1', [user])
-	res.send({ success: true })
+const checkPassword = async (req, res) => {
+	try {
+		const { password } = req.body
+		const user = req.user.id
+		const data = await pool.query('select password from local_auth where userid = $1', [user])
+		const dbPassword = data.rows[0].password
+		const match = await bcrypt.compare(password, dbPassword)
+		res.send({ match })
+	} catch (error) {
+		const err = {
+			statusCode: 500,
+			message: error,
+		}
+		return errorHandler(err, req, res)
+	}
 }
 
-const postNewPassword = async (req, res, next) => {
-	const password = req.body.password
-	const user = req.body.userid
-	await pool.query('update local_auth set ' + 'password=$2 ' + 'where userid=$1; ', [user, password])
-	res.send({ success: true })
+const newPassword = async (req, res) => {
+	try {
+		const { password } = req.body
+		const user = req.user.id
+		const hashedPassword = await bcrypt.hash(password, 10)
+		await pool.query('update local_auth set password=$2 where userid=$1; ', [user, hashedPassword])
+		res.send({ success: true })
+	} catch (error) {
+		const err = {
+			statusCode: 500,
+			message: error,
+		}
+		return errorHandler(err, req, res)
+	}
 }
 module.exports = {
 	test,
@@ -162,6 +174,6 @@ module.exports = {
 	deleteWishlist,
 	getProfile,
 	editProfile,
-	getCheckPassword,
-	postNewPassword,
+	checkPassword,
+	newPassword,
 }
