25d85af152687b3cf0f393f71589ce37829ad33d
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2020-11-19 21:09:48 +0700

    [ADMIN]:[FIX] User routes and controller

--- eduroom_backend/src/routes/userRoute.js
+++ eduroom_backend/src/routes/userRoute.js
@@ -1,7 +1,16 @@
 const express = require('express')
+
 const router = express.Router()
 const { jwtAuthenicate } = require('../middleware/jwtAuthenticate')
-const { getWishlist, getMycourse, deleteWishlist, getProfile, editProfile, getCheckPassword, postNewPassword } = require('../controllers/user/user')
+const {
+	getWishlist,
+	getMycourse,
+	deleteWishlist,
+	getProfile,
+	editProfile,
+	checkPassword,
+	newPassword,
+} = require('../controllers/user/user')
 
 router.get('/getWishlist', jwtAuthenicate, getWishlist)
 router.get('/getMycourse', jwtAuthenicate, getMycourse)
@@ -9,7 +18,7 @@ router.delete('/deleteWishlist', jwtAuthenicate, deleteWishlist)
 
 router.get('/getProfile', jwtAuthenicate, getProfile)
 router.patch('/postEditProfile', jwtAuthenicate, editProfile)
-router.post('/getCheckPassword', jwtAuthenicate, getCheckPassword)
-router.post('/postNewPassword', jwtAuthenicate, postNewPassword)
+router.post('/getCheckPassword', jwtAuthenicate, checkPassword)
+router.patch('/postNewPassword', jwtAuthenicate, newPassword)
 
 module.exports = router
