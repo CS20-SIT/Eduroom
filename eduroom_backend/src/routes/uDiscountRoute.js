5ce9178c9a45041f6240df1463ccbf04d5e74f2f
Sakkarin <sakkarin.kmutt@mail.kmutt.ac.th>
2020-11-20 15:37:23 +0700

    [G02] : [FIX] userid Cookie for uRegister

--- eduroom_backend/src/routes/uDiscountRoute.js
+++ eduroom_backend/src/routes/uDiscountRoute.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const path = require("path");
 const router = express.Router();
+const { jwtAuthenicate } = require('../middleware/jwtAuthenticate');
+
 
 const {getUniversitylist,registerUemail} = require("../controllers/universityDiscount/universityController");
-  router.get("/ulist", getUniversitylist);
-  router.post("/registerUemail", registerUemail);
+  router.get("/ulist", jwtAuthenicate, getUniversitylist);
+  router.post("/registerUemail", jwtAuthenicate, registerUemail);
+
 
   module.exports = router;
