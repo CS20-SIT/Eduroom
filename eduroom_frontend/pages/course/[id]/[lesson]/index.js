5186ed22585d0652a78471be202393b13f8b7737
Teerapat <62130500215@st.sit.kmutt.ac.th>
2020-11-22 05:26:45 +0700

    [UPGRADE] Lesson Page

--- eduroom_frontend/pages/course/[id]/[lesson]/index.js
+++ eduroom_frontend/pages/course/[id]/[lesson]/index.js
@@ -13,9 +13,25 @@ import Box from '@material-ui/core/Box';
 const CourseIDLesson = ({ courseDes, id }) => {
 
     const [sec,setSec] = useState(0); 
+    const [secBG, setSecBG] = useState(0);
+    const [part,setPart] = useState(0);
     var secNow = 0, partNow = 0;
-    const [srcc,setSrc] = useState("https://www.youtube.com/embed/jveH6adL5DY");
+    const [srcc,setSrc] = useState("https://www.youtube.com/embed/mWU6_86B10c");
+    // Set srcc from secBG part
+    const [partType,setPartType] = useState(1);
+    const [questionNow,setQuestionNow] = useState(0);
 
+    const checkIcon = (type) => {
+        if(type == 1){
+            return "https://cdn4.iconfinder.com/data/icons/glyphs/24/icons_play-512.png";
+        }
+        else if(type == 2){
+            return "https://cdn0.iconfinder.com/data/icons/very-basic-android-l-lollipop-icon-pack/24/document-512.png";
+        }
+        else if(type == 3){
+            return "https://cdn0.iconfinder.com/data/icons/math-business-icon-set/93/1_9-512.png";
+        }
+    }
     const testLog = (x) => {
         console.log(x)
     }
@@ -50,23 +66,48 @@ const CourseIDLesson = ({ courseDes, id }) => {
             <div className='bg-little-grey '>
                 <Link href={`/course/${id}`}><span className='text-primary text-lg py-8 px-8 pointer'>Back</span></Link>
                 <div className='container-2'>
+
+                    {/* Title */}
                     <div className='my-2'>
                         <span className='text-xl text-navy font-quicksand'>{courseDes[id-1].name}</span>
-                        {/* <span>
-                            <button className='text-md text-error font-quicksand bg-white border-red rounded-lg add-cart pointer'>Add to cart</button>
-                        </span>
-                        <span>
-                            <button className='text-md text-white font-quicksand bg-error border-red rounded-lg buy pointer'>Buy</button>
-                        </span> */}
                         <span className="share-icon pointer"><img alt="shareIcon" src="https://cdn3.iconfinder.com/data/icons/black-easy/512/538636-share_512x512.png" width="20px" height="20px" ></img></span>
-                        <div className='text-secondary font-quicksand mb-10'>{"Section "+`${courseDes[id-1].section[secNow].id}`+" : " + `${courseDes[id-1].section[secNow].name}`}</div>
+                        <div className='text-secondary font-quicksand mb-10 my-2'>{"Section "+`${courseDes[id-1].section[secBG].id}`+" : " + `${courseDes[id-1].section[secNow].name}` + "  |  Part " + `${courseDes[id-1].section[secBG].part[part].id}` + " : " + `${courseDes[id-1].section[secBG].part[part].name}`}</div>
                     </div>
+                    {/* Title */}
+
+
                     <div className="my-8" height="500px">
-                        <div className="inline-block">
+
+                        {/* Videos / Materials / Quiz */}
+                        <div className={`${partType == 1 ? "inline-block":"hidden"}`}>
                             <iframe className="" width="700" height="450" src={srcc}></iframe>
                         </div>
+                        <div className={`${partType == 2 ? "inline-block":"hidden"}`}>
+                            <div onClick={() => {
+                                downloadItem('http://rathcenter.com/Sheet/Logic.pdf');
+                            }}>Download</div>
+                        </div>
+                        <div className={`${partType == 3 ? "inline-block":"hidden"}`}>
+                            {courseDes[id-1].section[secBG].part[part].questionNum.map((q, qNum) => (
+                                // <div className={`${questionNow == qNum ? '':"hidden"}`}>
+                                <div className="">
+                                    <div className="font-quicksand text-xxl text-navy font-bold ml-30">{q.question}</div>
+                                    <div className="my-4 text-center mt-10 ml-20">
+                                        {q.choice.map((c)=>(
+                                            <div className="text-grey my-4 py-8 px-12 pointer rounded-sm bg-white shadow hover-navy">{c}</div>
+                                        ))}
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+
+                        {/* Videos / Materials / Quiz */}
+
+                        {/* Course Content Box */}
                         <div className="course-box bg-white inline-block">
                             <div className="font-lato text-navy text-lg mt-10 mx-8 border-bottom-grey course-content">COURSE CONTENT</div>
+                            
+                            {/* Map Section */}
                             <div>
                             {courseDes[id-1].section.map((e, index) => (
                                 <div>
@@ -76,9 +117,9 @@ const CourseIDLesson = ({ courseDes, id }) => {
                                         }
                                         else{
                                             setSec(index);
+                                            // secNow = index;
+                                            // console.log(secNow+" "+part);
                                         }
-                                        
-                                        
                                     }}>
                                         <div className="font-normal my-1">{"Section "+`${e.id}`+" : " + `${e.name}`}</div>
                                         <div className="font-light text-ll text-grey">{`${e.time}`+" mins."}</div>
@@ -87,24 +128,29 @@ const CourseIDLesson = ({ courseDes, id }) => {
                                     
                                     <div className={`${sec == index ? '':"hidden"}`}>
                                         {e.part.map((ee, indexx) => (
-                                            <div className="my-6 mx-4">
+                                            <div className={`${secBG == index&&part == indexx ? "py-4 mx-4 selectPart":"py-4 mx-4"}`}>
                                                 <div className="inline-block justify-icon position-ab">
-                                                <img width="30px" src="https://cdn2.iconfinder.com/data/icons/ui-basic-glyph/512/UI_Basic_GLYPH-110-512.png"/>
+                                                    <img width="24px" src={checkIcon(ee.type)}/>
                                                 </div>
                                                 <div className="font-quicksand px-2 text-ll pointer font-normal-bold inline-block part-word" onClick={()=>{
                                                     setSrc(ee.src);
+                                                    setSecBG(index);
+                                                    setPart(indexx);
+                                                    setPartType(ee.type);
+                                                    // console.log(index+" "+indexx);
                                                 }}>{"Part " + `${ee.id}` + ": " + `${ee.name}`}</div>
                                             </div>
                                         ))}
                                         
                                     </div>
-                                    {/* <div onClick={() => {
-                                        downloadItem('http://rathcenter.com/Sheet/Logic.pdf');
-                                    }}>Download</div> */}
                                 </div>
                             ))}
                             </div>
+                            {/* Map Section */}
+
                         </div>
+                        {/* Course Content Box */}
+                        
                         <div>
                         
                         </div>
@@ -144,8 +190,35 @@ export async function getServerSideProps(contex) {
                     {
                         id: 1,
                         name: 'Preview of course',
+                        type: 1,
                         src: 'https://www.youtube.com/embed/6S58yvva978',
+                        questionNum: [],
                     },
+                    {
+                        id: 2,
+                        name: 'Material',
+                        type: 2,
+                        src: 'resource.pdf',
+                        questionNum: [],
+                    },
+                    {
+                        id: 3,
+                        name: 'Pretest',
+                        type: 3,
+                        src: '',
+                        questionNum: [
+                            {
+                                question: "What is Python?",
+                                choice: ["Snake", "Computer Language", "Bamboo", "Thonpy"],
+                                answer: 1,
+                            },
+                            {
+                                question: "Who Kill Rama VIII?",
+                                choice: ["IX", "Prede", "Prayut", "Prawit"],
+                                answer: 0,
+                            },
+                        ]
+                    }
                 ]
             },
             {
@@ -156,17 +229,23 @@ export async function getServerSideProps(contex) {
                     {
                         id: 1,
                         name: 'Basic',
+                        type: 1,
                         src: 'https://www.youtube.com/embed/5Y-MghiDmQ4',
+                        questionNum: [],
                     },
                     {
                         id: 2,
                         name: 'Very basic',
-                        src: 'https://www.youtube.com/embed/5Y-MghiDmQ4',
+                        type: 1,
+                        src: 'https://www.youtube.com/embed/hQzbePDFO9I',
+                        questionNum: [],
                     },
                     {
                         id: 3,
                         name: 'Fucking basic',
-                        src: 'https://www.youtube.com/embed/5Y-MghiDmQ4',
+                        type: 1,
+                        src: 'https://www.youtube.com/embed/CWXQ031jDe8',
+                        questionNum: [],
                     },
                 ]
             },
@@ -178,7 +257,9 @@ export async function getServerSideProps(contex) {
                     {
                         id: 1,
                         name: 'Datatype',
-                        src: 'https://www.youtube.com/embed/KrToaEvDzdk"',
+                        type: 1,
+                        src: 'https://www.youtube.com/embed/QHM3RTMctJU',
+                        questionNum: [],
                     },
                 ]
             },
@@ -190,7 +271,9 @@ export async function getServerSideProps(contex) {
                     {
                         id: 1,
                         name: 'Syntax',
+                        type: 1,
                         src: 'https://www.youtube.com/embed/AGnECmJFA9U',
+                        questionNum: [],
                     },
                 ]
             },
