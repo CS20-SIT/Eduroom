543afdb24185ae759f363c80c4c470ff564e9f9c
Supawit <jimsupawitzz@gmail.com>
2020-11-23 11:17:54 +0700

    [Update] : Controller and Route

--- eduroom_backend/src/routes/chatRoute.js
+++ eduroom_backend/src/routes/chatRoute.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const router = express.Router()
-const {getChatlist, getGroupPicture} = require('../controllers/chatController')
+const {getChatlist, getGroupPicture, inviteCreate} = require('../controllers/chatController')
 
 let test = (req, res) => {
     res.send('test')
   }
 router.get('/getChatlist', getChatlist);
 router.get('/getGroupPicture', getGroupPicture);
-
+router.post('/inviteCreate', inviteCreate)
 
 
 module.exports = router
