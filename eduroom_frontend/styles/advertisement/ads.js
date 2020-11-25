f6ef31f7187dccb0b361396f8f5f2e957f55c8c8
Wuthipong <wuthipong.wst@mail.kmutt.ac.th>
2020-11-26 02:48:08 +0700

    [G02] : [ADD] Tab1 99% Tab2 60%

--- eduroom_frontend/styles/advertisement/ads.js
+++ eduroom_frontend/styles/advertisement/ads.js
@@ -25,7 +25,12 @@ export default css`
     font-family: 'Lato', sans-serif;
     font-weight: bold;
     color: #3d467f;
-    
+    }
+  .ad-tab1-header {
+      font-size: 2.75em;
+      font-family: 'Lato', sans-serif;
+      font-weight: regular;
+      color: #3d467f;
   }
   .ad-ad-description {
     padding: 5% 0% 10% 0%;
@@ -33,6 +38,19 @@ export default css`
     font-family: 'Quicksand', sans-serif;
     font-weight: 600;
     color: #5b5b5b;
+  }
+  .ad-tab1-description {
+    padding: 5% 0% 8% 0%;
+    font-size: 0.9em;
+    font-family: 'Quicksand', sans-serif;
+    font-weight: 600;
+    color: #5b5b5b;
+  }
+  .ad-question {
+    font-size: 1em;
+    font-family: 'Quicksand', sans-serif;
+    font-weight: bold;
+    color: 00000;
   }
   .ad-ad-button {
     background: #A880F7;
@@ -44,6 +62,14 @@ export default css`
     position: absolute;
     right: 40px;
   }
+  .ad-tab1-button {
+    background: #A880F7;
+    border-radius: 2px;
+    padding: 0.4rem 1.5rem;
+    border: none;
+    outline: none;
+    transition: 0.25s;
+  }
   .ad-ad-button:hover {
     cursor: pointer;
     opacity: 0.8;
@@ -54,5 +80,17 @@ export default css`
     font-weight: 700;
     font-size: 1.2em;
     font-family: 'Quicksand', sans-serif;
+  }
+  .tab1 {
+    width: 1635px;
+    height: 730px;
+    cursor: default;
+    background-image: url('/images/BG_Landing.svg');
+  }
+  .tab2 {
+    width: 93%;
+    height: 350px;
+    cursor: default;
+    background-image: url('/images/register_bg.svg');
   }
 `;
