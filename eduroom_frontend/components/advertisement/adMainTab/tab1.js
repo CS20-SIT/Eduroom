f7fa70fab2debde9bccb8d2ba905f3f446d4138b
Wuthipong <wuthipong.wst@mail.kmutt.ac.th>
2020-11-26 02:48:08 +0700

    [G02] : [ADD] Tab1 99% Tab2 60%

--- eduroom_frontend/components/advertisement/adMainTab/tab1.js
+++ eduroom_frontend/components/advertisement/adMainTab/tab1.js
@@ -1,17 +1,40 @@
-import React from 'react'
-import style from '../../../styles/advertisement/ads';
-import { Grid} from '@material-ui/core'
+import React, { Fragment, useState} from 'react'
+import style from '../../../styles/advertisement/ads'
+import { useRouter } from 'next/router';
 
 
 
 const Content = () => {
+
     return (
-        <div className="Ad-Detail-text" >
-            
+
+        <Fragment>
+        <div className="tab1" style={{marginTop:"-6.4%" ,marginLeft:"-50px",marginButtom:"-100px"}}>
+        <div style={{paddingTop:"200px" ,marginLeft:"150px"}}>
+        <div className="ad-tab1-header">Be seen where </div>
+        <div className="ad-tab1-header" style={{marginTop:"-10px"}}>everyone is watching </div>
+          <div className="ad-tab1-description">
+            With EDUROOM ads, reach potential customers <br></br>
+            and have them take action when they watch <br></br>
+            or search for videos on YouTube – and only <br></br>
+            pay when they show interest.
+        </div>
+        <div className="ad-question" >Already have a ADs?</div>
+        <button style={{marginTop:"10px"}}
+            className = "ad-tab1-button"
+            onClick={() => router.push('/login')}
+          >
+            <a className="ad-button-text">Start Now</a>
+          </button>
+          </div>
          <style jsx>
          {style}
        </style> 
-        </div>
+       </div>
+        
+        </Fragment>
+
+        
         )
     }
     export default Content
--- eduroom_frontend/components/advertisement/adMainTab/tab2.js
+++ eduroom_frontend/components/advertisement/adMainTab/tab2.js
@@ -1,18 +1,47 @@
-import React from 'react'
-import style from '../../../styles/advertisement/ads';
-import { Grid} from '@material-ui/core'
+import React, { Fragment, useState} from 'react'
+import style from '../../../styles/advertisement/ads'
+import { useRouter } from 'next/router';
+import {Link,Typography,InputBase, Paper, Grid} from '@material-ui/core'
 
 
 
 const Content = () => {
+
     return (
-        <div className="ad-description" >
-            
+
+        <Fragment>
+        <div className="tab2" style={{marginTop:"12%" ,marginLeft:"-50px",marginBottom:"100px"}}>
+        <Grid containner spacing={3}>  
+            <Grid item xs={6}>
+            <div style={{marginTop:"-20px" ,marginLeft:"150px"}}>
+            <div className="ad-tab1-header">Be seen where </div>
+            <div className="ad-tab1-header" style={{marginTop:"-10px"}}>everyone is watching </div>
+        
+            <div className="ad-tab1-description">
+                With EDUROOM ads, reach potential customers <br></br>
+                and have them take action when they watch <br></br>
+                or search for videos on YouTube – and only <br></br>
+                pay when they show interest.
+            </div>
+        
+            <div className="ad-question" >Already have a ADs?</div>
+                <button style={{marginTop:"10px"}}
+                className = "ad-tab1-button"
+                onClick={() => router.push('/login')}
+                >
+                <a className="ad-button-text">Start Now</a>
+                </button>
+            </div>
+        </Grid>       
+        </Grid>  
          <style jsx>
          {style}
        </style> 
-        </div>
-     
+       </div>
+        
+        </Fragment>
+
+        
         )
     }
     export default Content
