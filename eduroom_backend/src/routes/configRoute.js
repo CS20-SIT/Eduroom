48274959c4f090a5348e29446cc91be232ccf68e
Teerapat <62130500215@st.sit.kmutt.ac.th>
2020-12-01 16:29:44 +0700

    [ADD] Docker Initial

--- eduroom_backend/src/routes/configRoute.js
+++ eduroom_backend/src/routes/configRoute.js
@@ -8,6 +8,7 @@ const ForumRoute = require('./forumRoute')
 const AnalysisRoute = require('./analysisRoute')
 const GraderRoute = require('./graderRoute')
 const EventRoute = require('./eventRoute')
+const CourseRoute = require('./courseRoute')
 
 router.use('/test', TestRoute)
 router.use('/auth', AuthRoute)
@@ -16,5 +17,6 @@ router.use('/forum', ForumRoute)
 router.use('/analysis', AnalysisRoute)
 router.use('/grader', GraderRoute)
 router.use('/event',EventRoute)
+router.use('/course', CourseRoute)
 
 module.exports = router
