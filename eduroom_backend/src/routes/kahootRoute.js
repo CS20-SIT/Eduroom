742795481af3527c3ad587614a387de7dd178967
Wirawat <katak6565@gmail.com>
2020-12-08 19:36:45 +0700

    [G03]:[UPDATE] count student
    -SOCKET
       set-countAnswer --> to count student answers
    -FRONTEND
      gamePage1(instructor)/gamePage1std -->setCountP() --> to set count student

--- eduroom_backend/src/routes/kahootRoute.js
+++ eduroom_backend/src/routes/kahootRoute.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { fetchRoom, createRoom,fetchRoomHistory,createKahootHistory, player,fetchExactlyRoom,historyPlayer } = require('../controllers/edqiz/roomController');
+const { fetchRoom, createRoom,fetchRoomHistory,createKahootHistory, player,fetchExactlyRoom,historyPlayer,createHistoryPlayerAnswer } = require('../controllers/edqiz/roomController');
 
 router.get('/rooms', fetchRoom);
 router.post('/room', createRoom);
@@ -10,6 +10,6 @@ router.post('/roomHistory', createKahootHistory);
 router.post('/roomHistoryplayer', historyPlayer);
 router.post('/player', player);
 router.get('/sessionid/:pin', fetchExactlyRoom);
-
+router.post('/historyPlayerAnswer', createHistoryPlayerAnswer);
 
 module.exports = router;
