8162927296ca598fcfdb96f682b82841de1d3428
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2020-12-08 20:18:54 +0700

    [ADMIN]:[ADD] Multer to GCS and local with testRoutes

--- eduroom_backend/src/routes/testRoute.js
+++ eduroom_backend/src/routes/testRoute.js
@@ -2,18 +2,41 @@ const express = require('express')
 const router = express.Router()
 const { test } = require('../controllers/testController')
 const sendEmail = require('../utils/sendMail.js')
-const uploadHandler = require('../middleware/multer')
+const { uploadToGCSHandler, uploadToLocalHandler } = require('../middleware/multer')
 
 router.get('/', test)
-router.get('/mail',async(req,res,next)=>{
-    sendEmail({email:'test@test.com',subject:'Test SMTP Server',message:'Woah'})
-    res.status(200).json({success:true})
+router.get('/mail', async (req, res, next) => {
+	sendEmail({ email: 'test@test.com', subject: 'Test SMTP Server', message: 'Woah' })
+	res.status(200).json({ success: true })
 })
 
-router.post('/multer', uploadHandler('test.png', '/test/'), (req, res) => {
-    // res.send({ linkURL: req.files[0].linkUrl });
-    res.send({ url: req.file })
+// Upload Handler
+// uploadToLocalHandler -> Use when you have to manipuate the file before upload to Google Cloud Storage (I will update how to upload later)
+// uploadToGCSHandler() -> Use when no manipulation is required -> File in form-data are uploaded directly to GCS.
+
+router.post('/multerGCS', uploadToGCSHandler('test1/'), (req, res) => {
+	// res.send({ linkURL: req.files[0].linkUrl });
+	res.send(req.files)
+	// This is what you can get in req.files
+	// [
+	//     {
+	//         fieldname: <Field that we use in form-data>
+	//         linkUrl: <URL to that file>
+	//         contentType: <Type of that file>
+	//     }
+	// ]
 })
 
+router.post('/multerLocal', uploadToLocalHandler(), (req, res) => {
+	res.send(req.files)
+	// This is what you can get in req.files
+	// [
+	//     {
+	//         fieldname: <Field that we use in form-data>
+	//         path: <path of that file>
+	//         mimetype: <Type of that file>
+	//     }
+	// ]
+})
 
 module.exports = router
