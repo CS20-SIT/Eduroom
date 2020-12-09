e3bba10632382d2e4396847f235aae4cdb4b1205
note <note16651@hotmail.co.th>
2020-12-09 20:49:09 +0700

    [ADD] Balloon in coin-shop test.js

--- eduroom_frontend/pages/coin-shop/test.js
+++ eduroom_frontend/pages/coin-shop/test.js
@@ -1,65 +1,32 @@
 import { useEffect } from 'react'
+import Styles from '../../styles/CoinStyles/test';
+import React from 'react'
 
 const temp = (props) => {
-	useEffect(() => {
-		createBalloons(100)
-	}, [])
-	function random(num) {
-		return Math.floor(Math.random() * num)
-	}
-	function createBalloons(num) {
-		var balloonContainer = document.getElementById('balloon-container')
-		console.log(balloonContainer)
-		for (var i = num; i > 0; i--) {
-			var balloon = document.createElement('div')
-			balloon.className = Styles.balloon
-			balloon.style.cssText = getRandomStyles()
-			balloonContainer.append(balloon)
-		}
-		console.log(balloonContainer)
-	}
-	function getRandomStyles() {
-		var r = random(255)
-		var g = random(255)
-		var b = random(255)
-		var mt = random(200)
-		var ml = random(50)
-		var dur = random(5) + 5
-		console.log(dur)
-		return `
-                background-color: rgba(${r},${g},${b},0.7);
-                color: rgba(${r},${g},${b},0.7); 
-                box-shadow: inset -7px -3px 10px rgba(${r - 10},${g - 10},${b - 10},0.7);
-                margin: ${mt}px 0 0 ${ml}px;
-                animation: float ${dur}s ease-in infinite;
-                `
-	}
-
 	return (
 		<div>
+			Hello
 			<div>
-				<div id="balloon-container"></div>
+				<div id="balloon-container">
+					<div className="balloon"></div>
+					<div className="balloon2"></div>
+					<div className="balloon3"></div>
+					<div className="balloon4"></div>
+					<div className="balloon5"></div>
+					<div className="balloon6"></div>
+					<div className="balloon7"></div>
+					<div className="balloon8"></div>
+					<div className="balloon9"></div>
+					<div className="balloon10"></div>
+					<div className="balloon11"></div>
+					<div className="balloon12"></div>
+					<div className="balloon13"></div>
+					<div className="balloon14"></div>
+					<div className="balloon15"></div>
+				</div>
+				<style jsx>{Styles}</style>
 			</div>
-			<style jsx>{`
-				#balloon-container {
-					height: 100vh;
-					padding: 1em;
-					box-sizing: border-box;
-					display: flex;
-					flex-wrap: wrap;
-					overflow: hidden;
-					@keyframes float {
-						0% {
-							transform: translateY(100vh);
-							opacity: 1;
-						}
-						100% {
-							transform: translateY(-300vh);
-							opacity: 0;
-						}
-					}
-				}
-			`}</style>
+			
 		</div>
 	)
 }
