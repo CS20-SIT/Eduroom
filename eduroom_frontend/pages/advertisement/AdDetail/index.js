41cad90bd7e748b040858b67edf71ff1323afded
Sakkarin <sakkarin.kmutt@mail.kmutt.ac.th>
2020-12-10 17:17:41 +0700

    [Add] : [G02] Adinform page

--- eduroom_frontend/pages/advertisement/AdDetail/index.js
+++
@@ -1,8 +0,0 @@
-import React,{Fragment} from 'react'
-import Detail from '../../../components/advertisement/adDetail'
-const adDetail = () => {
-    return <Fragment>
-        <Detail/>
-    </Fragment>
-}
-export default adDetail
