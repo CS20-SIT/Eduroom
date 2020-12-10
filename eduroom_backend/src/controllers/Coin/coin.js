737577aedf3b7500e7cde983547116bb0c990762
Chingly <kavisara.noya@mail.kmutt.ac.th>
2020-12-10 23:08:21 +0700

    [G09] : [UPDATE] query add&reduce coinOwner

--- eduroom_backend/src/controllers/Coin/coin.js
+++ eduroom_backend/src/controllers/Coin/coin.js
@@ -30,17 +30,17 @@ exports.getDailyRewardStatus = async (req, res, next) => {
         errorHandler(error, req, res);
     }
 }
-exports.setCoinTransaction = async (req, res) => {
-    try {
-        const userId = 'db29433b-e05d-41ab-854b-b6f8023464f6'
-        const coin = req.body.coin
-        await pool.query(`INSERT INTO coin_transaction(userid, date, amountofcointransaction) VALUES ('${userId}',current_timestamp, ${coin})`)
-        res.status(201).send({ success: true })
-    } catch (error) {
-        errorHandler(error, req, res);
-    }
-}
-exports.ShowCoinOwner = async (req, res) => {
+// exports.setCoinTransaction = async (req, res) => {
+//     try {
+//         const userId = 'db29433b-e05d-41ab-854b-b6f8023464f6'
+//         const coin = req.body.coin
+//         await pool.query(`INSERT INTO coin_transaction(userid, date, amountofcointransaction) VALUES ('${userId}',current_timestamp, ${coin})`)
+//         res.status(201).send({ success: true })
+//     } catch (error) {
+//         errorHandler(error, req, res);
+//     }
+// }
+exports.showCoinOwner = async (req, res) => {
     try {
         const userId = 'db29433b-e05d-41ab-854b-b6f8023464f6'
         const result = await pool.query(`SELECT amountofcoin FROM coin_owner WHERE userid='${userId}'`)
@@ -60,5 +60,24 @@ exports.showStickerOwner = async (req, res) => {
         res.send(result.rows[0])
     } catch (error) {
         errorHandler(error, req, res);
+    }
+}
+exports.addReduceTransOwner = async (req,res) => {
+    try{
+        const userId = 'db29433b-e05d-41ab-854b-b6f8023464f6'
+        const coin = req.body.coin
+        const getCoinOwner = await pool.query(`SELECT amountofcoin FROM coin_owner WHERE userid='${userId}'`)    
+        let amountCoin = getCoinOwner.rows[0].amountofcoin;
+        if(coin < 0){
+            amountCoin= amountCoin-coin;
+        }else{
+            amountCoin= amountCoin+coin;
+        }
+        console.log(`UPDATE coin_owner SET amountofcoin=${amountCoin} WHERE userid='${userId}';`)
+        await pool.query(`UPDATE coin_owner SET amountofcoin=${amountCoin} WHERE userid='${userId}';`)
+        await pool.query(`INSERT INTO coin_transaction(userid, date, amountofcointransaction) VALUES ('${userId}',current_timestamp, ${coin})`)
+        res.status(201).send({ success: true })
+    }catch (error){
+        errorHandler(error,req,res);
     }
 }
--- eduroom_backend/src/routes/coinRoute.js
+++ eduroom_backend/src/routes/coinRoute.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const { getStickers,getDailyRewardStatus,setCoinTransaction, ShowCoinOwner,showStickerOwner } = require('../controllers/Coin/coin');
+const { getStickers,getDailyRewardStatus,showCoinOwner,showStickerOwner,addReduceTransOwner} = require('../controllers/Coin/coin');
 
 router.get('/stickers', getStickers);
 router.get('/dailyReward',getDailyRewardStatus);
-router.post('/coinTransaction',setCoinTransaction);
-router.get('/coinOwner',ShowCoinOwner);
+// router.post('/coinTransaction',setCoinTransaction);
+router.get('/coinOwner',showCoinOwner);
 router.get('/stickerOwner',showStickerOwner);
+router.post('/coinTransaction',addReduceTransOwner);
 module.exports = router;
