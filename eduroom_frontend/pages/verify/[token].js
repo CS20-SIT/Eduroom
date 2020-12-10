33046829b8d672556242642506b8c0ffedafe370
GGolfz <wisarutgolf11@gmail.com>
2020-12-10 14:33:54 +0700

    [ADMIN] : [ADD] Verification Check

--- eduroom_frontend/pages/verify/[token].js
+++ eduroom_frontend/pages/verify/[token].js
@@ -1,5 +1,31 @@
-import React,{Fragment} from 'react'
-const Token = () => {
-    return <Fragment></Fragment>
+import React, { Fragment, useEffect, useState } from 'react'
+import api from '../../api'
+import GeneralTemplate from '../../components/template/general'
+const Token = ({ token }) => {
+	const [status, setStatus] = useState('')
+	useEffect(() => {
+		api
+			.get(`/api/auth/verify/${token}`)
+			.then((res) => {
+				if (res.data.success) {
+					setStatus('Verification Success')
+				}
+			})
+			.catch((err) => {
+				setStatus(err.response.data.error)
+			})
+	}, [])
+	return (
+		<Fragment>
+			<GeneralTemplate>
+				<div>{status}</div>
+			</GeneralTemplate>
+		</Fragment>
+	)
+}
+
+export async function getServerSideProps(ctx) {
+	const token = ctx.query.token
+	return { props: { token } }
 }
 export default Token
