13d86df399c9966348ce441f47dfcaa9017d7098
Krishadawut Olde Monnikhof <krishadawut.om@mail.kmutt.ac.th>
2020-12-11 01:34:42 +0700

    [G05] : [ADD] backend mockup data

--- eduroom_backend/src/controllers/Chat/chatController.js
+++ eduroom_backend/src/controllers/Chat/chatController.js
@@ -1,9 +1,9 @@
-const pool = require('../database/db')
+const pool = require('../../database/db')
 
 const getChatlist = async (req, res, next) => {
   const userid = '3c93e461-7eab-45fb-b8ce-587b47286f8c'
   const chatlist = await pool.query(
-    `select chat.chatroomid, usp.firstname, roomname, message, sendtime from chat, chat_message cm, 
+    `select chat.chatroomid, usp.firstname, roomname, message, to_char(sendtime,'DD/MM') as sendtime from chat, chat_message cm, 
     user_profile usp where cm.chatroomid in ( select crm.chatroomid from chat_roommember crm where userid = '${userid}' ) 
     and cm.userid = usp.userid and chat.chatroomid in ( select chat.chatroomid from chat where sendtime = 
       (select max(sendtime) from chat_message where chat.chatroomid = chat_message.chatroomid) 
---
+++ eduroom_backend/src/controllers/Chat/chatMockupData.js
@@ -0,0 +1,238 @@
+module.exports = {
+	getUserProfileMockup: (userID) => {
+		return {
+			userID: userID,
+            userFirstName: 'Krishadawut',
+            userLastName: 'Olde Monnikhof',
+            userNickName: 'jimmy',
+			profilePicture: 'test.png',
+		}
+	},
+	getChatListMockup: (userID) => {
+		return [
+			{
+				chatroomid: 1,
+				firstName: 'John',
+				roomname: 'test4',
+				message: 'Hello this is test',
+				sendtime: '07/11',
+			},
+			{
+				chatroomid: 2,
+				firstname: 'John',
+				roomname: 'test4',
+				message: 'Hello this is test',
+				sendtime: '07/11',
+			},
+			{
+				chatroomid: 3,
+				firstname: 'John',
+				roomname: 'test4',
+				message: 'Hello this is test',
+				sendtime: '07/11',
+			},
+		]
+	},
+	getChatRoomProfilePictureMockup1: (chatRoomID) => {
+		return {
+			chatRoomID: chatRoomID,
+			chatRoomProfilePicture: 'test.png',
+		}
+	},
+	getChatRoomProfilePictureMockup2: (chatRoomID) => {
+		return {
+			chatRoomId: chatRoomID,
+			chatRoomProfilePictures: [
+				{
+					userID: 23435,
+					userProfilePicture: 'test.png',
+				},
+				{
+					userID: 234455,
+					userProfilePicture: 'test.png',
+				},
+				{
+					userID: 235,
+					userProfilePicture: 'test.png',
+				},
+			],
+		}
+	},
+	getSearchResultMockup: (keyword) => {
+		//limit 50 rows
+		return {
+			users: [
+				{
+					userID: 12432,
+					userFirstName: 'jimmy',
+					userLastName: 'supawit',
+					display: 'jim',
+					email: 'test',
+					userProfile: 'test.png',
+				},
+				{
+					userID: 122,
+					userFirstName: 'jimmy2',
+					userLastName: 'supawit',
+					display: 'jim2',
+					email: 'test2',
+					userProfile: 'test.png',
+				},
+			],
+		}
+	},
+	createGroupChatMockup: (profilePic, chatRoomName, members) => {
+		return {
+			success: true,
+		}
+	},
+	getInvitationListMockup: (userID) => {
+		return {
+			invitaions: [
+				{
+					invitaionID: 1,
+					chatRoomID: 2,
+					chatRoomName: 'Krishadawut',
+					invitor: 'Jimmy',
+				},
+			],
+		}
+	},
+	acceptInvitationMockup: (invitationID) => {
+		return {
+			success: true,
+		}
+	},
+	declineInvitationMockup: (invitationID) => {
+		return {
+			success: true,
+		}
+    },
+    hideChatRoomMockup: (chatRoomID,userID) => {
+        return {
+			success: true,
+		}
+    },
+	chatRoomDetailMockup: (chatRoomID,userID) => {
+        //add column "status" in to chatRoomMember  (if member are kickout it will be invalid or if the chat room is deleted all member will be invalid(chatRoomProlfilePic, chatMessage, systemMessage, stickerMessage))
+		return {
+			chatRoomID: 2,
+            chatRoomName: 'Krishadawut Olde Monnikhof',
+            themeColoe: {
+                senderColor:"#121243",
+                recieverColor:"#235363",
+            },
+            membersID: [435,346,2342,67876],
+			messages: [
+				{
+                    system: false,
+                    sticker: false,
+					messageID: 23,
+                    senderID: 213124,
+                    message: 'Hello Tom',
+                    //if sticker "path:'test.png' instead of message"
+					sendTime: '13:50',
+					reader: [
+						{
+							readerID: 32342,
+							readTime: '13:54',
+						},
+						{
+							readerID: 32342,
+							readTime: '13:54',
+						},
+						{
+							readerID: 32342,
+							readTime: '13:54',
+						},
+					],
+				},
+			],
+		}
+    },
+    hideMessageMockup: (userID,MessageID)=>{
+        return {
+			success: true,
+		}
+    },
+    deleteMessageMockup: (userID,MessageID)=>{
+        return {
+			success: true,
+		}
+    },
+    sendMessageMockup: (userID,chatRoomID,message)=>{
+        return {
+			success: true,
+		}
+    },
+    sendStickerMockup: (userID,chatRoomID,stickerID)=>{
+        return {
+			success: true,
+		}
+    },
+    getStickerListMockup:(userID)=>{
+        return{
+            stickers:[
+                {
+                    stickerID: 1,
+                    stickerName: 'Fruit',
+                    stickerType: 'Fruit',
+                    stickerPath: 'test.png'
+                },
+                {
+                    stickerID: 1,
+                    stickerName: 'Fruit',
+                    stickerType: 'Fruit',
+                    stickerPath: 'test.png'
+                },
+                {
+                    stickerID: 1,
+                    stickerName: 'Fruit',
+                    stickerType: 'Fruit',
+                    stickerPath: 'test.png'
+                }
+            ]
+        }
+    },
+    changeChatRoomProfilePictureMockup:(chatRoomID,Picture)=>{
+        return {
+			success: true,
+		}
+    },
+    changeChatRoomNameMockup:(chatRoomID,name)=>{
+        return {
+			success: true,
+		}
+    },
+    changeThemeColorMockup:(chatRoomID,userID,senderColor,recieverColor)=>{
+        return {
+			success: true,
+		}
+    },
+    addChatRoomMemberMockup:(chatRoomID,InvitorID,InviteeID)=>{
+        return {
+			success: true,
+		}
+    },
+    changeMemberNicknameMockup:(chatRoomID,changerID,userID,nickName)=>{
+        return {
+			success: true,
+		}
+    },
+    deleteMemberMockup:(chatRoomID,deleterID,userID)=>{
+        return {
+			success: true,
+		}
+    },
+    leaveChatRoomMockup:(chatRoomID,userID)=>{
+        return {
+			success: true,
+		}
+    },
+    deleteChatRoomMockup:(chatRoomID)=>{
+        return {
+			success: true,
+		}
+    }
+
+}
