d8615bc88e600368b867260a04e7802aee201b63
coolkidneversleep <iiippbhy@gmail.com>
2020-12-11 15:09:06 +0700

    [G12] : [ADD] edit forum frontend & backend

--- eduroom_backend/src/routes/forumRoute.js
+++ eduroom_backend/src/routes/forumRoute.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const { selectForum, showForum, setForum,createComment,selectRoom,deleteComment,setLike} = require('../controllers/forumController')
+const { selectForum, showForum, setForum,createComment,selectRoom,deleteComment,setLike, editForum} = require('../controllers/forumController')
 const { jwtAuthenicate } = require('../middleware/jwtAuthenticate')
 
 router.get('/', jwtAuthenicate ,showForum)
@@ -10,6 +10,7 @@ router.post('/comment', jwtAuthenicate,createComment)
 router.post('/create', jwtAuthenicate, setForum)
 router.get('/room/:roomname',jwtAuthenicate , selectRoom)
 router.delete('/comment', jwtAuthenicate, deleteComment) //api.delete
+router.put('/:id',jwtAuthenicate,editForum)
 //router.put = edit // api.put 
 
 module.exports = router
