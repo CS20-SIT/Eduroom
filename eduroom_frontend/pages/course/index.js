6d4d0241effbfa9dac1075c50ad59d85b67b6e91
pphawikaa <pphawikaa@gmail.com>
2020-12-13 00:41:43 +0700

    [ADD] course shop page

--- eduroom_frontend/pages/course/index.js
+++ eduroom_frontend/pages/course/index.js
@@ -1,5 +1,31 @@
-import React,{Fragment} from 'react'
-const Course = () => {
-    return <Fragment></Fragment>
+import React, { Fragment, useState, useEffect } from 'react'
+import ProductCourse from '../../components/course/courseStore'
+import ProductPackage from '../../components/package/packageStore'
+import Styles from '../../styles/course/cShop'
+import General from '../../components/template/general'
+import Carousel from '../../components/course/testCarouselAuto'
+
+const packages = () => {
+	const [show, setShow] = useState(false)
+	return (
+		<Fragment>
+			<General>
+				<Carousel></Carousel>
+
+				<div>
+					<h1>Test</h1>
+				</div>
+				<div>
+					<div style={{ margin: '10' }}>
+						<ProductCourse></ProductCourse>
+					</div>
+					<div>
+						<ProductPackage></ProductPackage>
+					</div>
+				</div>
+			</General>
+			<style jsx>{Styles}</style>
+		</Fragment>
+	)
 }
-export default Course
+export default packages
