ac2a2d06408d5b5922882f6c3f5fdce085f6530b
GGolfz <wisarutgolf11@gmail.com>
2020-12-12 22:47:42 +0700

    [ADMIN] : [ADD] Verify Success & Fail

--- eduroom_frontend/pages/verify/[token].js
+++ eduroom_frontend/pages/verify/[token].js
@@ -1,6 +1,8 @@
 import React, { Fragment, useEffect, useState } from 'react'
 import api from '../../api'
 import GeneralTemplate from '../../components/template/general'
+import VerificationSuccess from '../../components/verify/verifySuccess'
+import VerificationFail from '../../components/verify/verifyFail'
 const Token = ({ token }) => {
 	const [status, setStatus] = useState('')
 	useEffect(() => {
@@ -18,8 +20,13 @@ const Token = ({ token }) => {
 	return (
 		<Fragment>
 			<GeneralTemplate>
-				<div>{status}</div>
+				{status == 'Verification Success' ? (
+					<VerificationSuccess />
+				) : (
+					<VerificationFail error={status} />
+				)}
 			</GeneralTemplate>
+			<div style={{backgroundImage:'url(/images/verify/verify-bg.svg)',width:'100vw',height:'100vh',position:'absolute',top:'0',left:'0',backgroundPosition:'center',backgroundSize:'auto',backgroundRepeat:'no-repeat'}}/>
 		</Fragment>
 	)
 }
