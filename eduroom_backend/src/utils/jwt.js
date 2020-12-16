803582149122eb427f78e82b9e057dc30530f397
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2020-12-16 23:09:54 +0700

    [ADMIN]:[REMOVE] Unused jwt function

--- eduroom_backend/src/utils/jwt.js
+++ eduroom_backend/src/utils/jwt.js
@@ -6,39 +6,15 @@ const jwtCookieSignOption = {
 	expiresIn: '30d',
 }
 
-const jwtVerifySignOption = {
-	algorithm: 'HS256',
-	expiresIn: '1d',
-}
-
 const generateCookieJWT = (payload) => {
 	return jwt.sign({ id: payload, role: 'user' }, jwtSecret, jwtCookieSignOption)
-}
-
-const generateVerifyJWT = (payload) => {
-	return jwt.sign({ id: payload }, jwtSecret, jwtVerifySignOption)
 }
 
 const generateCookieAdminJWT = (payload) => {
 	return jwt.sign({ id: payload, role: 'admin' }, jwtSecret, jwtCookieSignOption)
 }
 
-const verifyVerificationJWT = (token) => {
-	return jwt.verify(token, jwtSecret, jwtVerifySignOption)
-}
-
-const verifyCookieAdminJWT = (token) => {
-	const verification = jwt.verify(token, jwtSecret, jwtVerifySignOption)
-	if(verification.role === 'admin'){
-		return verification.id
-	}
-	return null
-}
-
 module.exports = {
 	generateCookieJWT,
-	generateVerifyJWT,
-	verifyVerificationJWT,
-	generateCookieAdminJWT,
-	verifyCookieAdminJWT
+	generateCookieAdminJWT
 }
