17ef0e301441e5fe71c39de6f53e85603b337d64
Sakkarin <sakkarin.kmutt@mail.kmutt.ac.th>
2020-12-17 03:50:45 +0700

    [G02] : [ADD] Add Ads 70% BackEnd+FrontEnd

--- eduroom_backend/src/controllers/advertisement/advertisementController.js
+++ eduroom_backend/src/controllers/advertisement/advertisementController.js
@@ -15,32 +15,41 @@ const getMyAds = async (req, res, next) => {
     res.send(adList);
 };
 
-
+const getAdsType = async (req, res, next) => {
+    const data = await pool.query("select case when typename='vertipic' then 'Vertical Image'"+
+    "when typename='horipic' then 'Horizontal Image'"+
+    "when typename='video' then 'Video' end typename from ad_type ");
+    const typeList = data.rows;
+    res.send(typeList);
+};
 
 const addAds = async (req, res, next) => {
 
     
-    // const type = 2;
-    // const adstarttime = req.body.adstarttime ;
-    // const adexpiretime = req.body.adexpiretime;
-    // const contactemail = req.body.contactemail;
-    // const filelocation = "/testFileLocation/";
-    // const status = "Waiting";
-    // const ownerid = req.user.id;
-
     const type = 2;
-    const adstarttime =  '2020-12-06 00:00:00.000000' ;
-    const adexpiretime = '2022-12-06 00:00:00.000000';
-    const contactemail = 'Davika@gmail.com';
-    const filelocation = '/testFileLocation/';
-    const status = 'Waiting';
-    const ownerid = '44f8e863-226c-4bed-9556-aa6e1600d3bc';
+    const adtag = req.body.adtag;
+    let adstarttime = req.body.adstartdate ;
+    let adexpiretime = req.body.adexpiredate;
+    const contactemail = req.body.contactemail;
+    const filelocation = "/testFileLocation/";
+    const status = "Waiting";
+    const ownerid = req.user.id;
+
+
+    // const type = 2;
+    // const adstarttime =  '2020-12-06 00:00:00.000000' ;
+    // const adexpiretime = '2022-12-06 00:00:00.000000';
+    // const contactemail = 'Davika@gmail.com';
+    // const filelocation = '/testFileLocation/';
+    // const status = 'Waiting';
+    // const ownerid = '44f8e863-226c-4bed-9556-aa6e1600d3bc';
     
     await pool.query(
         "insert into ad(adid, type, adstarttime, adexpiretime, contactemail, filelocation, status, ownerid) values ((select count(*) from ad)+1,$1,$2,$3,$4,$5,$6,$7)",
         [type,adstarttime,adexpiretime,contactemail,filelocation,status,ownerid]
     )
+    
 
     res.send({ success: true })
 }
-module.exports = { getAllAds, addAds, getMyAds} 
+module.exports = { getAllAds, addAds, getMyAds,getAdsType} 
