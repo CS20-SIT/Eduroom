63a63c25b722b4c4171cfa3f3ec702196c546c88
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2020-12-16 11:34:27 +0700

    [ADMIN]:[ADD] admin regis, login, getProfile

--- eduroom_backend/src/utils/jwt.js
+++ eduroom_backend/src/utils/jwt.js
@@ -12,19 +12,33 @@ const jwtVerifySignOption = {
 }
 
 const generateCookieJWT = (payload) => {
-	return jwt.sign({ id: payload }, jwtSecret, jwtCookieSignOption)
+	return jwt.sign({ id: payload, role: 'admin' }, jwtSecret, jwtCookieSignOption)
 }
 
 const generateVerifyJWT = (payload) => {
 	return jwt.sign({ id: payload }, jwtSecret, jwtVerifySignOption)
 }
 
+const generateCookieAdminJWT = (payload) => {
+	return jwt.sign({ id: payload, role: 'admin' }, jwtSecret, jwtCookieSignOption)
+}
+
 const verifyVerificationJWT = (token) => {
 	return jwt.verify(token, jwtSecret, jwtVerifySignOption)
+}
+
+const verifyCookieAdminJWT = (token) => {
+	const verification = jwt.verify(token, jwtSecret, jwtVerifySignOption)
+	if(verification.role === 'admin'){
+		return verification.id
+	}
+	return null
 }
 
 module.exports = {
 	generateCookieJWT,
 	generateVerifyJWT,
 	verifyVerificationJWT,
+	generateCookieAdminJWT,
+	verifyCookieAdminJWT
 }
