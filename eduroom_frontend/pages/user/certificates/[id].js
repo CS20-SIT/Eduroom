feaed0e589f912b389c24aa0df3f989ffaf43042
GGolfz <wisarutgolf11@gmail.com>
2020-12-16 23:58:00 +0700

    [ADMIN] :[ADD] Certificate && [ADD] PDF (not success)

--- eduroom_frontend/pages/user/certificates/[id].js
+++ eduroom_frontend/pages/user/certificates/[id].js
@@ -1,5 +1,25 @@
-import React,{Fragment} from 'react'
-const CertID = () => {
-    return <Fragment></Fragment>
+import React, {Fragment} from 'react'
+import Certificate from '../../../components/user/certificate'
+const Certificates = (props) => {
+    return (
+        <div style={{width:'707px',height:'500px',border:'1px solid black'}}>
+        <Certificate data={props.data}/>
+        </div>
+    )
 }
-export default CertID
+export async function getServerSideProps({req,res,query}) {
+    const id = query.id
+    // wait for fetch firstname,lastname, coursename,finishdate from backend
+    const data = {
+        firstName:'Wisarut',lastName:'Kitticharoenphonngam',
+        courseName: 'Python For Everybody',
+        finishDate: '16 December 2020'
+    }
+    // const buffer = PDFGen.componentToPDF(<Certificate data={data}/>)
+    // res.setHeader("Content-Type","application/pdf")
+    // console.log(buffer)
+    // res.send({success:true})
+	return { props: {data} }
+}
+
+export default Certificates
