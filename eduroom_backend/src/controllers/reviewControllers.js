078ac0b01387d34932a2fecb5dd441f0860a8a20
nuttavadeepj <nuttavadee.pj@mail.kmutt.ac.th>
2020-12-18 00:17:41 +0700

    [G13] : [FINISH] Search Course

--- eduroom_backend/src/controllers/reviewControllers.js
+++ eduroom_backend/src/controllers/reviewControllers.js
@@ -1,20 +1,20 @@
-const ErrorResponse = require('../utils/errorResponse')
-const pool = require('../database/db')
-exports.getReview = async (req, res, next) => {
-	const id = req.query.courseid
-	const data = await pool.query('select * from package where packageid = $1', [id])
-	const reviewInfo = data.rows
-	res.status(200).json({ data: reviewInfo })
-}
-exports.selectCourse = async (req, res, next) => {
-	const id = req.params.id
-	console.log('id is ', id)
-	const data = await pool.query(
-		'select * form review_course  ',
-		[id]
-	)
-	const review = data.rows[0]
-	const comment = data2.rows
-	console.log(review)
-	res.status(200).json({ data: { review, comment } })
-}
+// const ErrorResponse = require('../utils/errorResponse')
+// const pool = require('../database/db')
+// exports.getReview = async (req, res, next) => {
+// 	const id = req.query.courseid
+// 	const data = await pool.query('select * from package where packageid = $1', [id])
+// 	const reviewInfo = data.rows
+// 	res.status(200).json({ data: reviewInfo })
+// }
+// exports.selectCourse = async (req, res, next) => {
+// 	const id = req.params.id
+// 	console.log('id is ', id)
+// 	const data = await pool.query(
+// 		'select * form review_course  ',
+// 		[id]
+// 	)
+// 	const review = data.rows[0]
+// 	const comment = data2.rows
+// 	console.log(review)
+// 	res.status(200).json({ data: { review, comment } })
+// }
