a7ccbeca5281319e3fc9cbf799143ec1f7f5519e
GGolfz <wisarutgolf11@gmail.com>
2020-12-18 22:01:56 +0700

    [ADMIN] : [ADD] Calendar to check event in day

--- eduroom_frontend/components/calendar/calendarCell.js
+++ eduroom_frontend/components/calendar/calendarCell.js
@@ -8,11 +8,23 @@ const Content = (props) => {
   const todayDate = props.todayDate;
   const setOpen = props.setOpen;
   const setShowDate = props.setShowDate;
-
+  const [hasEvent,setEvent] = useState(false)
+  useEffect(()=>{
+    let event = false
+    props.events?.map(el=>{
+      const sdate = new Date(el.startdate)
+      const edate = new Date(el.enddate)
+      const cdate = new Date(props.date+ ' 07:00')
+      if(cdate >= sdate && cdate <= edate){
+        event = true
+      }
+    })
+    setEvent(event)
+  },[props.events])
  
   return (
     <Fragment>
-      <div onClick={() => { setOpen(true);setShowDate(content); }} className={`${props.isToday&&content == todayDate ? 'currentDate' : ''} gridItem`} style={{cursor:"pointer"}}>{content}</div>
+      <div onClick={() => { setOpen(true);setShowDate(content); }} className={`${props.isToday&&content == todayDate ? 'currentDate' : ''} gridItem`} style={{cursor:"pointer",color:`${hasEvent?'red':'black'}`}}>{content}</div>
       <style jsx>{style}</style>
     </Fragment>
   );
