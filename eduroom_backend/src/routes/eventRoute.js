0858078ec5c9441bb47c2bdf282607050f559b3c
Gorgun <yosathorn.pengma@mail.kmutt.ac.th>
2020-12-19 16:18:17 +0700

    [G14] : [UPDATE] instructor can't not fix global Event

--- eduroom_backend/src/routes/eventRoute.js
+++ eduroom_backend/src/routes/eventRoute.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const {getGlobalEvent,getCourseEvent, createEvent, getEventInMonthYear,dEvent,getEvent,getMyCourse} = require('../controllers/event');
+const {getGlobalEvent,getCourseEvent, createEvent, getEventInMonthYear,dEvent,getEvent,getMyCourse,eEvent} = require('../controllers/event');
 const { jwtAuthenicate } = require('../middleware/jwtAuthenticate');
 router.get('/getGlobalEvent', getGlobalEvent);
 router.get('/getCourseEvent',getCourseEvent);
@@ -9,4 +9,5 @@ router.get('/getEventInMonthYear',jwtAuthenicate,getEventInMonthYear);
 router.delete('/dEvent',jwtAuthenicate,dEvent);
 router.get('/getEvent',getEvent)
 router.get('/getMyCourse',jwtAuthenicate,getMyCourse)
+router.post('/eEvent',jwtAuthenicate,dEvent);
 module.exports = router
