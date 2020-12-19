54d37a58658122268085d06b3eeb633a3d3dbdec
boyplus <thanaphon.me@mail.kmutt.ac.th>
2020-12-20 02:57:33 +0700

    [ADMIN] : [UPDATE] add to cart

--- eduroom_frontend/components/payment/cart.js
+++ eduroom_frontend/components/payment/cart.js
@@ -1,16 +1,31 @@
 import { Fragment } from 'react'
-import Course from './course'
 import style from '../../styles/course/cartStyle'
 
-const Carts = () => {
+const Cart = ({ data }) => {
 	return (
 		<Fragment>
-			<div>
-				<Course></Course>
+			<div className="box">
+				<div className="left">
+					<img src={data.picture} width="120px" height="100px"></img>
+					<div className="detail">
+						<h3 style={{ margin: '0' }}>{data.name}</h3>
+						<p style={{ color: '#5B5B5B', marginTop: '3px' }}>
+							{data.firstname} {data.lastname}
+						</p>
+					</div>
+				</div>
+				<div className="mid">
+					<div className="remove">Remove</div>
+				</div>
+				<div className="right">
+					<p className="price">
+						{'฿'} {data.price}
+					</p>
+				</div>
 			</div>
 			<style jsx>{style}</style>
 		</Fragment>
 	)
 }
 
-export default Carts
+export default Cart
