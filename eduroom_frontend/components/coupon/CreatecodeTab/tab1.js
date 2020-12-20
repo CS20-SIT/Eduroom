7ed264f506ae5abc45d08b9132263e8e80a42dce
Sakkarin <sakkarin.kmutt@mail.kmutt.ac.th>
2020-12-20 23:25:16 +0700

    [G02] : [Update] Create code constructor

--- eduroom_frontend/components/coupon/CreatecodeTab/tab1.js
+++ eduroom_frontend/components/coupon/CreatecodeTab/tab1.js
@@ -1,158 +1,211 @@
-import React, { Fragment, useState} from 'react'
+import React, { Fragment, useState } from 'react'
 import style from '../../../styles/advertisement/ads'
-import { useRouter} from 'next/router';
-import {Link,Typography,InputBase, Paper, Grid,List} from '@material-ui/core'
+import { useRouter } from 'next/router';
+import { Link, Typography, InputBase, Paper, Grid, List } from '@material-ui/core'
 
 
 
 
 const Content = () => {
   const router = useRouter();
-    return (
+  const [couponInfo, setcouponInfo] = useState({
+    codename: '',
+    description: '',
+    discount: 0,
+    duration: 0,
+    minprice: 0,
+    coinprice: 0,
+    uselimit: 0,
+    img: null,
 
-       
-        <div>
-        <div className="ad-ad-header" style={{marginLeft:"43%"}}><h3>create code</h3></div>
-        <div className="ad-ad-description" style={{margin:"-2% 0% 1% 20%"}}>Code Name
-          <Paper style={{padding: 10, backgroundColor: '#EFF0F6',width:"35%", margin:"1% 0% 1% 0%" }}>
-                        <form>
-                          <InputBase
-                            onChange={(e) =>
-                              setadInfo({ ...adInfo, adstartdate: e.target.value })
-                            }
-                            name="Codename"
-                            fullWidth
-                            autoFocus
-                            type={"text"}
-                            placeholder={"Code name"}
-                            inputProps={{ 'aria-label': 'naked' }}
-                          />
-                        </form>
-                      </Paper>
-                      Description
-                      <Paper style={{padding: 10, backgroundColor: '#EFF0F6',width:"70%", marginTop:"1%" }}>
-                        <form>
-                          <InputBase
-                            onChange={(e) =>
-                              setadInfo({ ...adInfo, adstartdate: e.target.value })
-                            }
-                            name="Description"
-                            fullWidth
-                            autoFocus
-                            type={"text"}
-                            placeholder={"Description"}
-                            inputProps={{ 'aria-label': 'naked' }}
-                          />
-                        </form>
-                      </Paper>
-          <Grid container spacing={3} style={{marginTop:"20px"}}>
-            <Grid item xs={6}>Discount
-            <Paper style={{padding: 10, backgroundColor: '#EFF0F6',width:"60%", marginTop:"1%" }}>
-                        <form>
-                          <InputBase
-                            onChange={(e) =>
-                              setadInfo({ ...adInfo, adstartdate: e.target.value })
-                            }
-                            name="Discount"
-                            fullWidth
-                            autoFocus
-                            type={"text"}
-                            placeholder={"Discount"}
-                            inputProps={{ 'aria-label': 'naked' }}
-                          />
-                        </form>
-                      </Paper>
-                      Duration
-                      <Paper style={{padding: 10, backgroundColor: '#EFF0F6',width:"60%", marginTop:"1%" }}>
-                        <form>
-                          <InputBase
-                            onChange={(e) =>
-                              setadInfo({ ...adInfo, adstartdate: e.target.value })
-                            }
-                            name="Duration"
-                            fullWidth
-                            autoFocus
-                            type={"text"}
-                            placeholder={"Duration"}
-                            inputProps={{ 'aria-label': 'naked' }}
-                          />
-                        </form>
-                      </Paper>
-                      Minimum Price
-                      <Paper style={{padding: 10, backgroundColor: '#EFF0F6',width:"60%", marginTop:"1%" }}>
-                        <form>
-                          <InputBase
-                            onChange={(e) =>
-                              setadInfo({ ...adInfo, adstartdate: e.target.value })
-                            }
-                            name="Minimum Price"
-                            fullWidth
-                            autoFocus
-                            type={"text"}
-                            placeholder={"Minimum Price"}
-                            inputProps={{ 'aria-label': 'naked' }}
-                          />
-                        </form>
-                      </Paper>
-            </Grid>
-            <Grid item xs={6} style={{marginLeft:"-10%"}}>Coin Price
-            <Paper style={{padding: 10, backgroundColor: '#EFF0F6',width:"60%", marginTop:"1%" }}>
-                        <form>
-                          <InputBase
-                            onChange={(e) =>
-                              setadInfo({ ...adInfo, adstartdate: e.target.value })
-                            }
-                            name="Coin Price"
-                            fullWidth
-                            autoFocus
-                            type={"text"}
-                            placeholder={"Coin Price"}
-                            inputProps={{ 'aria-label': 'naked' }}
-                          />
-                        </form>
-                      </Paper>
-                      Code Picture
-                      <Paper style={{padding: 10, backgroundColor: '#EFF0F6',width:"60%", marginTop:"1%" }}>
-                        <form>
-                          <InputBase
-                            onChange={(e) =>
-                              setadInfo({ ...adInfo, adstartdate: e.target.value })
-                            }
-                            name="Code Picture"
-                            fullWidth
-                            autoFocus
-                            type={"file"}
-                            placeholder={"Code Picture"}
-                            inputProps={{ 'aria-label': 'naked' }}
-                          />
-                        </form>
-                      </Paper>
-                      CodeType
-                      <Paper style={{padding: 10, backgroundColor: '#EFF0F6',width:"60%", marginTop:"1%" }}>
-                        <form>
-                          <InputBase
-                            onChange={(e) =>
-                              setadInfo({ ...adInfo, adstartdate: e.target.value })
-                            }
-                            name="CodeType"
-                            fullWidth
-                            autoFocus
-                            type={"text"}
-                            placeholder={"Minimum Price"}
-                            inputProps={{ 'aria-label': 'naked' }}
-                          />
-                        </form>
-                      </Paper></Grid>
+  });
+
+  const changeCouponPic = (e) => {
+    let newValue = e.target.files[0]
+    setcouponInfo({ ...couponInfo, img: newValue })
+  }
+
+  const handleClick = async () => {
+    try {
+      const myForm = new FormData()
+      myForm.append('coupon-img', couponInfo.img)
+      console.log(myForm)
+      // const res = await api.post('/api/coupon/upload/picture', myForm)
+      const body = {
+        codename: couponInfo.codename,
+        description: couponInfo.description,
+        discount: couponInfo.discount,
+        duration: couponInfo.duration,
+        minprice: couponInfo.minprice,
+        coinprice: couponInfo.coinprice,
+        uselimit: couponInfo.uselimit,
+        img: couponInfo.img
+      }
+
+      console.log(body);
+      handleSubmit(body);
+    } catch (err) { }
+
+  }
+  const handleSubmit = async (body) => {
+    try {
+      // const response = await api.post('/api/coupon/createCouponForSell', body)
+      console.log('success')
+      props.complete()
+    } catch (err) { }
+  }
+  return (
+
+    <div>
+      <div className="ad-ad-header" style={{ marginLeft: "43%" }}><h3>create code</h3></div>
+      <div className="ad-ad-description" style={{ margin: "-2% 0% 1% 20%" }}>Code Name
+          <Paper style={{ padding: 10, backgroundColor: '#EFF0F6', width: "35%", margin: "1% 0% 1% 0%" }}>
+          <form>
+            <InputBase
+              onChange={(e) =>
+                setcouponInfo({ ...couponInfo, codename: e.target.value })
+              }
+              name="codename"
+              fullWidth
+              autoFocus
+              type={"text"}
+              placeholder={"Code name"}
+              inputProps={{ 'aria-label': 'naked' }}
+            />
+          </form>
+        </Paper>
+        Description
+                      <Paper style={{ padding: 10, backgroundColor: '#EFF0F6', width: "70%", marginTop: "1%" }}>
+          <form>
+            <InputBase
+              onChange={(e) =>
+                setcouponInfo({ ...couponInfo, description: e.target.value })
+              }
+              name="description"
+              fullWidth
+              autoFocus
+              type={"text"}
+              placeholder={"Description"}
+              inputProps={{ 'aria-label': 'naked' }}
+            />
+          </form>
+        </Paper>
+        <Grid container spacing={3} style={{ marginTop: "20px" }}>
+          <Grid item xs={6} >Discount
+            <Paper style={{ padding: 10, backgroundColor: '#EFF0F6', width: "60%", marginTop: "1%" }}>
+              <form>
+                <InputBase
+                  onChange={(e) =>
+                    setcouponInfo({ ...couponInfo, discount: e.target.value })
+                  }
+                  name="Discount"
+                  fullWidth
+                  autoFocus
+                  type={"number"}
+                  placeholder={"Discount %"}
+                  inputProps={{ 'aria-label': 'naked' }}
+                />
+              </form>
+            </Paper>
+            Duration
+                      <Paper style={{ padding: 10, backgroundColor: '#EFF0F6', width: "60%", marginTop: "1%" }}>
+              <form>
+                <InputBase
+                  onChange={(e) =>
+                    setcouponInfo({ ...couponInfo, duration: e.target.value })
+                  }
+                  name="duration"
+                  fullWidth
+                  autoFocus
+                  type={"number"}
+                  placeholder={"Duration day(s)"}
+                  inputProps={{ 'aria-label': 'naked' }}
+                />
+              </form>
+            </Paper>
+            Minimum Price
+                      <Paper style={{ padding: 10, backgroundColor: '#EFF0F6', width: "60%", marginTop: "1%" }}>
+              <form>
+                <InputBase
+                  onChange={(e) =>
+                    setcouponInfo({ ...couponInfo, minprice: e.target.value })
+                  }
+                  name="minimumPrice"
+                  fullWidth
+                  autoFocus
+                  type={"number"}
+                  placeholder={"Minimum Price"}
+                  inputProps={{ 'aria-label': 'naked' }}
+                />
+              </form>
+            </Paper>
           </Grid>
+          <Grid item xs={6} style={{ marginLeft: "-10%" }}>Coin Price
+            <Paper style={{ padding: 10, backgroundColor: '#EFF0F6', width: "60%", marginTop: "1%" }}>
+              <form>
+                <InputBase
+                  onChange={(e) =>
+                    setcouponInfo({ ...couponInfo, coinprice: e.target.value })
+                  }
+                  name="coinprice"
+                  fullWidth
+                  autoFocus
+                  type={"number"}
+                  placeholder={"Coin Price"}
+                  inputProps={{ 'aria-label': 'naked' }}
+                />
+              </form>
+            </Paper>
+            Code Picture
+                      <Paper style={{ padding: 10, backgroundColor: '#EFF0F6', width: "60%", marginTop: "1%" }}>
+              <form>
+                <InputBase
+                  onChange={changeCouponPic}
+                  accept="image/*"
+                  name="couponimg"
+                  fullWidth
+                  autoFocus
+                  type={"file"}
+                  placeholder={"attrach your ad img"}
+                  inputProps={{ 'aria-label': 'naked' }}
+                />
+              </form>
+            </Paper>
+            CodeType
+                      <Paper style={{ padding: 10, backgroundColor: '#EFF0F6', width: "60%", marginTop: "1%" }}>
+              <form>
+                <InputBase
+                  onChange={(e) =>
+                    setcouponInfo({ ...couponInfo, codetype: e.target.value })
+                  }
+                  name="codetype"
+                  fullWidth
+                  autoFocus
+                  type={"text"}
+                  placeholder={"code type"}
+                  inputProps={{ 'aria-label': 'naked' }}
+                />
+              </form>
+            </Paper>
+          </Grid>
+        </Grid>
+        <div style={{ marginLeft: '-30%', marginTop: '50px', display: 'flex', justifyContent: 'center', alignItems: 'center', paddingBottom: '50px' }}>
+          <button
+            className="next-button"
+            onClick={handleClick}
+          >
+            <a className="ad-button-text">Submit</a>
+          </button>
         </div>
-         <style jsx>
-         {style}
-       </style> 
-       </div>
-        
-     
+      </div>
+      <style jsx>
+        {style}
+      </style>
+    </div>
 
-        
-        )
-    }
-    export default Content
+
+
+
+  )
+}
+export default Content
