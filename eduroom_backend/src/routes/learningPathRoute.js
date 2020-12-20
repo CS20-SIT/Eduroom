1825a620645b2006b2618fc216481551f3ecee07
boyplus <thanaphon.me@mail.kmutt.ac.th>
2020-12-20 17:57:28 +0700

    [ADMIN] : [INIT] quiz in learning path

--- eduroom_backend/src/routes/learningPathRoute.js
+++ eduroom_backend/src/routes/learningPathRoute.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const { getPathList, getNodeByPath, getExerciseByNodeId } = require('../controllers/learningpath/learningContoller')
+const { getPathList, getNodeByPath, getExerciseByNodeId, getNodeType } = require('../controllers/learningpath/learningContoller')
 const { jwtAuthenicate } = require('../middleware/jwtAuthenticate')
 router.get('/', getPathList)
 router.get('/path', jwtAuthenicate, getNodeByPath)
 router.get('/exercise', jwtAuthenicate, getExerciseByNodeId)
+router.get('/nodeType', jwtAuthenicate, getNodeType)
 
 module.exports = router
