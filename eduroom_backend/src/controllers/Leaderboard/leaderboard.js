9179455ac9144c39fd82fb449cd9d228adeed3c2
Warot Janpinitrat <newjanpi1540@gmail.com>
2020-12-20 21:36:16 +0700

    [G06] : [FIX] Userbar.js

--- eduroom_backend/src/controllers/Leaderboard/leaderboard.js
+++ eduroom_backend/src/controllers/Leaderboard/leaderboard.js
@@ -27,22 +27,5 @@ exports.sortRank = async (req, res, next) => {
   return
 }
 
-// const sortTitle = async (req, res, next) => {
-//   await pool.query('UPDATE leaderboard '+
-//   'SET titleid = CASE '+
-//   'WHEN xp >= 1800 then 1 '+
-//   'WHEN xp between 1499 and 1800 then 2 '+
-//   'WHEN xp between 1199 and 1500 then 3 '+
-//   'WHEN xp between 899 and 1200 then 4 '+
-//   'WHEN xp between 599 and 900 then 5 '+
-//   'WHEN xp between 299 and 600 then 6 '+
-//   'WHEN xp between 0 and 300 then 7 '+
-//   'ELSE 0 '+
-//   'END;')
 
-//   const title = await pool.pool('SELECT  FROM leaderboard')
-
-//   res.status(200).json(title)
-//   return
-// }
 
