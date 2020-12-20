659b4128516e46470c3b851c1c6706057112f4cb
Chalanthorn <s.chalanthorn@gmail.com>
2020-12-20 15:44:52 +0700

    [G10] : [UPDATE] Coding pages

--- eduroom_frontend/components/graderSubmit/problems/ProblemCode.js
+++ eduroom_frontend/components/graderSubmit/problems/ProblemCode.js
@@ -1,8 +1,20 @@
-import React, { Fragment } from "react"
-import style from "../../../styles/graderSubmit/problems/problemCode"
+import React, { Fragment } from 'react'
+import style from '../../../styles/graderSubmit/problems/problemCode'
 
 const ProblemCode = (props) => {
-  return <Fragment></Fragment>
+	return (
+		<Fragment>
+			<form action="">
+				<div className="box">
+					<textarea></textarea>
+				</div>
+				<div className="submit-button">
+					<input type="submit" value="Submit" />
+				</div>
+			</form>
+			<style jsx>{style}</style>
+		</Fragment>
+	)
 }
 
 export default ProblemCode
