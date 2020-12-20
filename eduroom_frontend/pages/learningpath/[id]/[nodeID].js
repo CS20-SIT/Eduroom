d399999826f53a36533a747b03642e7a83fc0709
boyplus <thanaphon.me@mail.kmutt.ac.th>
2020-12-20 16:27:47 +0700

    [ADMIN] : [ADD] check answer in learning path

--- eduroom_frontend/pages/learningpath/[id]/[nodeID].js
+++ eduroom_frontend/pages/learningpath/[id]/[nodeID].js
@@ -20,42 +20,62 @@ const NodeExercise = ({ id, nodeID }) => {
 	useEffect(() => {
 		fetchLearningPath()
 	}, [])
-
-	const renderButtons = () => {
+	const getNextClass = () => {
+		return correct ? 'active' : 'disable'
+	}
+	const getText = () => {
 		if (correct === true) {
-		} else {
+			return <span style={{ color: 'green' }}>Correct!!</span>
+		} else if (correct === false) {
+			return <span style={{ color: 'red' }}>Wrong!!</span>
 		}
+	}
+	const checkAnswer = () => {
+		setCorrect(text.toLowerCase() === learningPath.answer.toLowerCase())
 	}
 	const renderPage = () => {
 		if (!learningPath) return null
 		return (
 			<Fragment>
-				<div className="container">
-					<div className="card">
-						<div onClick={() => router.push('/learningpath')}>
-							<i className="fas fa-chevron-left back"></i>
-						</div>
-						<div style={{ margin: '20px' }}>
-							<h1 className="blue" style={{ margin: '0' }}>
-								{learningPath.path_name} Path
-							</h1>
-							<h3 style={{ margin: '0' }}>{learningPath.node_name}</h3>
-							<h4>Question: {learningPath.question}</h4>
-							<div className="form">
-								<div style={{ width: '300px' }}>
-									<input
-										type="text"
-										className="textField"
-										placeholder="Your answer"
-										value={text}
-										onChange={(e) => setText(e.target.value)}
-									></input>
+				<div style={{ display: 'flex', justifyContent: 'center' }}>
+					<div className="container">
+						<div className="card">
+							<div onClick={() => router.push('/learningpath')}>
+								<i className="fas fa-chevron-left back"></i>
+							</div>
+							<div style={{ margin: '20px' }}>
+								<h1 className="blue" style={{ margin: '0' }}>
+									{learningPath.path_name} Path
+								</h1>
+								<h3 style={{ margin: '0' }}>{learningPath.node_name}</h3>
+								<h4>Question: {learningPath.question}</h4>
+								<div className="form">
+									<div style={{ display: 'flex', alignItems: 'center' }}>
+										<div style={{ width: '300px' }}>
+											<input
+												type="text"
+												className="textField"
+												placeholder="Your answer"
+												value={text}
+												onChange={(e) => setText(e.target.value)}
+											></input>
+										</div>
+										<div className="check" onClick={checkAnswer}>
+											Check answer
+										</div>
+									</div>
+									<div>
+										<div style={{ fontSize: '20px' }} className={`${getNextClass()}`}>
+											Next <i className={`fas fa-chevron-right `} style={{ fontSize: '20px' }}></i>
+										</div>
+									</div>
 								</div>
-								<div>{renderButtons()}</div>
+								<div>{getText()}</div>
 							</div>
 						</div>
 					</div>
 				</div>
+
 				<style jsx>{style}</style>
 			</Fragment>
 		)
--- eduroom_frontend/styles/learningPathStyles/exercise.js
+++ eduroom_frontend/styles/learningPathStyles/exercise.js
@@ -1,7 +1,8 @@
 import css from 'styled-jsx/css'
 export default css`
 	.container {
-		padding: 20px;
+    padding: 20px;
+    width: 60%;
 	}
 	.blue {
 		color: #3d467f;
@@ -22,24 +23,44 @@ export default css`
 		border: none;
 		font-size: 1.1em;
 		color: #3d467f;
+	}
+
+	.form {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+	}
+	.back {
+		font-size: 20px;
+	}
+	.back:hover {
+		cursor: pointer;
+	}
+	.active {
+		opacity: 1;
+	}
+	.active:hover {
+		cursor: pointer;
+	}
+	.disable {
+		opacity: 0.5;
+	}
+	.disable:hover {
+		cursor: default;
   }
   
-  .form{
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
-  .back{
-    font-size: 20px;
-  }
-  .next{
-    font-size: 20px;
-  }
-  .back:hover{
-    cursor: pointer;
+  .error{
+    
   }
 
-  .next:hover{
+	.check {
+		padding: 10px;
+		background: #3d467f;
+    border-radius: 10px;
+    color: white;
+    margin-left: 20px;
+  }
+  .check:hover{
     cursor: pointer;
   }
 `
