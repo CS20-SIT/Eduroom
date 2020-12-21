958c7fa2e8f9d0b3c3bffdf466fdc765e461140c
Wuthipong <wuthipong.wst@mail.kmutt.ac.th>
2020-12-22 02:23:56 +0700

    [G02]Auto stash before merge of "master" and "origin/master"

--- eduroom_backend/src/routes/couponRoute.js
+++ eduroom_backend/src/routes/couponRoute.js
@@ -5,10 +5,16 @@ const { jwtAuthenicate, jwtAdminAuthenticate  } = require('../middleware/jwtAuth
 const { uploadToGCSHandler } = require('../middleware/multer')
 
 const {Upload,CreateCodeForSale,GetCodeType,UseCode,GetDiscountFromCoupon } = require("../controllers/coupon/couponController");
+const {Upload,CreateCodeForSale,GetCodeType, GetDataforTable, GetCoupon, GetCouponById, updateStatus} = require("../controllers/coupon/couponController");
 
   router.post('/upload/picture', jwtAdminAuthenticate ,uploadToGCSHandler('coupon/picture'), Upload)
   router.post('/createCodeForSale', jwtAdminAuthenticate,CreateCodeForSale)
   router.get('/GetCodeType', jwtAdminAuthenticate,GetCodeType)
   router.get('/UseCode',jwtAuthenicate,UseCode)
   router.get('/GetDiscountFromCoupon',GetDiscountFromCoupon)
+  router.get('/getDataFromTable',GetDataforTable);
+  router.get('/getCouponDetail',  GetCoupon);
+  router.get('/getCouponById', GetCouponById);
+  router.get('/updateStatus',  updateStatus);
+
   module.exports = router;
