b5054a333bb8a5198f135fafd5f74aa63cd96fbd
Sakkarin <sakkarin.kmutt@mail.kmutt.ac.th>
2020-12-22 01:53:29 +0700

    [G02] : [Update] Ads billing page

---
+++ eduroom_frontend/components/advertisement/adPaymentDetail.js
@@ -0,0 +1,254 @@
+import React, { Fragment, useState, useEffect } from 'react'
+import style from '../../styles/course/paymentPage'
+import Grid from '@material-ui/core/Grid'
+import OrderDetail from './payment/orderDetail'
+import CreditBox from './payment/creditBox'
+import PaypalBox from './payment/paypalBox'
+import Button from '@material-ui/core/Button'
+import Country from '../../components/payment/country'
+import { getItems, isInCart, addToCart, removeFromCart } from '../../utils/cart'
+import api from "../../api";
+
+const AdList = props => {
+    return (
+        <div>
+            <div style={{ marginRight: 95 }} className="col-6">adid#{props.adid}  {props.amount}</div>
+           
+            <style jsx>{style}</style>
+        </div>
+    )
+}
+
+const Checkout = () => {
+    const [open, setOpen] = useState(false)
+    const [type, setType] = useState('credit')
+    const [courseCart, setCourseCart] = useState([])
+    const [packageCart, setPackageCart] = useState([])
+    useEffect(() => {
+        const courseInCart = getItems('course')
+        const packageInCart = getItems('package')
+        console.log('course', courseInCart)
+        console.log('package', packageInCart)
+        setCourseCart(courseInCart)
+        setPackageCart(packageInCart)
+    }, [])
+    const handleOpenDialog = (e) => {
+        e.preventDefault()
+        setOpen(true)
+        handleSubmit()
+    }
+    const handleCloseDialog = (e) => {
+        e.preventDefault()
+        setOpen(false)
+    }
+    const handleSubmit = () => {
+        console.log(type)
+    }
+    const [data, setData] = useState([]);
+    const [total, setTotal] = useState([]);
+    useEffect(() => {
+        const fetchData = async () => {
+            const res = await api.get('/api/ads/getAdsToBills');
+            console.log(res.data);
+            setData(res.data);
+            const res2 = await api.get('/api/ads/getBillAdsTotal');
+            console.log(res2.data[0]);
+            setTotal(res2.data[0].totalprice);
+
+        };
+        fetchData();
+    }, []);
+    const arr = data.map((data, index) => {
+        return (
+            <div key={data.adid}>
+                <AdList adid={data.adid}
+                    amount={data.amount}
+                    index={index}
+                ></AdList>
+            </div>
+        )
+    })
+
+    return (
+        <Fragment>
+            <div style={{ backgroundImage: `url('/images/big-bg.svg')`, backgroundSize: 'cover', paddingTop: '3%', height: '100%' }} >
+                <div style={{ display: 'flex', justifyContent: 'center' }}>
+                    <div className="checkout">Checkout</div>
+                </div>
+
+                {/* <div style={{ display: 'grid', justifyContent: 'center' }}> */}
+                    <div>
+                        <div className="card">
+                            <Grid container className="test">
+                                <Grid item xs={6} className="divleft">
+                                    <div style={{ paddingLeft: 40 }}>
+                                        <div style={{ marginLeft: 15, marginTop: 20 }}>
+                                            <h1 className="billDiv">Billing Address</h1>
+
+                                            <div className="col-6">
+                                                <Country />
+                                                <div onChange={(event) => setType(event.target.value)}>
+                                                    <div>
+                                                        <input
+                                                            type="radio"
+                                                            name="Transaction"
+                                                            value="credit"
+                                                            checked={type === 'credit'}
+                                                            style={{
+                                                                cursor: 'pointer',
+                                                                height: 18,
+                                                                width: 18,
+                                                            }}
+                                                        ></input>
+                                                        <label
+                                                            for="credit"
+                                                            style={{
+                                                                fontWeight: 550,
+                                                                fontSize: 18,
+                                                                paddingBottom: 20,
+                                                            }}
+                                                        >
+                                                            Credit or Debit
+															<img
+                                                                src="/images/package/vesa.svg"
+                                                                className="vesa"
+                                                                style={{
+                                                                    width: 50,
+                                                                    height: 50,
+                                                                    paddingTop: 30,
+                                                                    marginLeft: 10,
+                                                                }}
+                                                            />
+                                                            <img
+                                                                src="/images/package/masterCard.svg"
+                                                                className="masterCard"
+                                                                style={{
+                                                                    width: 50,
+                                                                    height: 50,
+                                                                    marginLeft: 5,
+                                                                    paddingTop: 30,
+                                                                }}
+                                                            />
+                                                        </label>
+                                                    </div>
+                                                    <div style={{ marginBottom: 30 }}>
+                                                        <input
+                                                            type="radio"
+                                                            name="Transaction"
+                                                            value="paypal"
+                                                            checked={type == 'paypal'}
+                                                            style={{
+                                                                cursor: 'pointer',
+                                                                height: 18,
+                                                                width: 18,
+                                                            }}
+                                                        />
+                                                        <label
+                                                            for="paypal"
+                                                            style={{
+                                                                fontWeight: 550,
+                                                                fontSize: 18,
+                                                                marginRight: 20,
+                                                            }}
+                                                        >
+                                                            Paypal
+															<img
+                                                                src="/images/package/paypal.svg"
+                                                                className="paypal"
+                                                                style={{
+                                                                    width: 80,
+                                                                    height: 20,
+                                                                    marginLeft: 10,
+                                                                }}
+                                                            />
+                                                        </label>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div style={{ display: 'flex', justifyContent: 'center' }}>
+                                            {type == 'credit' ? <CreditBox /> : <PaypalBox />}
+                                        </div>
+                                    </div>
+                                </Grid>
+
+                                <Grid item xs={6} style={{ paddingTop: 30 }}>
+                                    <div style={{ display: 'flex', justifyContent: 'center' }}>
+                                        <div className="whitebox-ad">
+                                            <h1
+                                                style={{
+                                                    fontSize: 20,
+                                                    marginLeft: 20,
+                                                    fontWeight: 700,
+                                                }}
+                                            >
+                                                Summary
+											</h1>
+
+                                            <div
+                                                style={{
+                                                    display: 'flex',
+                                                    justifyContent: 'flex-start',
+                                                    fontWeight: 500,
+                                                    margin: '10px 0px 10px 50px',
+                                                }}
+                                            >
+                                                {arr}
+                                            </div>
+
+                                            <div
+                                                style={{
+                                                    display: 'flex',
+                                                    flexDirection: 'row',
+                                                    justifyContent: 'center',
+                                                }}
+                                            >
+
+                                            </div>
+
+
+                                            <hr style={{ width: 300, marginBottom: 10 }}></hr>
+
+                                            <div
+                                                style={{
+                                                    display: 'flex',
+                                                    justifyContent: 'flex-start',
+                                                    fontWeight: 500,
+                                                    margin: '10px 0px 20px 50px',
+                                                }}
+                                            >
+                                                <div style={{ marginRight: 160 }} className="col-6">
+                                                    Total
+												</div>
+                                                <div style={{ color: 'black' }} className="col-6">
+                                                    ฿500
+												</div>
+                                            </div>
+                                            <div className="text">
+                                                Eduroom is required by law to collect applicable transaction taxes for purchases made in certain
+												tax jurisdictions.
+											</div>
+                                        </div>
+                                    </div>
+                                </Grid>
+                            </Grid>
+
+                            {/* <div style={{ display: 'flex', flexDirection: 'row' }}>
+								<div className="col-6" style={{ marginTop: 50, width: 550 }}>
+									<OrderDetail />
+								</div>
+
+								<div className="col-6" style={{}}></div>
+							</div> */}
+                        </div>
+                    </div>
+                </div>
+
+
+                <style jsx>{style}</style>
+            {/* </div> */}
+        </Fragment>
+    )
+}
+export default Checkout
---
+++ eduroom_frontend/components/advertisement/payment/creditBox.js
@@ -0,0 +1,285 @@
+import React, { Fragment, useEffect, useState } from 'react'
+import style from '../../../styles/course/paymentPage'
+import Button from '@material-ui/core/Button'
+import DialogTitle from '@material-ui/core/DialogTitle'
+import Dialog from '@material-ui/core/Dialog'
+import DialogContent from '@material-ui/core/DialogContent'
+import { useRouter } from 'next/router'
+
+const creditBox = () => {
+	const router = useRouter()
+	const [open, setOpen] = useState(false)
+	const [type, setType] = useState('credit')
+
+	const handleOpenDialog = (e) => {
+		console.log('createForm.month', createForm.month)
+		console.log('createForm.year', createForm.year)
+		console.log('createForm.name', createForm.name)
+
+		e.preventDefault()
+		if (
+			createForm.cardNum.length == 12 &&
+			createForm.month != null &&
+			createForm.year != null &&
+			createForm.secure.length == 4 &&
+			createForm.name != null
+		) {
+			setOpen(true)
+			handleSubmit()
+		} else {
+			checkCreditCardDetail()
+		}
+	}
+	const handleCloseDialog = (e) => {
+		e.preventDefault()
+		setOpen(false)
+	}
+	const handleSubmit = () => {
+		console.log(type)
+	}
+	useEffect(() => {
+		console.log(type)
+	}, [type])
+
+	const [createForm, setForm] = useState({
+		name: '',
+		cardNum: '',
+		secure: '',
+		month: '',
+		year: '',
+	})
+	const checkCreditCardDetail = () => {
+		if (createForm.cardNum.length != 12) {
+			alert('Please check your card number')
+		}
+		if (createForm.secure.length != 4) {
+			alert('Please check your secure number')
+		}
+		if (createForm.name == null) {
+			alert('Please enter your name')
+		}
+		if (createForm.month == null) {
+			alert('Please enter month')
+		}
+		if (createForm.year == null) {
+			alert('Please enter year')
+		}
+	}
+	const handleChange = (e) => {
+		e.preventDefault()
+		setForm({ ...createForm, [e.target.name]: e.target.value })
+  }
+  const handleChangeMonth = (e) => {
+		e.preventDefault()
+		setForm({ ...createForm, [e.target.month]: e.target.value })
+	}
+
+	return (
+		<Fragment>
+			<div className="purplebox">
+				<div className="inner-purple">
+					<div className="input-name">
+						{' '}
+						<input
+							type="text"
+							className="input-text"
+							placeholder="Your name on card"
+							style={{ borderRadius: 10, border: 'none', paddingLeft: '16px' }}
+							name="name"
+							type={'text'}
+							value={createForm.name}
+							onChange={handleChange}
+						/>
+					</div>
+					<div className="input-name">
+						{' '}
+						<input
+							type="text"
+							className="input-text"
+							placeholder="Your card number"
+							style={{ borderRadius: 10, border: 'none', paddingLeft: '16px' }}
+							name="cardNum"
+							type={'text'}
+							value={createForm.cardNum}
+							onChange={handleChange}
+						/>
+					</div>
+					<div className="input-name">
+						<div style={{ width: '25%', marginRight: '1.5%' }}>
+							<select className="input-text" style={{ cursor: 'pointer', paddingLeft: '5px' }}>
+								<option value={createForm.month} onChange={handleChangeMonth} disabled selected>
+									MM
+								</option>
+								<option>01</option>
+								<option>02</option>
+								<option>03</option>
+								<option>04</option>
+								<option>05</option>
+								<option>06</option>
+								<option>07</option>
+								<option>08</option>
+								<option>09</option>
+								<option>10</option>
+								<option>11</option>
+								<option>12</option>
+							</select>
+						</div>
+						<div style={{ width: '25%', margin: '0% 1.5%' }}>
+							<select className="input-text" style={{ cursor: 'pointer', paddingLeft: '5px' }}>
+								<option value="year" disabled selected>
+									YY
+								</option>
+								<option>20</option>
+								<option>21</option>
+								<option>22</option>
+								<option>23</option>
+								<option>24</option>
+								<option>25</option>
+								<option>26</option>
+								<option>27</option>
+								<option>28</option>
+								<option>29</option>
+								<option>30</option>
+								<option>31</option>
+								<option>32</option>
+								<option>33</option>
+								<option>34</option>
+								<option>35</option>
+								<option>36</option>
+								<option>37</option>
+								<option>38</option>
+								<option>39</option>
+								<option>40</option>
+							</select>
+						</div>
+						<div style={{ width: '50%', marginLeft: '1.5%' }}>
+							<input
+								type="text"
+								className="input-text"
+								placeholder="Security Code"
+								style={{
+									borderRadius: 10,
+									border: 'none',
+									paddingLeft: '16px',
+								}}
+								name="secure"
+								type={'text'}
+								value={createForm.secure}
+								onChange={handleChange}
+							/>
+						</div>
+					</div>
+				</div>
+
+				<div
+					style={{
+						display: 'flex',
+						justifyContent: 'flex-start',
+						paddingRight: 150,
+						flexDirection: 'row',
+					}}
+				>
+					<img
+						src="/images/package/newLock.svg"
+						className="google-logo"
+						style={{ width: 35, height: 35, marginRight: 2, marginLeft: 30 }}
+					/>
+					<div style={{ color: '#5B5B5B' }} className="col-6">
+						Secure<br></br>Connection
+					</div>
+
+					<div className="col-6" style={{ marginLeft: 70 }}>
+						<div className="compay">
+							<Button
+								variant="contained"
+								style={{
+									backgroundColor: '#FB9CCB',
+									color: 'white',
+									borderRadius: 24,
+									width: 180,
+									height: 30,
+								}}
+								onClick={handleOpenDialog}
+								// onClick={() => checkCreditCardDetail(),handleOpenDialog()}
+							>
+								Complete Payment
+							</Button>
+
+							<Dialog open={open} onClose={handleCloseDialog}>
+								<div>
+									<div
+										style={{
+											backgroundColor: 'white',
+											border: '5px solid #F2ECFE',
+										}}
+									>
+										<DialogTitle style={{ textAlign: 'center' }}>
+											<div
+												style={{
+													fontSize: '2em',
+													color: '#A880F7',
+													fontWeight: 700,
+													marginTop: 20,
+												}}
+											>
+												PURECHASED!
+											</div>
+										</DialogTitle>
+
+										<DialogContent
+											style={{
+												width: '450px',
+												height: '400px',
+												display: 'flex',
+												justifyContent: 'center',
+											}}
+										>
+											<img
+												src="/images/package/purchesed.svg"
+												className="google-logo"
+												style={{ width: 350, height: 350 }}
+											/>
+										</DialogContent>
+
+										<div
+											style={{
+												display: 'flex',
+												justifyContent: 'center',
+												paddingBottom: 30,
+											}}
+										>
+											<div
+												style={{
+													display: 'flex',
+													justifyContent: 'center',
+													paddingBottom: 30,
+												}}
+											>
+												<Button
+													variant="contained"
+													style={{
+														backgroundColor: '#FB9CCB',
+														color: 'white',
+														borderRadius: 24,
+														width: 200,
+														height: 40,
+													}}
+													onClick={() => router.push('/course')}
+												>
+													Go to course
+												</Button>
+											</div>
+										</div>
+									</div>
+								</div>
+							</Dialog>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<style jsx>{style}</style>
+		</Fragment>
+	)
+}
+export default creditBox
