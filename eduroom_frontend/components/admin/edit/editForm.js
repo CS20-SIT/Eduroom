fdd51d05d8200d3ac43a98c90a0fcdc197c5efab
GGolfz <wisarutgolf11@gmail.com>
2020-12-21 16:13:57 +0700

    [ADMIN] : [ADD] Edit Password Form

--- eduroom_frontend/components/admin/edit/editForm.js
+++ eduroom_frontend/components/admin/edit/editForm.js
@@ -1,19 +1,43 @@
-import React, {Fragment,useState} from 'react'
+import React, { Fragment, useState } from 'react'
 import EditSection from './editSection'
 const EditForm = () => {
-    const [open,setOpen] = useState('')
-    const handleToggle = (type) => {
-        if(open == type){
-            setOpen('')
-        } else{
-            setOpen(type)
-        }
-    } 
-    return (
-        <Fragment>
-            <EditSection type="profile" open={open=='profile'} handleToggle = {handleToggle}/>
-            <EditSection type="password" open={open=='password'} handleToggle = {handleToggle}/>
-        </Fragment>
-    )
+	const [open, setOpen] = useState('')
+	const handleToggle = (type) => {
+		if (open == type) {
+			setOpen('')
+		} else {
+			setOpen(type)
+		}
+	}
+	const type = ["edit profile","password"]
+	return (
+		<Fragment>
+			<div className="admin-edit-form">
+				<div style={{marginBottom:'1rem'}}>
+					<img src="/images/admin/edit_profile.svg" alt="edit-profile" />
+				</div>
+				{
+					type.map(t => {
+						return (
+							<EditSection type={t} open={open == t} handleToggle={handleToggle} />
+						)
+					})
+				}
+			</div>
+			<style jsx>
+				{
+					`
+					.admin-edit-form {
+						padding: 2.5rem 5rem;
+						display: flex;
+						flex-flow: column;
+						align-items: center;
+						justify-content: center;
+						width: 100%;
+					}`
+				}
+			</style>
+		</Fragment>
+	)
 }
 export default EditForm
