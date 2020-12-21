3639e383253a82daaddf930b4db1bb54cc7552bb
Thanaphon <thanaphon.me@mail.kmutt.ac.th>
2020-12-22 02:55:31 +0700

    [ADMIN] : [UPDATE] rank and level of user

--- eduroom_backend/src/controllers/Leaderboard/leaderboard.js
+++ eduroom_backend/src/controllers/Leaderboard/leaderboard.js
@@ -1,31 +1,54 @@
 const ErrorResponse = require('../../utils/errorResponse')
 const pool = require('../../database/db')
 
-
 exports.sortRank = async (req, res, next) => {
-  await pool.query('UPDATE leaderboard '+
-  'SET titleid = CASE '+
-  'WHEN xp >= 1800 then 1 '+
-  'WHEN xp between 1499 and 1800 then 2 '+
-  'WHEN xp between 1199 and 1500 then 3 '+
-  'WHEN xp between 899 and 1200 then 4 '+
-  'WHEN xp between 599 and 900 then 5 '+
-  'WHEN xp between 299 and 600 then 6 '+
-  'WHEN xp between 0 and 300 then 7 '+
-  'ELSE 0 '+
-  'END;')
-  const rank = await pool.query(
-      'select up.displayname, lt.titlename, leaderboard.xp'+
-      ' from leaderboard '+
-      'join user_profile up on leaderboard.userid = up.userid '+ 
-      'join leaderboard_title lt on leaderboard.titleid = lt.titleid'
-      +' order by leaderboard.xp DESC; ')
-      
-      
-  // res.status(200).json({success:true,msg: 'test from backend',time:time.rows[0]})
-  res.status(200).json(rank.rows)
-  return
+	await pool.query(
+		'UPDATE leaderboard ' +
+			'SET titleid = CASE ' +
+			'WHEN xp >= 1800 then 1 ' +
+			'WHEN xp between 1499 and 1800 then 2 ' +
+			'WHEN xp between 1199 and 1500 then 3 ' +
+			'WHEN xp between 899 and 1200 then 4 ' +
+			'WHEN xp between 599 and 900 then 5 ' +
+			'WHEN xp between 299 and 600 then 6 ' +
+			'WHEN xp between 0 and 300 then 7 ' +
+			'ELSE 0 ' +
+			'END;'
+	)
+	const rank = await pool.query(
+		'select up.displayname, lt.titlename, leaderboard.xp' +
+			' from leaderboard ' +
+			'join user_profile up on leaderboard.userid = up.userid ' +
+			'join leaderboard_title lt on leaderboard.titleid = lt.titleid' +
+			' order by leaderboard.xp DESC; '
+	)
+
+	// res.status(200).json({success:true,msg: 'test from backend',time:time.rows[0]})
+	res.status(200).json(rank.rows)
+	return
 }
 
-
-
+exports.getUserDetail = async (req, res, next) => {
+	try {
+		const userid = req.user.id
+		const result = await pool.query(`SELECT totalxp as xp from user_xp where userid = $1`, [userid])
+		const all_rank = await pool.query(`SELECT userid from user_xp order by totalxp desc`)
+		let rank = 1
+		for (let i = 0; i < all_rank.rows.length; i++) {
+			if (all_rank.rows[i].userid === userid) {
+				rank = i + 1
+				break
+			}
+		}
+		if (result.rowCount > 0) {
+			const all_xp = result.rows[0].xp
+			const level = Math.floor(all_xp / 100)
+			const xp = all_xp - level * 100
+			res.send({ xp, level, rank, numUser: all_rank.rowCount })
+		} else {
+			res.send({ xp: 0 })
+		}
+	} catch (err) {
+		return next(new ErrorResponse(err, 500))
+	}
+}
--- eduroom_frontend/components/learningpath/Userbar.js
+++ eduroom_frontend/components/learningpath/Userbar.js
@@ -1,37 +1,49 @@
-import React, {Fragment} from 'react'
-const UserBar = ({user}) => {
-    return (
-        <Fragment>
-            <div className="user-bar">
-                <div className="row">
-                    <div style={{padding:'.1rem 1rem', fontWeight:'900', marginTop:'2px', fontSize:'20px'}}>LV : {user.level}</div>
-                    <div style={{fontSize:'20px' ,background:'#A27CEF',color:'white',padding:'.1rem 1rem',borderRadius:'1rem',marginRight:'5px', fontWeight:'900'}}>
-                    {user.exp}/9999</div>
-                    <div style={{padding:'.1rem 1rem',fontWeight:'900',fontSize:'20px'}}> &#127942; : {user.trophy}/10</div>
-                </div>
-                {/* <div className="row">
-                    <div style={{background:'#A27CEF',color:'white',padding:'.1rem 1rem',borderRadius:'1rem'}}>
-                    {user.exp}/9999</div>
-                </div> */}
-            </div>
-            <style jsx>
-                {`
-                .user-bar {
-                    display: flex;
-                    flex-flow: column;
-                    background: white;
-                    border-radius: 15px;
-                    padding: 1rem 2rem;
-                    margin: 1rem 2rem;
-                }
-                .row {
-                    display: flex;
-                    flex: 1;
-                    width:100%;
-                }
-                `}
-            </style>
-        </Fragment>
-    )
+import { Fragment } from 'react'
+const UserBar = ({ user, numUser }) => {
+	return (
+		<Fragment>
+			<div className="user-bar">
+				<div className="row">
+					<div style={{ padding: '.1rem 1rem', fontWeight: '900', marginTop: '2px', fontSize: '20px' }}>
+						LV : {user.level}
+					</div>
+					<div
+						style={{
+							fontSize: '20px',
+							background: '#A27CEF',
+							color: 'white',
+							padding: '.1rem 1rem',
+							borderRadius: '1rem',
+							marginRight: '5px',
+							fontWeight: '900',
+						}}
+					>
+						{user.exp}/100
+					</div>
+					<div style={{ padding: '.1rem 1rem', fontWeight: '900', fontSize: '20px' }}>
+						{' '}
+						&#127942; : {user.trophy}/{numUser}
+					</div>
+				</div>
+			</div>
+			<style jsx>
+				{`
+					.user-bar {
+						display: flex;
+						flex-flow: column;
+						background: white;
+						border-radius: 15px;
+						padding: 1rem 2rem;
+						margin: 1rem 2rem;
+					}
+					.row {
+						display: flex;
+						flex: 1;
+						width: 100%;
+					}
+				`}
+			</style>
+		</Fragment>
+	)
 }
-export default UserBar;
+export default UserBar
