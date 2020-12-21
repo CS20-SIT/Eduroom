05d36e335c0928ce1ab5d124b8f73055ad42d01a
Thanaphon <thanaphon.me@mail.kmutt.ac.th>
2020-12-22 03:36:07 +0700

    [ADMIN] : [UPDATE] achievement of user in learning path

--- eduroom_frontend/components/learningpath/Userbar.js
+++ eduroom_frontend/components/learningpath/Userbar.js
@@ -1,5 +1,29 @@
-import { Fragment } from 'react'
-const UserBar = ({ user, numUser }) => {
+import { Fragment, useState } from 'react'
+import DialogTitle from '@material-ui/core/DialogTitle'
+import Dialog from '@material-ui/core/Dialog'
+
+const UserBar = ({ user, numUser, achievement }) => {
+	const [open, setOpen] = useState(false)
+	const handleClose = () => {
+		setOpen(false)
+	}
+	const renderAchievement = () => {
+		const arr = achievement.map((ac, idx) => {
+			return (
+				<Fragment key={idx}>
+					<div style={{ display: 'flex', justifyContent: 'space-between', minWidth: '400px' }}>
+						<h4>{ac.detail}</h4>
+						{ac.isAchieve ? (
+							<h4 style={{ color: 'green' }}>Complete</h4>
+						) : (
+							<h4 style={{ color: '#ff6961' }}>Not complete</h4>
+						)}
+					</div>
+				</Fragment>
+			)
+		})
+		return arr
+	}
 	return (
 		<Fragment>
 			<div className="user-bar">
@@ -20,14 +44,31 @@ const UserBar = ({ user, numUser }) => {
 					>
 						{user.exp}/100
 					</div>
-					<div style={{ padding: '.1rem 1rem', fontWeight: '900', fontSize: '20px' }}>
+					<div className="rank" onClick={() => setOpen(true)}>
 						{' '}
 						&#127942; : {user.trophy}/{numUser}
 					</div>
 				</div>
 			</div>
+			<Dialog onClose={handleClose} aria-labelledby="simple-dialog-title" open={open}>
+				<div className="container">
+					<h1 style={{ textAlign: 'center' }}>Achievement</h1>
+					{renderAchievement()}
+				</div>
+			</Dialog>
 			<style jsx>
 				{`
+					.container {
+						padding: 30px 50px;
+					}
+					.rank {
+						padding: 0.1rem 1rem;
+						font-weight: 900;
+						font-size: 20px;
+					}
+					.rank:hover {
+						cursor: pointer;
+					}
 					.user-bar {
 						display: flex;
 						flex-flow: column;
