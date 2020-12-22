92205507f043fe136cf6bd82d53d79c2e7ef3473
Thanaphon <thanaphon.me@mail.kmutt.ac.th>
2020-12-22 13:53:19 +0700

    [ADMIN] : [ADD] approve instructor frontend

---
+++ eduroom_frontend/styles/admin/approveInstructor.js
@@ -0,0 +1,22 @@
+import css from 'styled-jsx/css'
+export default css`
+	.container {
+		margin: 50px;
+	}
+	.box {
+		background: #ffffff;
+		box-shadow: 0px 3px 10px #ebebeb;
+		border-radius: 20px;
+	}
+
+	.approveBtn {
+		background: #3d467f;
+		border-radius: 20px;
+		padding: 10px 15px;
+		opacity: 1;
+	}
+	.approveBtn:hover {
+		opacity: 0.75;
+		cursor: pointer;
+	}
+`
