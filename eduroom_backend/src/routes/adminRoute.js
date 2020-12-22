7d73eb3bc26343ad61ec9fe590dc44b08e746ac2
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2020-12-22 13:50:18 +0700

    [ADMIN]:[ADD] Approve instrctor and courses

--- eduroom_backend/src/routes/adminRoute.js
+++ eduroom_backend/src/routes/adminRoute.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { editAdminProfile, editAdminPassword, editAdminProfilePic } = require('../controllers/adminController')
+const { editAdminProfile, editAdminPassword, editAdminProfilePic, approveCourse, getInReviewCourse, getUnVerifiedInstructor, verifyInstructor } = require('../controllers/adminController')
 const { jwtAdminAuthenticate } = require('../middleware/jwtAuthenticate')
 const { uploadToLocalHandler } = require('../middleware/multer')
 
@@ -8,5 +8,12 @@ const router = express.Router()
 router.post('/editAdminProfile', jwtAdminAuthenticate, editAdminProfile)
 router.post('/editPassword', jwtAdminAuthenticate, editAdminPassword)
 router.post('/editProfilePic', jwtAdminAuthenticate, uploadToLocalHandler(), editAdminProfilePic)
+
+router.get('/approveCourse', jwtAdminAuthenticate, getInReviewCourse)
+router.post('/approveCourse', jwtAdminAuthenticate, approveCourse)
+
+router.get('/verifyInstructor', jwtAdminAuthenticate, getUnVerifiedInstructor)
+router.post('/verifyInstructor', jwtAdminAuthenticate, verifyInstructor)
+
 
 module.exports = router
