54f5385affa10c62bc639fce9a261739f142d365
Krishadawut Olde Monnikhof <krishadawut.om@mail.kmutt.ac.th>
2020-12-24 08:59:45 +0700

    [G05]:[UPDATE] Fix some bug

--- eduroom_frontend/components/chat/messageRight.js
+++ eduroom_frontend/components/chat/messageRight.js
@@ -99,7 +99,7 @@ export default function messageRight(props) {
 					display: inline-block;
 				}
 				.dropdown {
-					!z-index:1
+					z-index:2 !important;
 					margin-right: 15px;
 					max-height: 150px;
 					position: absolute;
@@ -111,7 +111,7 @@ export default function messageRight(props) {
 					padding: 10px 10px 10px 10px;
 				}
 				.dropdown2 {
-					!z-index:1
+					z-index:2 !important;
 					max-height: 150px;
 					position: absolute;
 					background-color: #f5f5f5;
--- eduroom_socket_chat/src/index.js
+++ eduroom_socket_chat/src/index.js
@@ -14,7 +14,6 @@ const io = socketIO.listen(app, socketOptions);
 io.on('connection', (client) => {
 console.log('user connected');
 	client.on("joinRoom",(room)=>{
-		//read message
 		client.join(room)
 	})
 	client.on("leaveRoom",(room)=>{
@@ -22,26 +21,39 @@ console.log('user connected');
 	})
 	client.on("sendMessage",(room)=>{
 		io.in(room).emit('recieveMessage',room)
+		io.in('lr'+room).emit('changeChatList',room)
 	})
 	client.on("unsendMessage",(room)=>{
 		io.in(room).emit('recieveUnsendMessage',room)
+		io.in('lr'+room).emit('changeChatList',room)
 	})
 	client.on("changeChatRoomName",(room)=>{
 		io.in(room).emit('recieveChangeChatRoomName',room)
+		io.in('lr'+room).emit('changeChatList',room)
 	})
 	client.on("readMessage",(room)=>{
 		io.in(room).emit('recieveReadMessage',room)
 	})
 	client.on("changeProfilePic",(room)=>{
 		io.in(room).emit('recieveChangeProfilePic',room)
+		io.in('lr'+room).emit('changeChatList',room)
 	})
 	client.on("leaveChatRoom",(room)=>{
 		io.in(room).emit('recieveLeaveChatRoom',room)
+		io.in('lr'+room).emit('changeChatList',room)
 	})
 	client.on("deleteChatRoom",(room)=>{
 		io.in(room).emit('recieveDeleteChatRoom',room)
+		io.in('lr'+room).emit('changeChatList',room)
 	})
 	client.on("kickOut",(room)=>{
 		io.in(room).emit('recieveKickOut',room)
+		io.in('lr'+room).emit('changeChatList',room)
+	})
+	client.on("joinListRoom",(room)=>{
+		client.join('lr'+room)
+	})
+	client.on("leaveListRoom",(room)=>{
+		client.leave('lr'+room)
 	})
 });
